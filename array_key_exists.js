62edc6294c862955e29fbf7fdb28d2183cc92013
Felix Geisendörfer <felix@debuggable.com>
2008-04-10 13:28:56 +0000

    Fixed array_key_exists

--- array_key_exists.js
+++ array_key_exists.js
@@ -1,6 +1,7 @@
 function array_key_exists ( key, search ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	// +   improved by: Felix Geisendörfer (http://www.debuggable.com/felix)
     // *     example 1: array_key_exists('kevin', {'kevin': 'van Zonneveld'});
     // *     returns 1: true
 
@@ -9,5 +10,5 @@ function array_key_exists ( key, search ) {
         return false;
     }
 
-    return search[key] !== undefined;
+    return key in search;
 }
