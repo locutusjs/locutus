9f6221b1f67f26c3f546dc768671892e77a82a69
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-04-10 15:04:12 +0000

    you use 'oe' if 'ö' is not working for some reason

--- array_key_exists.js
+++ array_key_exists.js
@@ -1,7 +1,7 @@
 function array_key_exists ( key, search ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-	// +   improved by: Felix Geisendörfer (http://www.debuggable.com/felix)
+    // +   improved by: Felix Geisendoerfer (http://www.debuggable.com/felix)
     // *     example 1: array_key_exists('kevin', {'kevin': 'van Zonneveld'});
     // *     returns 1: true
 
