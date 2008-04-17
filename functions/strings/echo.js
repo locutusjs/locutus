84ae0f681fd0d3b0f34f40e6873ec974a5bd0f3b
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-04-17 10:18:19 +0000

    Using innerHTML on the BODY is very dangerous because you will break all references to HTMLElements that were done before !
    I strongly recommend you to use document.createDocumentFragment.

--- functions/strings/echo.js
+++ functions/strings/echo.js
@@ -1,15 +1,19 @@
 function echo ( ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Philip Peterson
+    // +   improved by: echo is bad
     // *     example 1: echo('Hello', 'World');
     // *     returns 1: null
     
+    var doc_elem = document.createDocumentFragment();
+    
     for( i = 0; i < echo.arguments.length; i++ ) {
-        if( document.body && document.body.innerHTML ) {
-            document.body.innerHTML = document.body.innerHTML + echo.arguments[i];
+        if( doc_elem.body && doc_elem.body.innerHTML ) {
+            doc_elem.body.innerHTML = doc_elem.body.innerHTML + echo.arguments[i];
         } else {
-            document.write( echo.arguments[i] );
+            doc_elem.write( echo.arguments[i] );
         }
     }
+    
     return null;
 }
