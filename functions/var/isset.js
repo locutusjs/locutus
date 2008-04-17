3c19594b029348a12a120679ab105b69889d50a2
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-04-17 10:47:49 +0000

    Can be maked quicker

--- functions/var/isset.js
+++ functions/var/isset.js
@@ -1,19 +1,21 @@
-function isset( mixed_var ) {
+function isset(  ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: FremyCompany
     // *     example 1: isset( undefined, true);
     // *     returns 1: false
     // *     example 2: isset( 'Kevin van Zonneveld' );
     // *     returns 2: true
 
-    var i = 0, argc = arguments.length, argv = arguments, set=true;
-
-    for (i = 0; i< argc; i++){
-        if( argv[i] == undefined ){
-            set = false;
-            break;
+    var a=arguments; var l=a.length; var i=0;
+    
+    while ( i!=l ) {
+        if (typeof(a[i])=='undefined') { 
+            return false; 
+        } else { 
+            i++; 
         }
     }
-
-    return set;
+    
+    return true;
 }
