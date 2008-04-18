a5a9a6ad1480fa9d144d6ba62cc6c8659e506166
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-04-18 06:36:29 +0000

    The following code, on php 5, gave me as output "hemmo, mars"

--- functions/strings/str_replace.js
+++ functions/strings/str_replace.js
@@ -5,6 +5,7 @@ function str_replace(search, replace, subject) {
     // +   improved by: Philip Peterson
     // +   improved by: Simon Willison (http://simonwillison.net)
     // +    revised by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+    // -    depends on: is_array
     // *     example 1: str_replace(' ', '.', 'Kevin van Zonneveld');
     // *     returns 1: 'Kevin.van.Zonneveld'
     // *     example 2: str_replace(['{name}', 'l'], ['hello', 'm'], '{name}, lars');
