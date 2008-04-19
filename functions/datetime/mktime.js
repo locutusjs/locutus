745c81cc6db46eb7281df8a4eb7cf06e4c2a32bf
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-04-19 12:44:22 +0000

    setting a month to something greater than 11 would not increase the year

--- functions/datetime/mktime.js
+++ functions/datetime/mktime.js
@@ -8,18 +8,18 @@ function mktime() {
     // *     example 1: mktime( 14, 10, 2, 2, 1, 2008 );
     // *     returns 1: 1201871402
     
-    var no, i = 0, d = new Date(), argv = arguments, argc = argv.length;
+    var no, ma = 0, mb = 0, i = 0, d = new Date(), argv = arguments, argc = argv.length;
     d.setHours(0,0,0); d.setDate(1); d.setMonth(1); d.setYear(1972);
-
+ 
     var dateManip = {
         0: function(tt){ return d.setHours(tt); },
         1: function(tt){ return d.setMinutes(tt); },
-        2: function(tt){ return d.setSeconds(tt); },
-        3: function(tt){ return d.setMonth(parseInt(tt)-1); },
-        4: function(tt){ return d.setDate(tt); },
-        5: function(tt){ return d.setYear(tt); }
+        2: function(tt){ set = d.setSeconds(tt); mb = d.getDate() - 1; return set; },
+        3: function(tt){ set = d.setMonth(parseInt(tt)-1); ma = d.getFullYear() - 1972; return set; },
+        4: function(tt){ return d.setDate(tt+mb); },
+        5: function(tt){ return d.setYear(tt+ma); }
     };
-
+    
     for( i = 0; i < argc; i++ ){
         no = parseInt(argv[i]);
         if(no && isNaN(no)){
