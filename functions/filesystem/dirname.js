e766998c804c69bff7b2697a3e6835695e5d6374
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-04-28 15:23:25 +0000

    dirname('/dir/test/'); // outputs /dir. That IS correct. Pointed out by XoraX

--- functions/filesystem/dirname.js
+++ functions/filesystem/dirname.js
@@ -6,7 +6,7 @@ function dirname(path) {
     // *     example 2: dirname('c:/Temp/x');
     // *     returns 2: 'c:/Temp'
     // *     example 3: dirname('/dir/test/');
-    // *     returns 3: '/dir/test'
+    // *     returns 3: '/dir'
     
     return path.replace(/\\/g,'/').replace(/\/[^\/]*\/?$/, '');
 }
