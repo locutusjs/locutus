7d2b160f2450541f635f78a5e446d50ceefa7f26
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-05-02 09:45:49 +0000

    Brett Zamir: added support to allow a default of 1 for length. Brett Zamir: removed the f_backwards argument which is a distinction not present in PHP

--- functions/strings/str_split.js
+++ functions/strings/str_split.js
@@ -1,33 +1,21 @@
-function str_split ( f_string, f_split_length, f_backwards ){
+function str_split ( f_string, f_split_length){
     // http://kevin.vanzonneveld.net
     // +     original by: Martijn Wieringa
+    // +     improved by: Brett Zamir
     // *         example 1: str_split('Hello Friend', 3);
     // *         returns 1: ['Hel', 'lo ', 'Fri', 'end']
-
-    if(f_backwards == undefined) {
-        f_backwards = false;
+ 
+    if (f_split_length == undefined) {
+        f_split_length = 1;
     }
-
     if(f_split_length > 0){
-        var result = new Array();
-
-        if(f_backwards) {
-            var r = (f_string.length % f_split_length);
-
-            if(r > 0){
-                result[result.length] = f_string.substring(0, r);
-                f_string = f_string.substring(r);
-            }
-        }
-
+        var result = [];
         while(f_string.length > f_split_length) {
             result[result.length] = f_string.substring(0, f_split_length);
             f_string = f_string.substring(f_split_length);
         }
-
         result[result.length] = f_string;
         return result;
     }
-
     return false;
 }
