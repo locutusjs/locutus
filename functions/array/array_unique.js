510f328bc496fb0309c2125677eb2686928be7a9
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-05-02 09:33:39 +0000

    duncan: PHP array_unique returns an array, not a boolean.

--- functions/array/array_unique.js
+++ functions/array/array_unique.js
@@ -1,18 +1,20 @@
 function array_unique( array ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Carlos R. L. Rodrigues (http://www.jsfromhell.com)
+    // +      input by: duncan
+    // +    bufixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // *     example 1: array_unique(['Kevin','Kevin','van','Zonneveld']);
-    // *     returns 1: true
+    // *     returns 1: ['Kevin','van','Zonneveld']
 
-    var p, i, j;
-    for(i = array.length; i;){
+    var p, i, j, tmp_arr = array;
+    for(i = tmp_arr.length; i;){
         for(p = --i; p > 0;){
-            if(array[i] === array[--p]){
-                for(j = p; --p && array[i] === array[p];);
-                i -= array.splice(p + 1, j - p).length;
+            if(tmp_arr[i] === tmp_arr[--p]){
+                for(j = p; --p && tmp_arr[i] === tmp_arr[p];);
+                i -= tmp_arr.splice(p + 1, j - p).length;
             }
         }
     }
 
-    return true;
+    return tmp_arr;
 }
