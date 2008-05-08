4732230db25a762d4e34cded38012d04d9dc9495
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-05-08 19:42:49 +0000

    Slow in IE due to string concatenation. Faster to push onto an array and return array.join('')

--- functions/xml/utf8_decode.js
+++ functions/xml/utf8_decode.js
@@ -1,27 +1,29 @@
 function utf8_decode ( str_data ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
+    // +      input by: Aman Gupta
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // *     example 1: utf8_decode('Kevin van Zonneveld');
     // *     returns 1: 'Kevin van Zonneveld'
 
-    var string = "", i = 0, c = c1 = c2 = 0;
+    var tmp_arr = [], i = 0, c = c1 = c2 = 0;
 
     while ( i < str_data.length ) {
         c = str_data.charCodeAt(i);
         if (c < 128) {
-            string += String.fromCharCode(c);
+            tmp_arr[] = String.fromCharCode(c); 
             i++;
-        } else if((c > 191) && (c < 224)) {
+        } else if ((c > 191) && (c < 224)) {
             c2 = str_data.charCodeAt(i+1);
-            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+            tmp_arr[] = String.fromCharCode(((c & 31) << 6) | (c2 & 63));
             i += 2;
         } else {
             c2 = str_data.charCodeAt(i+1);
             c3 = str_data.charCodeAt(i+2);
-            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+            tmp_arr[] = String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
             i += 3;
         }
     }
-
-    return string;
+    
+    return tmp_arr.join('');
 }
