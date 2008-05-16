00edc58bcd67ccac06e38f07e8c711627dada66d
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-05-16 14:43:59 +0000

    In PHP, strlen(45) returns 2. With the above JS implementation, strlen(45) returns undefined (numbers have no "length" property)

--- functions/strings/strlen.js
+++ functions/strings/strlen.js
@@ -1,8 +1,9 @@
 function strlen( string ){
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: Sakimori
     // *     example 1: strlen('Kevin van Zonneveld');
     // *     returns 1: 19
 
-    return string.length;
+    return ("" + string).length;
 }
