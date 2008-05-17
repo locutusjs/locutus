afd28e4ddf3a4c0ce9297aa44c225c655ce4d21f
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-05-17 13:06:26 +0000

    Support for explode's limit attribute

--- functions/strings/explode.js
+++ functions/strings/explode.js
@@ -1,27 +1,32 @@
-function explode( delimiter, string ) {
+function explode( delimiter, string, limit ) {
     // http://kevin.vanzonneveld.net
-    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   improved by: kenneth
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +     original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +     improved by: kenneth
+    // +     improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +     improved by: d3x
     // *     example 1: explode(' ', 'Kevin van Zonneveld');
     // *     returns 1: {0: 'Kevin', 1: 'van', 2: 'Zonneveld'}
-
+    // +     further improved by: d3x
+    // *         example 1: explode('=', 'a=bc=d', 2);
+    // *         returns : ['a', 'bc=d']
+ 
     var emptyArray = { 0: '' };
-
-    if ( arguments.length != 2
+ 
+    if ( arguments.length != 3
         || typeof arguments[0] == 'undefined'
-        || typeof arguments[1] == 'undefined' )
+        || typeof arguments[1] == 'undefined'
+        || typeof arguments[2] == 'undefined' )
     {
         return null;
     }
-
+ 
     if ( delimiter === ''
         || delimiter === false
         || delimiter === null )
     {
         return false;
     }
-
+ 
     if ( typeof delimiter == 'function'
         || typeof delimiter == 'object'
         || typeof string == 'function'
@@ -29,10 +34,18 @@ function explode( delimiter, string ) {
     {
         return emptyArray;
     }
-
+ 
     if ( delimiter === true ) {
         delimiter = '1';
     }
-
-    return string.toString().split ( delimiter.toString() );
+    
+    if(!limit){
+        return string.toString().split ( delimiter.toString() );
+    } else {
+        var splitted = string.toString().split(delimiter.toString());
+        var partA = splitted.splice(0, limit - 1);
+        var partB = splitted.join(delimiter.toString());
+        partA.push(partB);
+        return partA;
+    }
 }
