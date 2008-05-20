b2e454030479dfaf6ca6a4a5c9c77a1d4dc475e6
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-05-20 21:27:29 +0000

    Where formatArray function is testing data types, it does not except types of Object

--- functions/var/print_r.js
+++ functions/var/print_r.js
@@ -1,20 +1,22 @@
 function print_r( array, return_val ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Michael White (http://crestidg.com)
+    // +   improved by: Ben Bryan
     // *     example 1: print_r(1, true);
     // *     returns 1: 1
 
     var output = "", pad_char = " ", pad_val = 4;
 
     var formatArray = function (obj, cur_depth, pad_val, pad_char) {
-        if(cur_depth > 0)
+        if (cur_depth > 0) {
             cur_depth++;
+        }
 
         var base_pad = repeat_char(pad_val*cur_depth, pad_char);
         var thick_pad = repeat_char(pad_val*(cur_depth+1), pad_char);
         var str = "";
 
-        if (obj instanceof Array) {
+        if (obj instanceof Array || obj instanceof Object) {
             str += "Array\n" + base_pad + "(\n";
             for (var key in obj) {
                 if (obj[key] instanceof Array) {
@@ -26,14 +28,16 @@ function print_r( array, return_val ) {
             str += base_pad + ")\n";
         } else {
             str = obj.toString();
-        };
+        }
 
         return str;
     };
 
     var repeat_char = function (len, pad_char) {
         var str = "";
-        for(var i=0; i < len; i++) { str += pad_char; };
+        for(var i=0; i < len; i++) { 
+            str += pad_char; 
+        };
         return str;
     };
     output = formatArray(array, 0, pad_val, pad_char);
