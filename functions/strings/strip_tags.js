113cc6030871051497540ac0385c37a79d069b9f
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-05-26 06:49:59 +0000

    Allowed Tags support for strip_tags

--- functions/strings/strip_tags.js
+++ functions/strings/strip_tags.js
@@ -1,8 +1,17 @@
-function strip_tags( str ){
+function strip_tags(str, allowed_tags) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // *     example 1: strip_tags('Kevin <br />van <i>Zonneveld</i>');
-    // *     returns 1: 'Kevin van Zonneveld'
-
-    return str.replace(/<\/?[^(>|i)]+>/gi, '');
+    // *     example 1: strip_tags('<p>Kevin</p> <br /><b>van</b> <i>Zonneveld</i>', '<i>,<b>');
+    // *     returns 1: 'Kevin <b>van</b> <i>Zonneveld</i>'
+    
+    match = '>';
+    if (allowed_tags) {
+        allowed_tags.replace(/[><]/g, '');
+        allowed_tags.replace(/ /g, '');
+        allowed_tags.replace(/,/g, '|');
+        
+        match += '|'+allowed_tags; 
+    }
+    
+    return str.replace('/<\/?[^('+match+')]+>/gi', '');
 }
