c7a5fcd227c9bd75f8f322590b4780a07416ef40
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-05-31 09:47:43 +0000

    Fixed strip_tags allowed_tags

--- functions/strings/strip_tags.js
+++ functions/strings/strip_tags.js
@@ -4,18 +4,38 @@ function strip_tags(str, allowed_tags) {
     // *     example 1: strip_tags('<p>Kevin</p> <br /><b>van</b> <i>Zonneveld</i>', '<i>,<b>');
     // *     returns 1: 'Kevin <b>van</b> <i>Zonneveld</i>'
     
-    var match = '';
+    var key = '', tag = '';
+    var matches = allowed_array = [];
+    var allowed_keys = {};
     
+    // Build allowes tags associative array
     if (allowed_tags) {
-        allowed_tags = allowed_tags.replace(/[\<\> ]+/g, '');;
-        allowed_tags = allowed_tags.replace(/\,/g, '|'); 
+        allowed_tags  = allowed_tags.replace(/[\<\> ]+/g, '');;
+        allowed_array = allowed_tags.split(',');
+        
+        for (key in allowed_array) {
+            tag = allowed_array[key];
+            allowed_keys['<' + tag + '>']   = true;
+            allowed_keys['<' + tag + ' />'] = true;
+            allowed_keys['</' + tag + '>']  = true;
+        }
     }
     
-    match = '/<\/?(?!(?:'+allowed_tags+')\\b)[^>]+>/gi';
+    // Match tags
+    matches = str.match(/(<\/?[^>]+>)/gi);
     
-     
+    // Is tag not in allowed list? Remove from str! 
+    for (key in matches) {
+        tag = matches[key];
+        document.write('<xmp>'+tag+'</xmp>');
+        
+        if (!allowed_keys[tag]) {
+            document.write('not allowed');
+            str = str.replace(tag, "");
+        } else {
+            document.write('allowed');
+        }
+    }
     
-    //alert(match);
-    
-    return str.replace(match, '');
+    return str;
 }
