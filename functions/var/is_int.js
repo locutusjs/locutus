b07b000de15e3ddc21da0c8e11a8c2ebdcf02c48
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-05-31 12:28:52 +0000

    Minor improvements to functions

--- functions/var/is_int.js
+++ functions/var/is_int.js
@@ -1,10 +1,10 @@
-function is_numeric( mixed_var ) {
+function is_int( mixed_var ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Alex
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // *     example 1: is_numeric(186.31);
+    // *     example 1: is_int(186.31);
     // *     returns 1: false
-    // *     example 2: is_numeric(12);
+    // *     example 2: is_int(12);
     // *     returns 2: true
 
     var y = parseInt(mixed_var * 1);
