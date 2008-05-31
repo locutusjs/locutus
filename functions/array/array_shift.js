27900820314a108d0d266d29d9558c93639db0e9
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-05-31 12:44:41 +0000

    Minor improvements to functions

--- functions/array/array_shift.js
+++ functions/array/array_shift.js
@@ -1,33 +1,13 @@
 function array_shift( array ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: Martijn Wieringa
     // *     example 1: array_shift(['Kevin', 'van', 'Zonneveld']);
     // *     returns 1: 'Kevin'
 
-    var i=0, first_elm=null, cnt=0, tmp_arr = {};
-
-    // input sanitation
-    if( !array || (array.constructor !== Array && array.constructor !== Object) || !array.length ){
-        return null;
+    if (array.length > 0) {
+        return array.shift();
     }
-
-    if( array.constructor === Array ){
-        first_elm = array[0];
-        for( i = 0; i < array.length; i++ ){
-            array[i] = array[i+1];
-        }
-        array.length--;
-    } else if( array.constructor === Object ){
-        for(var key in array){
-            if( cnt == 0 ){
-                first_elm = array[key];
-            } else{
-                tmp_arr[key] = array[key];
-            }
-            cnt ++;
-        }
-        array = tmp_arr;
-    }
-
-    return first_elm;
+    
+    return null;
 }
--- functions/array/array_unshift.js
+++ functions/array/array_unshift.js
@@ -1,34 +1,15 @@
 function array_unshift( array ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: Martijn Wieringa
     // *     example 1: array_unshift(['van', 'Zonneveld'], 'Kevin');
     // *     returns 1: 3
 
-    var cnt=0, tot_cnt=0, tmp_arr = {}, argc = arguments.length, argv = arguments;
-
-    // input sanitation
-    if( !array || (array.constructor !== Array && array.constructor !== Array && typeof array != 'object' && typeof array != 'array') ){
-        return null;
+    var argc = arguments.length, argv = arguments, i;
+    
+    for (i = 1; i < argc; i++) {
+        array.unshift(argv[i]);
     }
-
-    // prepend
-    for (i = 1; i< argc; i++){
-        tmp_arr[cnt] = argv[i];
-        cnt++; tot_cnt++;
-    }
-
-    // append original
-    for(var key in array){
-        if( typeof key == 'number' && isFinite( key ) ){
-            // modify numeric key
-            tmp_arr[cnt] = array[key];
-            cnt++; tot_cnt++;
-        } else{
-            // save original alphanumeric key
-            tmp_arr[key] = array[key];
-            tot_cnt++;
-        }
-    }
-
-    return tot_cnt;
+    
+    return (array.length);
 }
