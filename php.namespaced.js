fe1b64481759067f75b8ec042f9cb78835da2881
PHP.JS Automation <info@phpjs.org>
2008-06-08 14:08:16 +0000

    generated

--- php.namespaced.js
+++ php.namespaced.js
@@ -1,7 +1,7 @@
 /* 
  * More info at: http://kevin.vanzonneveld.net/techblog/article/phpjs_licensing/
  * 
- * This is version: 1.21
+ * This is version: 1.22
  * php.js is copyright 2008 Kevin van Zonneveld.
  * 
  * Portions copyright Michael White (http://crestidg.com), _argos, Jonas
@@ -2298,7 +2298,7 @@
             // Format a number with grouped thousands
             // 
             // +    discuss at: http://kevin.vanzonneveld.net/techblog/article/javascript_equivalent_for_phps_number_format/
-            // +       version: 804.1712
+            // +       version: 806.816
             // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
             // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
             // +     bugfix by: Michael White (http://crestidg.com)
@@ -2309,8 +2309,8 @@
             // *     returns 1: 1234.57     
         
             var n = number, c = isNaN(decimals = Math.abs(decimals)) ? 2 : decimals;
-            var d = dec_point == undefined ? "," : dec_point;
-            var t = thousands_sep == undefined ? "." : thousands_sep, s = n < 0 ? "-" : "";
+            var d = dec_point == undefined ? "." : dec_point;
+            var t = thousands_sep == undefined ? "," : thousands_sep, s = n < 0 ? "-" : "";
             var i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
             
             return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
