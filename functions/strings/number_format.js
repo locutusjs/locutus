54a778a165a4b0ecbba32b517091e2a6c4e80ff6
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-06-08 14:05:49 +0000

    the default values for dec_point and thou_sep are the wrong way around

--- functions/strings/number_format.js
+++ functions/strings/number_format.js
@@ -10,8 +10,8 @@ function number_format( number, decimals, dec_point, thousands_sep ) {
     // *     returns 1: 1234.57     
 
     var n = number, c = isNaN(decimals = Math.abs(decimals)) ? 2 : decimals;
-    var d = dec_point == undefined ? "," : dec_point;
-    var t = thousands_sep == undefined ? "." : thousands_sep, s = n < 0 ? "-" : "";
+    var d = dec_point == undefined ? "." : dec_point;
+    var t = thousands_sep == undefined ? "," : thousands_sep, s = n < 0 ? "-" : "";
     var i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
     
     return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
