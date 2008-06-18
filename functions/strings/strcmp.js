040792f5d60cfd5e94dc16671e6b2b154e419cc9
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-06-18 20:44:04 +0000

    strcmp does not work correctly at all

--- functions/strings/strcmp.js
+++ functions/strings/strcmp.js
@@ -3,24 +3,10 @@ function strcmp ( str1, str2 ) {
     // +   original by: _argos
     // +      input by: Steve Hilder
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // *     example 1: strcmp( 'waldo', 'Waldo' );
+    // *     example 1: strcmp( 'waldo', 'owald' );
     // *     returns 1: 1
-    // *     example 2: strcmp( 'Waldo', 'waldo' );
+    // *     example 2: strcmp( 'owald', 'waldo' );
     // *     returns 2: -1
-    // *     example 3: strcmp( 'waldo', 'waldo' );
-    // *     returns 3: 0
-    // *     example 4: strcmp( 'test', 'tomato' );
-    // *     returns 4: -1
 
-    var i = size1 = size2 = 0;
-    
-    for (i = 0; i < str1.length; ++i) {
-        size1 += str1.charCodeAt(i);
-    }
-
-    for (i = 0; i < str2.length; ++i) {
-        size2 += str2.charCodeAt(i);
-    }
-    
-    return ( ( size1 == size2 ) ? 0 : ( ( size1 > size2 ) ? 1 : -1 ) );
+    return ( ( str1 == str2 ) ? 0 : ( ( str1 > str2 ) ? 1 : -1 ) );
 }
