03d3f16ab30d60cf8b9d8517a1d889a299ea2867
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-06-22 09:08:44 +0000

    Implement basic sort_flags

--- functions/array/rsort.js
+++ functions/array/rsort.js
@@ -1,10 +1,28 @@
-function rsort( array ) {
+function rsort( array, sort_flags ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // *     example 1: rsort(['Kevin', 'van', 'Zonneveld']);
     // *     returns 1: true
     
-    array.sort();
+    var sorter = false;
+    
+    // For now only SORT_NUMERIC has a custom sorter
+    // and SORT_REGULAR, SORT_STRING, and SORT_LOCALE_STRING
+    // are all handled with the default sorter 
+    if (sort_flags == 'SORT_NUMERIC') {
+        sorter = function (a, b) {
+            return(a - b);
+        };
+    }
+    
+    if (sorter !== false) {
+        array.sort(sorter);
+    } else {
+        array.sort();
+    }
+    
+    // What makes it rsort:
     array.reverse();
+    
     return true;
 }
--- functions/array/sort.js
+++ functions/array/sort.js
@@ -1,9 +1,25 @@
-function sort( array ) {
+function sort( array, sort_flags ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // *     example 1: sort(['Kevin', 'van', 'Zonneveld']);
     // *     returns 1: true
     
-    array.sort();
+    var sorter = false;
+    
+    // For now only SORT_NUMERIC has a custom sorter
+    // and SORT_REGULAR, SORT_STRING, and SORT_LOCALE_STRING
+    // are all handled with the default sorter 
+    if (sort_flags == 'SORT_NUMERIC') {
+        sorter = function (a, b) {
+            return(a - b);
+        };
+    }
+    
+    if (sorter !== false) {
+        array.sort(sorter);
+    } else {
+        array.sort();
+    }
+    
     return true;
 }
