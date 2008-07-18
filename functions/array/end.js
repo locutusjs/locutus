9017fdd3e38d7501ff972e5390723b0de6a58149
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-07-18 07:27:34 +0000

    The native .pop() method didn't not work with objects (associative arrays)

--- functions/array/end.js
+++ functions/array/end.js
@@ -4,13 +4,22 @@ function end ( array ) {
     // +   bugfixed by: Legaev Andrey
     // +    revised by: J A R
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   restored by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // *     example 1: end({firstname: 'Kevin', middle: 'van', surname: 'Zonneveld'});
     // *     returns 1: 'Zonneveld'
+
+    var last_elm, key;
     
-    var tmp_arr = {};
-    
-    // We don't want to 'pop' the original array and reduce it's size
-    tmp_arr = array;
-    
-    return tmp_arr.pop();
+    // The native .pop() method didn't not work with objects (associative arrays)
+    // We need that for PHP compatibility
+
+    if (array.constructor === Array){
+        last_elm = array[(array.length-1)];
+    } else {
+        for (key in array){
+            last_elm = array[key];
+        }
+    }
+
+    return last_elm;
 }
