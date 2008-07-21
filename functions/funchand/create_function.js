f134578d623362a6dc1e74d1b93af57a4c36f431
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-07-21 13:05:43 +0000

    Added 3 more functions by Johnny Mast

--- functions/funchand/create_function.js
+++ functions/funchand/create_function.js
@@ -2,7 +2,7 @@ function create_function (args, code) {
     // http://kevin.vanzonneveld.net
     // +   original by: Johnny Mast (http://www.phpvrouwen.nl)
     // *     example 1: create_function('a, b', "return (a + b);");
-    // *     returns 1: function
+    // *     returns 1: 'function'
     
     eval ('var _oFunctionObject = function (' + args + ') { ' +  code + '}');
     return _oFunctionObject;
