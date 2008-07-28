118940ff75d3286cd553854b2dfbf2e3c07a57f5
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-07-28 07:36:05 +0000

    Code formatting

--- functions/strings/trim.js
+++ functions/strings/trim.js
@@ -14,23 +14,23 @@ function trim( str, charlist ) {
 
     var whitespace;
     
-    if(!charlist){
+    if (!charlist) {
         whitespace = ' \n\r\t\f\x0b\xa0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000';
-    } else{
+    } else {
         whitespace = charlist.replace(/([\[\]\(\)\.\?\/\*\{\}\+\$\^\:])/g, '\$1');
     }
   
-	for (var i = 0; i < str.length; i++) {
-		if (whitespace.indexOf(str.charAt(i)) === -1) {
-		str = str.substring(i);
-		break;
-		}
-	}
-	for (i = str.length - 1; i >= 0; i--) {
-		if (whitespace.indexOf(str.charAt(i)) === -1) {
-			str = str.substring(0, i + 1);
-			break;
-    	}
-	}
-	return whitespace.indexOf(str.charAt(0)) === -1 ? str : '';
+    for (var i = 0; i < str.length; i++) {
+        if (whitespace.indexOf(str.charAt(i)) === -1) {
+            str = str.substring(i);
+            break;
+        }
+    }
+    for (i = str.length - 1; i >= 0; i--) {
+        if (whitespace.indexOf(str.charAt(i)) === -1) {
+            str = str.substring(0, i + 1);
+            break;
+        }
+    }
+    return whitespace.indexOf(str.charAt(0)) === -1 ? str : '';
 }
