e405a69e0bbd3b1a8638559c64165587b3a9330e
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-08-29 10:17:47 +0000

    Improved urldecode & urlencode for increased PHP compatibility

--- functions/url/urldecode.js
+++ functions/url/urldecode.js
@@ -2,6 +2,8 @@ function urldecode( str ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Philip Peterson
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: AJ
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // %          note: info on what encoding functions to use from: http://xkr.us/articles/javascript/encode-compare/
     // *     example 1: urldecode('Kevin+van+Zonneveld%21');
     // *     returns 1: 'Kevin van Zonneveld!'
