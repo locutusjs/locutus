a1ffc9aa98887418a83b901d4570b615290c2618
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-09-07 22:19:12 +0000

    Improved strlen handling

--- functions/strings/strlen.js
+++ functions/strings/strlen.js
@@ -1,9 +1,18 @@
-function strlen( string ){
+function strlen (string) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +   improved by: Sakimori
+    // +      input by: Kirk Strobeck
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // *     example 1: strlen('Kevin van Zonneveld');
     // *     returns 1: 19
 
-    return ("" + string).length;
+    var tmp_str = '', l = 0;
+    tmp_str = string + '';
+    
+    if (tmp_str.length) {
+        return tmp_str.length; 
+    }  
+    
+    return 0;
 }
