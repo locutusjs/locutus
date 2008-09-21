f631c9001714e1c6798b1da93064315fc3d3aeba
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-09-21 19:44:38 +0000

    Fixed minor bugs

--- _tools/phpjstest.php
+++ _tools/phpjstest.php
@@ -1,96 +1,96 @@
 #!/usr/bin/php -q
 <?php
-    error_reporting(E_ALL);
-    require_once "PHPJS_Library/PHPJS/Library.php";
+error_reporting(E_ALL);
+require_once "PHPJS_Library/PHPJS/Library.php";
 
-    // Check for CLI
-    if ((php_sapi_name() != 'cli')) {
-        die("CLI Only");
-    }
-    
-    
-    $dir = realpath(dirname(__FILE__)."/..")."/functions";
-    $PHPJS_Tester_Shell = new PHPJS_Library_Tester_Shell($dir);
-    
-    // Parse commands
-    $argFunc = "all";
-    if (isset($argv[1])) {
-        $argFunc = $argv[1];
-    }
-    
-    $argMode = "run";
-    if (isset($argv[2])) {
-        $argMode = $argv[2];
-    }
+// Check for CLI
+if ((php_sapi_name() != 'cli')) {
+    die("CLI Only");
+}
 
-    $funcName = $argFunc;
-    $mode     = $argMode;
+
+$dir = realpath(dirname(__FILE__)."/..")."/functions";
+$PHPJS_Tester_Shell = new PHPJS_Library_Tester_Shell($dir);
+
+// Parse commands
+$argFunc = "all";
+if (isset($argv[1])) {
+    $argFunc = $argv[1];
+}
+
+$argMode = "run";
+if (isset($argv[2])) {
+    $argMode = $argv[2];
+}
+
+$funcName = $argFunc;
+$mode     = $argMode;
+
+// Load funcion if nescessary
+if ($funcName !== "all" && $funcName !== false && strlen($funcName) > 1) {
+    if (strpos($funcName, "/") !== false || strpos($funcName, ".") !== false) {
+        // Convert a path to plain function name
+        $funcName = basename($funcName, ".js");
+    }
     
-    // Load funcion if nescessary
-    if ($funcName !== "all" && $funcName !== false && strlen($funcName) > 1) {
-        if (strpos($funcName, "/") !== false || strpos($funcName, ".") !== false) {
-            // Convert a path to plain function name
-            $funcName = basename($funcName, ".js");
-        }
+    if (!$PHPJS_Tester_Shell->functionExists($funcName)) {
+        die("Function '".$funcName."' does not exist\n");
+    } else {
+        $Function = $PHPJS_Tester_Shell->getFunction($funcName);
+    }        
+}
+
+// Choose mode & execute
+switch($mode) {
+    case "testcode":
+        echo $Function->testCode();
+        break;
+    case "debug":
+        $hr = str_repeat("=", 75)."\n";
+
+        echo "Info\n";
+        echo $hr;
+        echo "source file: ". $Function->getRealPath()."\n";
+        echo "tester file: ". $Function->getTesterPath()."\n";
+        echo "\n\n";
         
-        if (!$PHPJS_Tester_Shell->functionExists($funcName)) {
-            die("Function '".$funcName."' does not exist\n");
+        echo "Examples\n";
+        echo $hr;
+        print_r($Function->DocBlock->getExamples());
+        echo "\n\n";
+        
+        echo "Dependencies\n";
+        echo $hr;
+        print_r($Function->getDependencies());
+        echo "\n\n";
+
+        echo "Output\n";
+        echo $hr;
+        $Function->testFunction(true);
+        echo "\n\n";
+        
+        echo "Test result\n";
+        echo $hr;
+        $Function->testFunction(false);
+        break;
+    case "output":
+        $Function->testFunction(true);
+        break;
+    case "php":
+        $Function->testFunction(false, true);
+        break;
+    case "from":
+        $PHPJS_Tester_Shell->testFrom($funcName);
+        break;
+    case "run":
+        if ($funcName == "all") {
+            $PHPJS_Tester_Shell->testAll();
         } else {
-            $Function = $PHPJS_Tester_Shell->getFunction($funcName);
-        }        
-    }
-    
-    // Choose mode & execute
-    switch($mode) {
-        case "testcode":
-            echo $Function->testCode();
-            break;
-        case "debug":
-            $hr = str_repeat("=", 75)."\n";
-
-            echo "Info\n";
-            echo $hr;
-            echo "source file: ". $Function->getRealPath()."\n";
-            echo "tester file: ". $Function->getTesterPath()."\n";
-            echo "\n\n";
-            
-            echo "Examples\n";
-            echo $hr;
-            print_r($Function->DocBlock->getExamples());
-            echo "\n\n";
-            
-            echo "Dependencies\n";
-            echo $hr;
-            print_r($Function->getDependencies());
-            echo "\n\n";
-
-            echo "Output\n";
-            echo $hr;
-            $Function->testFunction(true);
-            echo "\n\n";
-            
-            echo "Test result\n";
-            echo $hr;
-            $Function->testFunction(false);
-            break;
-        case "output":
-            $Function->testFunction(true);
-            break;
-        case "php":
-            $Function->testFunction(false, true);
-            break;
-        case "from":
-            $PHPJS_Tester_Shell->testFrom($funcName);
-            break;
-        case "run":
-            if ($funcName == "all") {
-                $PHPJS_Tester_Shell->testAll();
-            } else {
-                echo $Function->testFunction();
-            }
-            break;
-        default:
-            die("Unknown command: ".$argMode);
-            break;
-    }
+            echo $Function->testFunction();
+        }
+        break;
+    default:
+        die("Unknown command: ".$argMode);
+        break;
+}
 ?>
--- functions/xml/utf8_decode.js
+++ functions/xml/utf8_decode.js
@@ -6,21 +6,21 @@ function utf8_decode ( str_data ) {
     // *     example 1: utf8_decode('Kevin van Zonneveld');
     // *     returns 1: 'Kevin van Zonneveld'
 
-    var tmp_arr = [], i = ac = c = c1 = c2 = 0;
+    var tmp_arr = [], i = ac = c1 = c2 = c3 = 0;
 
     while ( i < str_data.length ) {
-        c = str_data.charCodeAt(i);
-        if (c < 128) {
-            tmp_arr[ac++] = String.fromCharCode(c); 
+        c1 = str_data.charCodeAt(i);
+        if (c1 < 128) {
+            tmp_arr[ac++] = String.fromCharCode(c1); 
             i++;
-        } else if ((c > 191) && (c < 224)) {
+        } else if ((c1 > 191) && (c < 224)) {
             c2 = str_data.charCodeAt(i+1);
-            tmp_arr[ac++] = String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+            tmp_arr[ac++] = String.fromCharCode(((c1 & 31) << 6) | (c2 & 63));
             i += 2;
         } else {
             c2 = str_data.charCodeAt(i+1);
             c3 = str_data.charCodeAt(i+2);
-            tmp_arr[ac++] = String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+            tmp_arr[ac++] = String.fromCharCode(((c1 & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
             i += 3;
         }
     }
