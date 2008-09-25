4c7baca76daad850e6e9b0773dff1075fe731324
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-09-25 08:22:03 +0000

    Fixed regex

--- functions/strings/htmlspecialchars_decode.js
+++ functions/strings/htmlspecialchars_decode.js
@@ -2,24 +2,25 @@ function htmlspecialchars_decode(string, quote_style) {
     // http://kevin.vanzonneveld.net
     // +   original by: Mirek Slugen
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   bugfixed by: loonquawl
+    // +   bugfixed by: Mateusz "loonquawl" Zalega
+    // +      input by: ReverseSyntax
     // *     example 1: htmlspecialchars_decode("<p>this -&gt; &quot;</p>", 'ENT_NOQUOTES');
     // *     returns 1: '<p>this -> &quot;</p>'
     
     string = string.toString();
     
     // Always encode
-    string = string.replace('/&amp;/g', '&');
-    string = string.replace('/&lt;/g', '<');
-    string = string.replace(/&gt;/g, '>');
+    string = string.replace(/&amp;/g, '&');
+    string = string.replace(/&lt;/g, '<');
+    string = string.replace(/&gt;/g '>');
     
     // Encode depending on quote_style
     if (quote_style == 'ENT_QUOTES') {
-        string = string.replace('/&quot;/g', '"');
-        string = string.replace('/&#039;/g', '\'');
+        string = string.replace(/&quot;/g, '"');
+        string = string.replace(/&#039;/g, '\'');
     } else if (quote_style != 'ENT_NOQUOTES') {
         // All other cases (ENT_COMPAT, default, but not ENT_NOQUOTES)
-        string = string.replace('/&quot;/g', '"');
+        string = string.replace(/&quot;/g, '"');
     }
     
     return string;
