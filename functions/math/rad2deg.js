7ad00497c395cf0508b55d6ab16baac6c2d57800
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-10-01 13:17:33 +0000

    Fixed bad example

--- functions/math/rad2deg.js
+++ functions/math/rad2deg.js
@@ -2,7 +2,7 @@ function rad2deg(angle) {
     // http://kevin.vanzonneveld.net
     // +   original by: Enrique Gonzalez
     // *     example 1: rad2deg(3.141592653589793);
-    // *     returns 1: 45
+    // *     returns 1: 180
     
     return (angle/Math.PI) * 180;
 }
