074d39550db0ad414df90a0a0c6fc069bd1c9ca0
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-10-01 12:24:11 +0000

    Added 2 functions by Enrique González

--- functions/math/deg2rad.js
+++ functions/math/deg2rad.js
@@ -1,6 +1,6 @@
 function deg2rad(angle) {
     // http://kevin.vanzonneveld.net
-    // +   original by: Enrique González
+    // +   original by: Enrique Gonzalez
     // *     example 1: deg2rad(45);
     // *     returns 1: 0.7853981633974483
     
--- functions/math/rad2deg.js
+++ functions/math/rad2deg.js
@@ -1,6 +1,6 @@
 function rad2deg(angle) {
     // http://kevin.vanzonneveld.net
-    // +   original by: Enrique González
+    // +   original by: Enrique Gonzalez
     // *     example 1: rad2deg(3.141592653589793);
     // *     returns 1: 45
     
