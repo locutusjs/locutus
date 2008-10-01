3be0667c007af8bbf1806f8a685e046ca16ba54f
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-10-01 10:34:03 +0000

    Added fmod

---
+++ functions/math/fmod.js
@@ -0,0 +1,30 @@
+function fmod(x, y) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Onno Marsman
+    // %          note: The example currently returns 0.8 and not PHP-manual's 0.5  
+    // *     example 1: fmod(4.7, 1.3);
+    // *     returns 1: 0.5
+    
+    var tmp, tmp2, p = 0, pY = 0, l = 0.0, l2 = 0.0;
+    
+    tmp = x.toExponential().match(/^.\.?(.*)e(.+)$/);
+    p = parseInt(tmp[2])-(tmp[1]+'').length;
+    tmp = y.toExponential().match(/^.\.?(.*)e(.+)$/);
+    pY = parseInt(tmp[2])-(tmp[1]+'').length;
+    
+    if (pY > p) {
+        p = pY;
+    }
+    
+    tmp2 = (x%y);
+    
+    if (p < -100 || p > 20) {
+        // toFixed will give an out of bound error so we fix it like this:
+        var l = Math.round(Math.log(tmp2)/Math.log(10));
+        var l2 = Math.pow(10, l);
+        
+        return (tmp2/l2).toFixed(l-p)*l2;
+    } else {
+        return parseFloat(tmp2.toFixed(-p));
+    }
+}
