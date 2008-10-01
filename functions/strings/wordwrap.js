dde3a130360cef1dc973e770f55e2497ac561c83
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-10-01 10:23:18 +0000

    Added PHP defaults & more examples to wordwrap()

--- functions/strings/wordwrap.js
+++ functions/strings/wordwrap.js
@@ -3,16 +3,27 @@ function wordwrap( str, int_width, str_break, cut ) {
     // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
     // +   improved by: Nick Callen
     // +    revised by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: Sakimori
     // *     example 1: wordwrap('Kevin van Zonneveld', 6, '|', true);
     // *     returns 1: 'Kevin |van |Zonnev|eld'
+    // *     example 2: wordwrap('The quick brown fox jumped over the lazy dog.', 20, '<br />\n');
+    // *     returns 2: 'The quick brown fox <br />\njumped over the lazy<br />\n dog.'
+    // *     example 3: wordwrap('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
+    // *     returns 3: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim \nveniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea \ncommodo consequat.'
     
-    var m = int_width, b = str_break, c = cut;
+    // PHP Defaults
+    var m = ((arguments.length >= 2) ? arguments[1] : 75   );
+    var b = ((arguments.length >= 3) ? arguments[2] : "\n" );
+    var c = ((arguments.length >= 4) ? arguments[3] : false);
+    
     var i, j, l, s, r;
     
-    if(m < 1) {
+    if (m < 1) {
         return str;
     }
-    for(i = -1, l = (r = str.split("\n")).length; ++i < l; r[i] += s) {
+    
+    for (i = -1, l = (r = str.split("\n")).length; ++i < l; r[i] += s) {
         for(s = r[i], r[i] = ""; s.length > m; r[i] += s.slice(0, j) + ((s = s.slice(j)).length ? b : "")){
             j = c == 2 || (j = s.slice(0, m + 1).match(/\S*(\s)?$/))[1] ? m : j.input.length - j[0].length || c == 1 && m || j.input.length + (j = s.slice(m).match(/^\S*/)).input.length;
         }
