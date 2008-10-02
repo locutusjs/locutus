32581d63b026c529811940ab705fc6e716f93fcd
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-10-02 21:55:34 +0000

    Added 2 hex functions

---
+++ functions/math/dechex.js
@@ -0,0 +1,10 @@
+function dechex(number) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Philippe Baumann
+    // *     example 1: dechex(10);
+    // *     returns 1: 'a'
+    // *     example 2: dechex(47);
+    // *     returns 2: '2f'
+    
+    return number.toString(16);
+}
