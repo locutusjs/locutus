8fbf691f6c238e98ce88c56da5ccaa59dc614b8a
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-10-03 13:07:22 +0000

    Added decbin decoct

---
+++ functions/math/decbin.js
@@ -0,0 +1,10 @@
+function decbin(number) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Enrique Gonzalez
+    // *     example 1: decbin(12);
+    // *     returns 1: '1100'
+    // *     example 2: decbin(26);
+    // *     returns 2: '11010'
+    
+    return number.toString(2);
+}
---
+++ functions/math/decoct.js
@@ -0,0 +1,10 @@
+function decoct(number) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Enrique Gonzalez
+    // *     example 1: decoct(15);
+    // *     returns 1: '17'
+    // *     example 2: decoct(264);
+    // *     returns 2: '410'
+    
+    return number.toString(8);
+}
