0816fb454b7722f8438a2ed039492d94e2b8edea
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-10-03 13:09:53 +0000

    Added decbin decoct

--- functions/math/decoct.js
+++ functions/math/decoct.js
@@ -3,7 +3,7 @@ function decoct(number) {
     // +   original by: Enrique Gonzalez
     // *     example 1: decoct(15);
     // *     returns 1: '17'
-    // *     example 2: decoct(264);
+    // *     example 2: decoct(264); 
     // *     returns 2: '410'
     
     return number.toString(8);
