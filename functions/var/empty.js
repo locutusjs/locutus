1bc6066cb52de28ae2852a34f05d156d71f0af8a
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-10-06 14:48:52 +0000

    Improved empty

--- functions/var/empty.js
+++ functions/var/empty.js
@@ -4,22 +4,32 @@ function empty( mixed_var ) {
     // +      input by: Onno Marsman
     // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +      input by: LH
+    // +   improved by: Onno Marsman
     // *     example 1: empty(null);
     // *     returns 1: true
     // *     example 2: empty(undefined);
     // *     returns 2: true
     // *     example 3: empty([]);
     // *     returns 3: true
+    // *     example 4: empty({});
+    // *     returns 4: true
     
-    if (mixed_var === "" 
-        || mixed_var === 0   
+    var key;
+    
+    if (mixed_var === ""
+        || mixed_var === 0
         || mixed_var === "0"
-        || mixed_var === null  
+        || mixed_var === null
         || mixed_var === false
-        || mixed_var === undefined    
-        || ((typeof mixed_var == 'array' || typeof mixed_var == 'object') && mixed_var.length === 0) ){
+        || mixed_var === undefined
+    ){
         return true;
     }
-    
+    if (typeof mixed_var == 'object') {
+        for (key in mixed_var) {
+            return false;
+        }
+        return true;
+    }
     return false;
 }
