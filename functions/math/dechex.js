44d6e0d26dca2fed93e7fe438afcff32056acbff
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-10-06 09:59:59 +0000

    Many bugfixes & additions by Onno Marsman & Philippe Baumann

--- functions/math/dechex.js
+++ functions/math/dechex.js
@@ -1,10 +1,11 @@
 function dechex(number) {
     // http://kevin.vanzonneveld.net
     // +   original by: Philippe Baumann
+    // +   bugfixed by: Onno Marsman
     // *     example 1: dechex(10);
     // *     returns 1: 'a'
     // *     example 2: dechex(47);
     // *     returns 2: '2f'
     
-    return number.toString(16);
+    return parseInt(number).toString(16);
 }
--- functions/strings/trim.js
+++ functions/strings/trim.js
@@ -8,12 +8,17 @@ function trim( str, charlist ) {
     // +      input by: DxGx
     // +   improved by: Steven Levithan (http://blog.stevenlevithan.com)
     // +    tweaked by: Jack
+    // +   bugfixed by: Onno Marsman
     // *     example 1: trim('    Kevin van Zonneveld    ');
     // *     returns 1: 'Kevin van Zonneveld'
     // *     example 2: trim('Hello World', 'Hdle');
     // *     returns 2: 'o Wor'
+    // *     example 3: trim(16, 1);
+    // *     returns 3: 6
 
     var whitespace, l = 0, i = 0;
+    str += '';
+    charlist += '';
     
     if (!charlist) {
         whitespace = ' \n\r\t\f\x0b\xa0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000';
