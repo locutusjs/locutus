fe36b0ded64b472849facc7f4c14ae476284b8e1
Onno Marsman <onno.marsman@gmail.com>
2008-10-06 17:41:45 +0000

    Bugfixed strripos to be weakly typed

--- functions/strings/strripos.js
+++ functions/strings/strripos.js
@@ -1,9 +1,10 @@
 function strripos( haystack, needle, offset){
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
     // *     example 1: strripos('Kevin van Zonneveld', 'E');
     // *     returns 1: 16
 
-    var i = haystack.toLowerCase().lastIndexOf( needle.toLowerCase(), offset ); // returns -1
+    var i = (haystack+'').toLowerCase().lastIndexOf( (needle+'').toLowerCase(), offset ); // returns -1
     return i >= 0 ? i : false;
 }
