18fd96202c878323d8f19e439b2f148e0c3a012e
Onno Marsman <onno.marsman@gmail.com>
2008-10-07 17:00:22 +0000

    Yet another fix for a function that didn't allow non-string types to be passed.

--- functions/strings/strrpos.js
+++ functions/strings/strrpos.js
@@ -1,9 +1,10 @@
 function strrpos( haystack, needle, offset){
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
     // *     example 1: strrpos('Kevin van Zonneveld', 'e');
     // *     returns 1: 16
 
-    var i = haystack.lastIndexOf( needle, offset ); // returns -1
+    var i = (haystack+'').lastIndexOf( needle, offset ); // returns -1
     return i >= 0 ? i : false;
 }
