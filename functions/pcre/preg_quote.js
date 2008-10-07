3bcd1c8eb60ece32458541e2c308a7e4bc4e09bd
Onno Marsman <onno.marsman@gmail.com>
2008-10-07 16:57:11 +0000

    Added even more fixes for string functions that didn't allow non-string types to be passed.

--- functions/pcre/preg_quote.js
+++ functions/pcre/preg_quote.js
@@ -3,6 +3,7 @@ function preg_quote( str ) {
     // +   original by: booeyOH
     // +   improved by: Ates Goral (http://magnetiq.com)
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
     // *     example 1: preg_quote("$40");
     // *     returns 1: '\$40'
     // *     example 2: preg_quote("*RRRING* Hello?");
@@ -10,5 +11,5 @@ function preg_quote( str ) {
     // *     example 3: preg_quote("\\.+*?[^]$(){}=!<>|:");
     // *     returns 3: '\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:'
 
-    return str.replace(/([\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:])/g, "\\$1");
+    return (str+'').replace(/([\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:])/g, "\\$1");
 }
--- functions/url/base64_decode.js
+++ functions/url/base64_decode.js
@@ -3,19 +3,22 @@ function base64_decode( data ) {
     // +   original by: Tyler Akins (http://rumkin.com)
     // +   improved by: Thunder.m
     // +      input by: Aman Gupta
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)    
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
     // -    depends on: utf8_decode
     // *     example 1: base64_decode('S2V2aW4gdmFuIFpvbm5ldmVsZA==');
     // *     returns 1: 'Kevin van Zonneveld'
-    
-    // mozilla has this native 
+
+    // mozilla has this native
     // - but breaks in 2.0.0.12!
     //if (typeof window['btoa'] == 'function') {
     //    return btoa(data);
     //}
-    
+
     var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
     var o1, o2, o3, h1, h2, h3, h4, bits, i = ac = 0, dec = "", tmp_arr = [];
+
+    data += '';
 
     do {  // unpack four hexets into three octets using index points in b64
         h1 = b64.indexOf(data.charAt(i++));
@@ -37,9 +40,9 @@ function base64_decode( data ) {
             tmp_arr[ac++] = String.fromCharCode(o1, o2, o3);
         }
     } while (i < data.length);
-    
+
     dec = tmp_arr.join('');
     dec = utf8_decode(dec);
-    
+
     return dec;
 }
