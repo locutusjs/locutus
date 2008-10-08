faef991f5d598917a7aebce0994b2ca2320308a6
Onno Marsman <onno.marsman@gmail.com>
2008-10-08 21:19:57 +0000

    added functions mt_rand, getrandmax and mt_getrandmax

---
+++ functions/math/getrandmax.js
@@ -0,0 +1,8 @@
+function getrandmax()
+{
+    // http://kevin.vanzonneveld.net
+    // +   original by: Onno Marsman
+    // *     example 1: getrandmax();
+    // *     returns 1: 2147483647
+    return 2147483647;
+}
---
+++ functions/math/mt_rand.js
@@ -0,0 +1,14 @@
+function mt_rand( min, max ) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Onno Marsman
+    // *     example 1: mt_rand(1, 1);
+    // *     returns 1: 1
+    var argc = arguments.length;
+    if (argc == 0) {
+        min = 0;
+        max = 2147483647;
+    } else if (argc == 1) {
+        throw new Error('Warning: mt_rand() expects exactly 2 parameters, 1 given');
+    }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
