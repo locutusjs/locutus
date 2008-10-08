22641c8e692de7c3b12e20bcb151a5f9a1da9b26
Onno Marsman <onno.marsman@gmail.com>
2008-10-08 21:32:31 +0000

    added function is_bool

---
+++ functions/var/is_bool.js
@@ -0,0 +1,11 @@
+function is_bool(mixed_var)
+{
+    // http://kevin.vanzonneveld.net
+    // +   original by: Onno Marsman
+    // *     example 1: is_bool(false);
+    // *     returns 1: true
+    // *     example 2: is_bool(0);
+    // *     returns 2: false
+
+    return (typeof mixed_var == 'boolean');
+}
