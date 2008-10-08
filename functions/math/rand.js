3ce1966f983f657ab800ae9aa61a81117a4553c9
Onno Marsman <onno.marsman@gmail.com>
2008-10-08 21:11:14 +0000

    Fixed rand to accept just 0 or 2 parameters like in PHP instead of 1 or 2 parameters.

--- functions/math/rand.js
+++ functions/math/rand.js
@@ -1,12 +1,15 @@
 function rand( min, max ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Leslie Hoare
+    // +   bugfixed by: Onno Marsman
     // *     example 1: rand(1, 1);
     // *     returns 1: 1
-
-    if( max ) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    } else {
-        return Math.floor(Math.random() * (min + 1));
+    var argc = arguments.length;
+    if (argc == 0) {
+        min = 0;
+        max = 2147483647;
+    } else if (argc == 1) {
+        throw new Error('Warning: rand() expects exactly 2 parameters, 1 given');
     }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
