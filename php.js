695a7bc32f1230567327218ad58d3a6954ac0bcb
PHP.JS Automation <info@phpjs.org>
2008-10-13 09:45:43 +0000

    generated

--- php.js
+++ php.js
@@ -2729,7 +2729,7 @@ function sleep(seconds) {
     // %          note: For study purposes. Current implementation could lock up the user's browser. 
     // %          note: Consider using setTimeout() instead.
     // *     example 1: sleep(1);
-    // *     returns 1: 0;
+    // *     returns 1: 0
     
     seconds *= 1000;
     var start = new Date().getTime();
--- php.namespaced.js
+++ php.namespaced.js
@@ -2744,7 +2744,7 @@
             // %          note: For study purposes. Current implementation could lock up the user's browser. 
             // %          note: Consider using setTimeout() instead.
             // *     example 1: $P.sleep(1);
-            // *     returns 1: 0;
+            // *     returns 1: 0
             
             seconds *= 1000;
             var start = new Date().getTime();
