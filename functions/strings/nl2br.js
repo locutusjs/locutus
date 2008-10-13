bc59cd2a40ae2a8bf2a807566fa90295d5fbdf5a
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-10-13 09:25:10 +0000

    Greatly improved nl2br. Thank you Atli Þór

--- functions/strings/nl2br.js
+++ functions/strings/nl2br.js
@@ -1,10 +1,18 @@
-function nl2br( str ) {
+function nl2br (str, is_xhtml) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +   improved by: Philip Peterson
     // +   improved by: Onno Marsman
+    // +   improved by: Atli Þór
     // *     example 1: nl2br('Kevin\nvan\nZonneveld');
     // *     returns 1: 'Kevin<br />\nvan<br />\nZonneveld'
-
-    return (str + '').replace(/([^>])\n/g, '$1<br />\n');
+    // *     example 2: nl2br("\nOne\nTwo\n\nThree\n", false);
+    // *     returns 2: '<br>\nOne<br>\nTwo<br>\n<br>\nThree<br>\n'
+    
+    breakTag = "<br />";
+    if (typeof is_xhtml == "boolean" || is_xhtml == false) {
+        breakTag = "<br>";
+    }
+    
+    return (str + '').replace(/([^>]?)\n/g, '$1'+ breakTag +'\n');
 }
