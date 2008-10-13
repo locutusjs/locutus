7a6015670886e0376f1e4f3e4e8a851b90d6d6e1
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-10-13 09:30:19 +0000

    Added sleep

--- functions/misc/sleep.js
+++ functions/misc/sleep.js
@@ -1,7 +1,8 @@
 function sleep(seconds) {
     // http://kevin.vanzonneveld.net
     // +   original by: Christian Doebler
-    // %          note: Could lock up the user's browser. Consider using setTimeout() instead.
+    // %          note: For study purposes. Current implementation could lock up the user's browser. 
+    // %          note: Consider using setTimeout() instead.
     // *     example 1: sleep(1);
     // *     returns 1: 0;
     
