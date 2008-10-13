f2c9d11385134bd5bdf8709f7e3cfb5a6e403618
Onno Marsman <onno.marsman@gmail.com>
2008-10-13 17:25:32 +0000

    Bugfixed nl2br, failed when true was passed for is_xhtml

--- functions/strings/nl2br.js
+++ functions/strings/nl2br.js
@@ -3,16 +3,19 @@ function nl2br (str, is_xhtml) {
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +   improved by: Philip Peterson
     // +   improved by: Onno Marsman
-    // +   improved by: Atli ﬁÛr
+    // +   improved by: Atli √û√≥r
+    // +   bugfixed by: Onno Marsman
     // *     example 1: nl2br('Kevin\nvan\nZonneveld');
     // *     returns 1: 'Kevin<br />\nvan<br />\nZonneveld'
     // *     example 2: nl2br("\nOne\nTwo\n\nThree\n", false);
     // *     returns 2: '<br>\nOne<br>\nTwo<br>\n<br>\nThree<br>\n'
-    
-    breakTag = "<br />";
-    if (typeof is_xhtml == "boolean" || is_xhtml == false) {
-        breakTag = "<br>";
+    // *     example 3: nl2br("\nOne\nTwo\n\nThree\n", true);
+    // *     returns 3: '<br />\nOne<br />\nTwo<br />\n<br />\nThree<br />\n'
+
+    breakTag = '<br />';
+    if (typeof is_xhtml != 'undefined' && !is_xhtml) {
+        breakTag = '<br>';
     }
-    
+
     return (str + '').replace(/([^>]?)\n/g, '$1'+ breakTag +'\n');
 }
