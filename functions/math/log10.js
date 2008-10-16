5e3f7db2b6291c498a9173ee4f168f9c578f169c
Onno Marsman <onno.marsman@gmail.com>
2008-10-16 07:31:01 +0000

    dev

---
+++ functions/math/log10.js
@@ -0,0 +1,11 @@
+function log10(arg) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Philip Peterson
+    // +   improved by: Onno Marsman
+    // *     example 1: log10(10);
+    // *     returns 1: 1
+    // *     example 2: log10(1);
+    // *     returns 2: 0
+
+    return Math.log(arg)/Math.log(10);
+}
