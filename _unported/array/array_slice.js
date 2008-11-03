a2875b5dfdb33b0a77ac7b9194876ef5791e5f40
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-11-03 10:22:25 +0000

    Can't move array_slice to functions yet

--- _unported/array/array_slice.js
+++ _unported/array/array_slice.js
@@ -1,1 +1,45 @@
-// not yet ported. feel like it?
+function array_slice(arr, offst, lgth, preserve_keys) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir
+    // *     example 1: array_slice(["a", "b", "c", "d", "e"], 2, -1);
+    // *     returns 1: {0: 'c', 1: 'd'}
+    // *     example 2: array_slice(["a", "b", "c", "d", "e"], 2, -1, true);
+    // *     returns 2: {2: 'c', 3: 'd'}
+    
+	if (!(arr instanceof Array) || (preserve_keys && offst != 0)) { // Assoc. array as input or if required as output
+		var lgt =0, newAssoc = {};
+		for (var key in arr) {
+			if (key !== 'length') {
+				lgt += 1;
+				newAssoc[key] = arr[key];
+			}
+		}
+		arr = newAssoc;
+		
+		offst = (offst < 0) ? lgt + offst : offst;
+		lgth  = lgth == undefined ? lgt : (lgth < 0) ? lgt + lgth - offst : lgth;
+		
+		var assoc = {};
+		var start = false, it=-1, arrlgth=0, no_pk_idx=0;
+		for (var key in arr) {
+		    ++it;
+		    if (arrlgth >= lgth) {
+		      break;
+		    }
+		    if (it == offst){
+		      start = true;
+		    }
+		    if (!start) {
+		       continue;
+		    }
+		    ++arrlgth;
+		    if (!isNaN(key) && !preserve_keys) {
+                assoc[no_pk_idx++] = arr[key];
+		    } else {
+		        assoc[key] = arr[key];
+		    }
+		}
+		assoc.length = arrlgth; // Make as array-like object (though length will not be dynamic)
+		return assoc;
+	}
+}
