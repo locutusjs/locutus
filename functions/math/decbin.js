6042e99ff1904a37fe7533765549a56da8d6c2b1
Onno Marsman <onno.marsman@gmail.com>
2008-11-07 07:15:09 +0000

    Fixed decbin for when a string was passed

--- functions/math/decbin.js
+++ functions/math/decbin.js
@@ -1,10 +1,13 @@
 function decbin(number) {
     // http://kevin.vanzonneveld.net
     // +   original by: Enrique Gonzalez
+    // +   bugfixed by: Onno Marsman
     // *     example 1: decbin(12);
     // *     returns 1: '1100'
     // *     example 2: decbin(26);
     // *     returns 2: '11010'
+    // *     example 3: decbin('26');
+    // *     returns 3: '11010'
     
-    return number.toString(2);
+    return parseInt(number).toString(2);
 }
