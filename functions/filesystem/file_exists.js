2a46dd79226875066bf59bf65c015be0d455863a
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-11-13 22:04:23 +0000

    Added 2 functions

---
+++ functions/filesystem/file_exists.js
@@ -0,0 +1,21 @@
+function file_exists (url) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Enrique González
+    // %        note 1: This function uses XmlHttpRequest and cannot retrieve resource from different domain.
+    // %        note 1: Mainly here for study purposes. 
+    // *     example 1: file_get_contents('http://kevin.vanzonneveld.net/pj_test_supportfile_1.htm');
+    // *     returns 1: '123'
+
+    var req = null;
+    try { req = new ActiveXObject("Msxml2.XMLHTTP"); } catch (e) {
+        try { req = new ActiveXObject("Microsoft.XMLHTTP"); } catch (e) {
+            try { req = new XMLHttpRequest(); } catch(e) {}
+        }
+    }
+    if (req == null) throw new Error('XMLHttpRequest not supported');
+    
+    req.open("GET", url, false);
+    req.send(null);
+    
+    return req.responseText;
+}
