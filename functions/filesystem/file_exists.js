ea800742b21be41ffaced57be331f27dbfbca74a
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-11-18 11:23:33 +0000

    Creditted Jani Hartikainen

--- functions/filesystem/file_exists.js
+++ functions/filesystem/file_exists.js
@@ -1,6 +1,8 @@
 function file_exists (url) {
     // http://kevin.vanzonneveld.net
     // +   original by: Enrique Gonzalez
+    // +      input by: Jani Hartikainen
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // %        note 1: This function uses XmlHttpRequest and cannot retrieve resource from different domain.
     // %        note 1: Synchronous so may lock up browser, mainly here for study purposes. 
     // *     example 1: file_exists('http://kevin.vanzonneveld.net/pj_test_supportfile_1.htm');
--- functions/filesystem/file_get_contents.js
+++ functions/filesystem/file_get_contents.js
@@ -1,6 +1,8 @@
 function file_get_contents( url ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Legaev Andrey
+    // +      input by: Jani Hartikainen
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // %        note 1: This function uses XmlHttpRequest and cannot retrieve resource from different domain.
     // %        note 1: Synchronous so may lock up browser, mainly here for study purposes. 
     // %        note 1: To avoid browser blocking issues's consider using jQuery's: $('#divId').load('http://url') instead.
