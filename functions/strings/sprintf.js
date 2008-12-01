0c6bdfac5692a2a7d3c3c1e810775d446ff958f2
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-12-01 08:13:14 +0000

    implementation missed the custom padding character: printf("[%'#10s]\n",  $s);

--- functions/strings/sprintf.js
+++ functions/strings/sprintf.js
@@ -4,24 +4,31 @@ function sprintf( ) {
     // + namespaced by: Michael White (http://getsprink.com)
     // +    tweaked by: Jack
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: Paulo Ricardo F. Santos
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // *     example 1: sprintf("%01.2f", 123.1);
     // *     returns 1: 123.10
+    // *     example 2: sprintf("[%10s]", 'monkey');
+    // *     returns 2: '[    monkey]'
+    // *     example 3: sprintf("[%'#10s]", 'monkey');
+    // *     returns 3: '[####monkey]'
 
-    var regex = /%%|%(\d+\$)?([-+#0 ]*)(\*\d+\$|\*|\d+)?(\.(\*\d+\$|\*|\d+))?([scboxXuidfegEG])/g;
+    var regex = /%%|%(\d+\$)?([-+\'#0 ]*)(\*\d+\$|\*|\d+)?(\.(\*\d+\$|\*|\d+))?([scboxXuidfegEG])/g;
     var a = arguments, i = 0, format = a[i++];
 
     // pad()
     var pad = function(str, len, chr, leftJustify) {
+        if (!chr) chr = ' ';
         var padding = (str.length >= len) ? '' : Array(1 + len - str.length >>> 0).join(chr);
         return leftJustify ? str + padding : padding + str;
     };
 
     // justify()
-    var justify = function(value, prefix, leftJustify, minWidth, zeroPad) {
+    var justify = function(value, prefix, leftJustify, minWidth, zeroPad, customPadChar) {
         var diff = minWidth - value.length;
         if (diff > 0) {
             if (leftJustify || !zeroPad) {
-                value = pad(value, minWidth, ' ', leftJustify);
+                value = pad(value, minWidth, customPadChar, leftJustify);
             } else {
                 value = value.slice(0, prefix.length) + pad('', diff, '0', true) + value.slice(prefix.length);
             }
@@ -39,11 +46,11 @@ function sprintf( ) {
     };
 
     // formatString()
-    var formatString = function(value, leftJustify, minWidth, precision, zeroPad) {
+    var formatString = function(value, leftJustify, minWidth, precision, zeroPad, customPadChar) {
         if (precision != null) {
             value = value.slice(0, precision);
         }
-        return justify(value, '', leftJustify, minWidth, zeroPad);
+        return justify(value, '', leftJustify, minWidth, zeroPad, customPadChar);
     };
 
     // finalFormat()
@@ -51,12 +58,13 @@ function sprintf( ) {
         if (substring == '%%') return '%';
 
         // parse flags
-        var leftJustify = false, positivePrefix = '', zeroPad = false, prefixBaseX = false;
+        var leftJustify = false, positivePrefix = '', zeroPad = false, prefixBaseX = false, customPadChar = ' ';
         var flagsl = flags.length;
         for (var j = 0; flags && j < flagsl; j++) switch (flags.charAt(j)) {
             case ' ': positivePrefix = ' '; break;
             case '+': positivePrefix = '+'; break;
             case '-': leftJustify = true; break;
+            case "'": customPadChar = flags.charAt(j+1); break;
             case '0': zeroPad = true; break;
             case '#': prefixBaseX = true; break;
         }
@@ -97,7 +105,7 @@ function sprintf( ) {
         var value = valueIndex ? a[valueIndex.slice(0, -1)] : a[i++];
 
         switch (type) {
-            case 's': return formatString(String(value), leftJustify, minWidth, precision, zeroPad);
+            case 's': return formatString(String(value), leftJustify, minWidth, precision, zeroPad, customPadChar);
             case 'c': return formatString(String.fromCharCode(+value), leftJustify, minWidth, precision, zeroPad);
             case 'b': return formatBaseX(value, 2, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
             case 'o': return formatBaseX(value, 8, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
@@ -106,25 +114,24 @@ function sprintf( ) {
             case 'u': return formatBaseX(value, 10, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
             case 'i':
             case 'd': {
-                        var number = parseInt(+value);
-                        var prefix = number < 0 ? '-' : positivePrefix;
-                        value = prefix + pad(String(Math.abs(number)), precision, '0', false);
-                        return justify(value, prefix, leftJustify, minWidth, zeroPad);
-                    }
+                var number = parseInt(+value);
+                var prefix = number < 0 ? '-' : positivePrefix;
+                value = prefix + pad(String(Math.abs(number)), precision, '0', false);
+                return justify(value, prefix, leftJustify, minWidth, zeroPad);
+            }
             case 'e':
             case 'E':
             case 'f':
             case 'F':
             case 'g':
-            case 'G':
-                        {
-                        var number = +value;
-                        var prefix = number < 0 ? '-' : positivePrefix;
-                        var method = ['toExponential', 'toFixed', 'toPrecision']['efg'.indexOf(type.toLowerCase())];
-                        var textTransform = ['toString', 'toUpperCase']['eEfFgG'.indexOf(type) % 2];
-                        value = prefix + Math.abs(number)[method](precision);
-                        return justify(value, prefix, leftJustify, minWidth, zeroPad)[textTransform]();
-                    }
+            case 'G': {
+                var number = +value;
+                var prefix = number < 0 ? '-' : positivePrefix;
+                var method = ['toExponential', 'toFixed', 'toPrecision']['efg'.indexOf(type.toLowerCase())];
+                var textTransform = ['toString', 'toUpperCase']['eEfFgG'.indexOf(type) % 2];
+                value = prefix + Math.abs(number)[method](precision);
+                return justify(value, prefix, leftJustify, minWidth, zeroPad)[textTransform]();
+            }
             default: return substring;
         }
     };
