a42e35744be9e207dc789765f444845d62e965ee
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-12-01 14:05:11 +0000

    Added chunk_split

--- functions/array/chunk_split.js
+++ functions/array/chunk_split.js
@@ -2,9 +2,9 @@ function chunk_split(body, chunklen, end) {
     // http://kevin.vanzonneveld.net
     // +   original by: Paulo Ricardo F. Santos
     // *     example 1: chunk_split('Hello world!', 1, '*');
-    // *     returns 1: 'H*e*l*l*o* *w*o*r*l*d*!'
+    // *     returns 1: 'H*e*l*l*o* *w*o*r*l*d*!*'
     // *     example 2: chunk_split('Hello world!', 10, '*');
-    // *     returns 2: 'Hello worl*d!'
+    // *     returns 2: 'Hello worl*d!*'
     
     if (chunklen < 1) {
         return false;
