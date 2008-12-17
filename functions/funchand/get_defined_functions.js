8ee9425a2f879c73a898020b7a6353a1b5597868
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-12-17 13:36:18 +0000

    5 functions by Brett Zamir

---
+++ functions/funchand/get_defined_functions.js
@@ -0,0 +1,24 @@
+function get_defined_functions() {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir
+    // %        note 1: Test case 1: If get_defined_functions can find itself in the defined functions, it worked :)
+    // *     example 1: function test_in_array(array, p_val) {for(var i = 0, l = array.length; i < l; i++) {if(array[i] == p_val) return true;} return false;}
+    // *     example 1: funcs = get_defined_functions();
+    // *     example 1: found = test_in_array(funcs, 'get_defined_functions');
+    // *     results 1: found == true
+
+    var i = '', arr = [];
+
+    for (i in window) {
+        try {
+            if (typeof window[i] === 'function' && window[i].name) {
+                arr.push(window[i].name);
+            }
+        }
+        catch (e) {
+
+        }
+    }
+
+    return arr;
+}
