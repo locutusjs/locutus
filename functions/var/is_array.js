2b37f528ffab4bb73a3be6998efbcee232ee5137
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-12-17 11:53:08 +0000

    Simplified is_array by Onno Marsman

--- functions/var/is_array.js
+++ functions/var/is_array.js
@@ -4,11 +4,12 @@ function is_array( mixed_var ) {
     // +   improved by: Legaev Andrey
     // +   bugfixed by: Cord
     // +   bugfixed by: Manish
+    // +   improved by: Onno Marsman
     // %        note 1: In php.js, javascript objects are like php associative arrays 
     // *     example 1: is_array(['Kevin', 'van', 'Zonneveld']);
     // *     returns 1: true
     // *     example 2: is_array('Kevin van Zonneveld');
     // *     returns 2: false
 
-    return (mixed_var instanceof Array || mixed_var instanceof Object);
+    return (mixed_var instanceof Object);
 }
