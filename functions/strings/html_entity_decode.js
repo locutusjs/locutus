bc9500455ae6d48c852d6eb30c6c507e813f2c07
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-12-30 15:34:37 +0000

    Established html_entity_decode has the same bug as http://bugs.php.net/bug.php?id=25707

--- functions/strings/html_entity_decode.js
+++ functions/strings/html_entity_decode.js
@@ -7,9 +7,12 @@ function html_entity_decode( string, quote_style ) {
     // +   bugfixed by: Onno Marsman
     // +   improved by: marc andreu
     // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // %        note 1: Has this issue: http://bugs.php.net/bug.php?id=25707, see example 2
     // -    depends on: get_html_translation_table
     // *     example 1: html_entity_decode('Kevin &amp; van Zonneveld');
     // *     returns 1: 'Kevin & van Zonneveld'
+    // *     example 2: html_entity_decode('&amp;lt;');
+    // *     returns 2: '&lt;'
 
     var histogram = {}, symbol = '', tmp_str = '', i = 0;
     tmp_str = string.toString();
