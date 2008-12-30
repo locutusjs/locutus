b19cedff0453e6dfe84dcba26690dbcad2de7eeb
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-12-30 09:58:15 +0000

    funchand improvements

--- functions/funchand/call_user_func.js
+++ functions/funchand/call_user_func.js
@@ -1,14 +1,11 @@
 function call_user_func(cb, parameters) {
     // http://kevin.vanzonneveld.net
     // +   original by: Brett Zamir
-    // +   improved by: 
-    // *     example 1: call_user_func_array('isNaN', ['a']);
+    // *     example 1: call_user_func('isNaN', 'a');
     // *     returns 1: true
-    // *     example 2: call_user_func_array('isNaN', [1]);
-    // *     returns 2: false
 
     var func;
-
+ 
     if (typeof cb == 'string') {
         if (typeof this[cb] == 'function') {
             func = this[cb];
@@ -23,5 +20,5 @@ function call_user_func(cb, parameters) {
         throw new Exception(func + ' is not a valid function');
     }
 
-    func.apply(null, Array.prototype.slice.call(parameters, 1));
+    return func.apply(null, Array.prototype.slice.call(parameters, 1));
 }
