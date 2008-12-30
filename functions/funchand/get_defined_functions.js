44725cb6cf37ff3303f4fa60cce53f95d8c89d1b
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-12-30 11:35:31 +0000

    you actually have to iterate over the window properties which are objects to get a few other globals not returned by a mere iteration of window itself (in Mozilla, these are: QueryInterface, addEventListener, getComputedStyle).

--- functions/funchand/get_defined_functions.js
+++ functions/funchand/get_defined_functions.js
@@ -1,18 +1,30 @@
 function get_defined_functions() {
     // http://kevin.vanzonneveld.net
     // +   original by: Brett Zamir
+    // +   improved by: Brett Zamir
     // %        note 1: Test case 1: If get_defined_functions can find itself in the defined functions, it worked :)
     // *     example 1: function test_in_array(array, p_val) {for(var i = 0, l = array.length; i < l; i++) {if(array[i] == p_val) return true;} return false;}
     // *     example 1: funcs = get_defined_functions();
     // *     example 1: found = test_in_array(funcs, 'get_defined_functions');
     // *     results 1: found == true
 
-    var i = '', arr = [];
+    var i = '', arr = [], already = {};
 
     for (i in window) {
         try {
-            if (typeof window[i] === 'function' && window[i].name) {
-                arr.push(window[i].name);
+            if (typeof window[i] === 'function') {
+                if (!already[i]) {
+                    already[i] = 1;
+                    arr.push(i);
+                }
+            }
+            else if (typeof window[i] === 'object') {
+                for (var j in window[i]) {
+                    if (typeof window[j] === 'function' && window[j] && !already[j]) {
+                        already[j] = 1;
+                        arr.push(j);
+                    }
+                }
             }
         }
         catch (e) {
