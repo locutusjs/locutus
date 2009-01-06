7b784938699adbd712ccc3fe6aca43c777b9b339
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-01-06 16:18:34 +0000

    Fixed sanitation bug

--- functions/array/array_sum.js
+++ functions/array/array_sum.js
@@ -2,18 +2,23 @@ function array_sum( array ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +   bugfixed by: Nate
+    // +   bugfixed by: Gilbert
     // *     example 1: array_sum([4, 9, 182.6]);
     // *     returns 1: 195.6
+    // *     example 2: total = []; index = 0.1; for(y=0; y < 12; y++){total[y] = y + index;}
+    // *     example 2: array_sum(total);
+    // *     returns 2: 67.2
 
-    var key, sum=0;
-
+    var key, sum = 0;
+    
     // input sanitation
-    if( !array || (array.constructor !== Array && array.constructor !== Object) || !array.length ){
+    if (typeof array !== 'object') {
         return null;
     }
-
-    for(key in array){
-        sum += array[key];
+    
+    for (key in array) {
+        //tester_print_r(typeof sum);
+        sum += (array[key] * 1);
     }
 
     return sum;
