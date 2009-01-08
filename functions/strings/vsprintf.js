27dd692fe036268829f869622a19bed78e51d9ac
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-01-08 16:03:21 +0000

    Added vsprintf

---
+++ functions/strings/vsprintf.js
@@ -0,0 +1,9 @@
+function vsprintf(format, args) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: ejsanders
+    // -    depends on: sprintf
+    // *     example 1: vsprintf('%04d-%02d-%02d', [1988, 8, 1]);
+    // *     returns 1: '1988-08-01'
+
+    return sprintf.apply(this, [format].concat(args));
+}
