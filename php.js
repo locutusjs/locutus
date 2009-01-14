1ec2a69bc2039a1ceefff115e309611c2af98db5
PHP.JS Automation <info@phpjs.org>
2009-01-14 14:56:49 +0000

    generated

--- php.js
+++ php.js
@@ -1,7 +1,7 @@
 /* 
  * More info at: http://kevin.vanzonneveld.net/techblog/article/phpjs_licensing/
  * 
- * This is version: 2.03
+ * This is version: 2.04
  * php.js is copyright 2008 Kevin van Zonneveld.
  * 
  * Portions copyright Onno Marsman, Brett Zamir, Michael White
@@ -14,10 +14,10 @@
  * (http://geekfg.blogspot.com), Johnny Mast (http://www.phpvrouwen.nl), d3x,
  * marrtins, AJ, Alex, Alfonso Jimenez (http://www.alfonsojimenez.com), Aman
  * Gupta, Arpad Ray (mailto:arpad@php.net), Karol Kowalski, Mirek Slugen,
- * Sakimori, Thunder.m, Tyler Akins (http://rumkin.com), mdsjack
- * (http://www.mdsjack.bo.it), 0m3r, Alexander Ermolaev
- * (http://snippets.dzone.com/user/AlexanderErmolaev), Allan Jensen
- * (http://www.winternet.no), Andrea Giammarchi
+ * Public Domain (http://www.json.org/json2.js), Sakimori, Thunder.m, Tyler
+ * Akins (http://rumkin.com), mdsjack (http://www.mdsjack.bo.it), 0m3r,
+ * Alexander Ermolaev (http://snippets.dzone.com/user/AlexanderErmolaev),
+ * Allan Jensen (http://www.winternet.no), Andrea Giammarchi
  * (http://webreflection.blogspot.com), Andreas, Andrej Pavlovic, Anton
  * Ongson, Arno, Atli Þór, Bayron Guevara, Ben Bryan, Benjamin Lupton, Brad
  * Touesnard, Bryan Elliott, Cagri Ekin, Caio Ariede (http://caioariede.com),
@@ -3718,6 +3718,236 @@ function get_included_files() {
     }
 
     return includes;
+}// }}}
+
+// {{{ json_decode
+function json_decode(str_json) {
+    // Decodes a JSON string
+    // 
+    // +    discuss at: http://kevin.vanzonneveld.net/techblog/article/javascript_equivalent_for_phps_json_decode/
+    // +       version: 901.1415
+    // +   original by: Public Domain (http://www.json.org/json2.js)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // *     example 1: json_decode('[\n    "e",\n    {\n    "pluribus": "unum"\n}\n]');
+    // *     returns 1: ['e', {pluribus: 'unum'}]
+
+    /*
+        http://www.JSON.org/json2.js
+        2008-11-19
+        Public Domain.
+        NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+        See http://www.JSON.org/js.html
+    */
+
+    var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
+    var j;
+    var text = str_json;
+
+    var walk = function(holder, key) {
+        // The walk method is used to recursively walk the resulting structure so
+        // that modifications can be made.
+        var k, v, value = holder[key];
+        if (value && typeof value === 'object') {
+            for (k in value) {
+                if (Object.hasOwnProperty.call(value, k)) {
+                    v = walk(value, k);
+                    if (v !== undefined) {
+                        value[k] = v;
+                    } else {
+                        delete value[k];
+                    }
+                }
+            }
+        }
+        return reviver.call(holder, key, value);
+    }
+
+    // Parsing happens in four stages. In the first stage, we replace certain
+    // Unicode characters with escape sequences. JavaScript handles many characters
+    // incorrectly, either silently deleting them, or treating them as line endings.
+    cx.lastIndex = 0;
+    if (cx.test(text)) {
+        text = text.replace(cx, function (a) {
+            return '\\u' +
+            ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+        });
+    }
+
+    // In the second stage, we run the text against regular expressions that look
+    // for non-JSON patterns. We are especially concerned with '()' and 'new'
+    // because they can cause invocation, and '=' because it can cause mutation.
+    // But just to be safe, we want to reject all unexpected forms.
+
+    // We split the second stage into 4 regexp operations in order to work around
+    // crippling inefficiencies in IE's and Safari's regexp engines. First we
+    // replace the JSON backslash pairs with '@' (a non-JSON character). Second, we
+    // replace all simple value tokens with ']' characters. Third, we delete all
+    // open brackets that follow a colon or comma or that begin the text. Finally,
+    // we look to see that the remaining characters are only whitespace or ']' or
+    // ',' or ':' or '{' or '}'. If that is so, then the text is safe for eval.
+    if (/^[\],:{}\s]*$/.
+        test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@').
+            replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+            replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+
+        // In the third stage we use the eval function to compile the text into a
+        // JavaScript structure. The '{' operator is subject to a syntactic ambiguity
+        // in JavaScript: it can begin a block or an object literal. We wrap the text
+        // in parens to eliminate the ambiguity.
+
+        j = eval('(' + text + ')');
+
+        // In the optional fourth stage, we recursively walk the new structure, passing
+        // each name/value pair to a reviver function for possible transformation.
+
+        return typeof reviver === 'function' ?
+        walk({
+            '': j
+        }, '') : j;
+    }
+
+    // If the text is not JSON parseable, then a SyntaxError is thrown.
+    throw new SyntaxError('JSON.parse');
+}// }}}
+
+// {{{ json_encode
+function json_encode(mixed_val) {
+    // Returns the JSON representation of a value
+    // 
+    // +    discuss at: http://kevin.vanzonneveld.net/techblog/article/javascript_equivalent_for_phps_json_encode/
+    // +       version: 901.1415
+    // +   original by: Public Domain (http://www.json.org/json2.js)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // *     example 1: json_encode(['e', {pluribus: 'unum'}]);
+    // *     returns 1: '[\n    "e",\n    {\n    "pluribus": "unum"\n}\n]'
+
+    /*
+        http://www.JSON.org/json2.js
+        2008-11-19
+        Public Domain.
+        NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+        See http://www.JSON.org/js.html
+    */
+    
+    var indent;
+    var value = mixed_val;
+    var i;
+
+    var quote = function (string) {
+        var escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
+        var meta = {    // table of character substitutions
+            '\b': '\\b',
+            '\t': '\\t',
+            '\n': '\\n',
+            '\f': '\\f',
+            '\r': '\\r',
+            '"' : '\\"',
+            '\\': '\\\\'
+        };
+
+        escapable.lastIndex = 0;
+        return escapable.test(string) ?
+        '"' + string.replace(escapable, function (a) {
+            var c = meta[a];
+            return typeof c === 'string' ? c :
+            '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+        }) + '"' :
+        '"' + string + '"';
+    }
+
+    var str = function(key, holder) {
+        var gap = '';
+        var indent = '    ';
+        var i = 0;          // The loop counter.
+        var k = '';          // The member key.
+        var v = '';          // The member value.
+        var length = 0;
+        var mind = gap;
+        var partial = [];
+        var value = holder[key];
+
+        // If the value has a toJSON method, call it to obtain a replacement value.
+        if (value && typeof value === 'object' &&
+            typeof value.toJSON === 'function') {
+            value = value.toJSON(key);
+        }
+        
+        // What happens next depends on the value's type.
+        switch (typeof value) {
+            case 'string':
+                return quote(value);
+
+            case 'number':
+                // JSON numbers must be finite. Encode non-finite numbers as null.
+                return isFinite(value) ? String(value) : 'null';
+
+            case 'boolean':
+            case 'null':
+                // If the value is a boolean or null, convert it to a string. Note:
+                // typeof null does not produce 'null'. The case is included here in
+                // the remote chance that this gets fixed someday.
+
+                return String(value);
+
+            case 'object':
+                // If the type is 'object', we might be dealing with an object or an array or
+                // null.
+                // Due to a specification blunder in ECMAScript, typeof null is 'object',
+                // so watch out for that case.
+                if (!value) {
+                    return 'null';
+                }
+
+                // Make an array to hold the partial results of stringifying this object value.
+                gap += indent;
+                partial = [];
+
+                // Is the value an array?
+                if (Object.prototype.toString.apply(value) === '[object Array]') {
+                    // The value is an array. Stringify every element. Use null as a placeholder
+                    // for non-JSON values.
+
+                    length = value.length;
+                    for (i = 0; i < length; i += 1) {
+                        partial[i] = str(i, value) || 'null';
+                    }
+
+                    // Join all of the elements together, separated with commas, and wrap them in
+                    // brackets.
+                    v = partial.length === 0 ? '[]' :
+                    gap ? '[\n' + gap +
+                    partial.join(',\n' + gap) + '\n' +
+                    mind + ']' :
+                    '[' + partial.join(',') + ']';
+                    gap = mind;
+                    return v;
+                }
+
+                // Iterate through all of the keys in the object.
+                for (k in value) {
+                    if (Object.hasOwnProperty.call(value, k)) {
+                        v = str(k, value);
+                        if (v) {
+                            partial.push(quote(k) + (gap ? ': ' : ':') + v);
+                        }
+                    }
+                }
+
+                // Join all of the member texts together, separated with commas,
+                // and wrap them in braces.
+                v = partial.length === 0 ? '{}' :
+                gap ? '{\n' + gap + partial.join(',\n' + gap) + '\n' +
+                mind + '}' : '{' + partial.join(',') + '}';
+                gap = mind;
+                return v;
+        }
+    };
+
+    // Make a fake root object containing our value under the key of ''.
+    // Return the result of stringifying the value.
+    return str('', {
+        '': value
+    });
 }// }}}
 
 // {{{ include
