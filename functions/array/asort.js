aeb5b0508fbbfade9dd6ac844ace2c37627c83e8
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-01-15 10:27:40 +0000

    Minor issues with asort

--- functions/array/asort.js
+++ functions/array/asort.js
@@ -2,6 +2,7 @@ function asort(inputArr) {
     // http://kevin.vanzonneveld.net
     // +   original by: Brett Zamir
     // %          note: The examples are correct, this is a new way
+    // %          note: Credits to: http://javascript.internet.com/math-related/bubble-sort.html 
     // *     example 1: data = {d: 'lemon', a: 'orange', b: 'banana', c: 'apple'};
     // *     example 1: asort(data);
     // *     results 1: data == {c: 'apple', b: 'banana', d: 'lemon', a: 'orange'}
@@ -11,18 +12,18 @@ function asort(inputArr) {
         var i = 0, j = 0, tempValue = '', tempKeyVal = '';
 
         for (i = inputArr.length-2; i >= 0; i--) {
-            for (j=0; j <= i; j++) {
+            for (j = 0; j <= i; j++) {
                 if (inputArr[j+1] < inputArr[j]) {
-                    tempValue = inputArr[j];
-                    inputArr[j] = inputArr[j+1];
+                    tempValue     = inputArr[j];
+                    inputArr[j]   = inputArr[j+1];
                     inputArr[j+1] = tempValue;
-                    tempKeyVal = keyArr[j];
-                    keyArr[j] = keyArr[j+1];
-                    keyArr[j+1] = tempKeyVal;
+                    tempKeyVal    = keyArr[j];
+                    keyArr[j]     = keyArr[j+1];
+                    keyArr[j+1]   = tempKeyVal;
                 }
             }
         }
-    }
+    };
 
     var valArr = [], keyArr=[], k = '', i = 0;
 
