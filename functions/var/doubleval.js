7442a142f06ab19c5e311197a41d5ad56ca95c72
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-01-25 14:11:35 +0000

    Added settype

---
+++ functions/var/doubleval.js
@@ -0,0 +1,11 @@
+function doubleval( mixed_var ) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir
+    //  -   depends on: floatval
+    // %        note 1: 1.0 is simplified to 1 before it can be accessed by the function, this makes
+    // %        note 1: it different from the PHP implementation. We can't fix this unfortunately.
+    // *     example 1: doubleval(186);
+    // *     returns 1: 186.00
+
+    return floatval(mixed_var);
+}
---
+++ functions/var/is_real.js
@@ -0,0 +1,11 @@
+function is_real( mixed_var ) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir
+    //  -   depends on: is_float
+    // %        note 1: 1.0 is simplified to 1 before it can be accessed by the function, this makes
+    // %        note 1: it different from the PHP implementation. We can't fix this unfortunately.
+    // *     example 1: is_double(186.31);
+    // *     returns 1: true
+
+    return is_float(mixed_var);
+}
