65a6406de01fb5f6a582bd6389452a0da9dca858
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-01-25 13:58:41 +0000

    Improved defined

--- functions/misc/defined.js
+++ functions/misc/defined.js
@@ -2,8 +2,17 @@ function defined( constant_name )  {
     // http://kevin.vanzonneveld.net
     // +   original by: Waldo Malqui Silva
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +    revised by: Brett Zamir
     // *     example 1: defined('IMAGINARY_CONSTANT1');
     // *     returns 1: false
 
-    return (typeof window[constant_name] !== 'undefined');
+    var tmp = window[constant];
+    
+    window[constant] = window[constant] ? 'changed'+window[constant].toString() : 'changed';
+    var returnval = window[constant] === tmp;
+    if (!returnval) { // Reset
+        window[constant] = tmp;
+    }
+
+    return returnval;
 }
