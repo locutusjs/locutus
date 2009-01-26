e434f3762507a607b23eee67b71a31a3fc856165
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-01-26 16:50:01 +0000

    Work on compiler

--- _tools/PHPJS_Library/PHPJS/Exception.php
+++ _tools/PHPJS_Library/PHPJS/Exception.php
@@ -1,5 +1,12 @@
 <?php
 class PHPJS_Exception extends Exception {
-    
+    function render() {
+        $a = func_get_args();
+        print_r($a);
+        print_r($this->getMessage());
+        print_r($this->getFile());
+        print_r($this->getLine());
+        var_dump($this);
+    }
 }
 ?>
