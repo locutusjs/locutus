ce8bd7d92e4dc7db902f8860085c87281d9a7c3a
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-02-01 22:32:26 +0000

    Made suffle associative-aware

--- functions/array/shuffle.js
+++ functions/array/shuffle.js
@@ -1,10 +1,23 @@
-function shuffle( array ) {
+function shuffle( inputArr ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
     // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // *     example 1: shuffle(['Kevin', 'van', 'Zonneveld']);
+    // +    revised by: Brett Zamir
+    // *     example 1: shuffle({5:'a', 2:'3', 3:'c', 4:5, 'q':5});
     // *     returns 1: true
+
+    var valArr = [];
+    var k = '', i = 0;
+
+    for (k in inputArr) { // Get key and value arrays
+        valArr.push(inputArr[k]);
+        delete inputArr[k] ;
+    }
+    valArr.sort(function() {return 0.5 - Math.random();});
+
+    for (i = 0; i < valArr.length; i++) { // Repopulate the old array
+        inputArr[i] = valArr[i];
+    }
     
-    array.sort(function() {return 0.5 - Math.random();});
     return true;
 }
