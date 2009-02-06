1a373cb6d2a3b41f365a07a9e2bc216b7eec42d7
Brett Zamir <brettz9@yahoo.com>
2009-02-06 16:16:30 +0000

    Added get_declared_classes()

---
+++ functions/classobj/get_declared_classes.js
@@ -0,0 +1,37 @@
+function get_declared_classes() {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir
+    //  -   depends on: class_exists
+    // *     example 1: function A (z) {this.z=z} // Assign 'this' in constructor, making it class-like
+    // *     example 1: function B () {}
+    // *     example 1: B.c = function () {}; // Add a static method, making it class-like
+    // *     example 1: function C () {}
+    // *     example 1: C.prototype.z = function () {}; // Add to prototype, making it behave as a "class"
+    // *     results 1: alert(get_declared_classes()); // [C, B, A]
+
+    var i = '', arr = [], already = {};
+
+    for (i in window) {
+        try {
+            if (typeof window[i] === 'function') {
+                if (!already[i] && class_exists(i)) {
+                    already[i] = 1;
+                    arr.push(i);
+                }
+            }
+            else if (typeof window[i] === 'object') {
+                for (var j in window[i]) {
+                    if (typeof window[j] === 'function' && window[j] && !already[j] && class_exists(j)) {
+                        already[j] = 1;
+                        arr.push(j);
+                    }
+                }
+            }
+        }
+        catch (e) {
+
+        }
+    }
+
+    return arr;
+}
