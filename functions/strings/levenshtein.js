62b0a525482e372b42f87e8595b812db9ef58166
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-03-02 10:16:28 +0000

    Faster levenshtein implementation by Andrea Giammarchi

--- functions/strings/levenshtein.js
+++ functions/strings/levenshtein.js
@@ -1,18 +1,44 @@
-function levenshtein( str1, str2 ) {
+function levenshtein (a, b){
     // http://kevin.vanzonneveld.net
-    // +   original by: Carlos R. L. Rodrigues (http://www.jsfromhell.com)
-    // +   bugfixed by: Onno Marsman
-    // *     example 1: levenshtein('Kevin van Zonneveld', 'Kevin van Sommeveld');
-    // *     returns 1: 3
-
-    var s, l = (s = (str1+'').split("")).length, t = (str2 = (str2+'').split("")).length, i, j, m, n;
-    if(!(l || t)) return Math.max(l, t);
-    for(var a = [], i = l + 1; i; a[--i] = [i]);
-    for(i = t + 1; a[0][--i] = i;);
-    for(i = -1, m = s.length; ++i < m;){
-        for(j = -1, n = str2.length; ++j < n;){
-            a[(i *= 1) + 1][(j *= 1) + 1] = Math.min(a[i][j + 1] + 1, a[i + 1][j] + 1, a[i][j] + (s[i] != str2[j]));
-        }
+    // +      original by: Carlos R. L. Rodrigues (http://www.jsfromhell.com)
+    // +      bugfixed by: Onno Marsman
+    // +       revised by: Andrea Giammarchi (http://webreflection.blogspot.com)
+    // + reimplemented by: Brett Zamir
+    // *        example 1: levenshtein('Kevin van Zonneveld', 'Kevin van Sommeveld');
+    // *        returns 1: 3
+    
+    var split=false, min=Math.min, len1=0, len2=0, I=0, i=0, d=[], c='', j=0, J=0;
+    try{
+        ('0')[0];
+    } catch(i){
+        split=true;
     }
-    return a[l][t];
+    if (a == b) {
+        return 0;
+    }
+    if (!a.length || !b.length) {
+        return b.length || a.length;
+    }
+    if (split){
+        a = a.split('');b = b.split('');
+    }
+    len1 = a.length + 1;
+    len2 = b.length + 1;
+    d = [[0]];
+    while (++i < len2) {
+        d[0][i] = i;
+    }
+    i = 0;
+    while (++i < len1) {
+        J = j = 0;
+        c = a[I];
+        d[i] = [i];
+        while (++j < len2) {
+            d[i][j] = min(d[I][j] + 1, d[i][J] + 1, d[I][J] + (c != b[J]));
+            ++J;
+        }
+        ++I;
+    }
+    
+    return d[len1 - 1][len2 - 1];
 }
