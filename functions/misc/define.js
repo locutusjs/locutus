8fe127a557c9c5395fa217d78a2ab0eb2fff96a6
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-03-02 10:13:10 +0000

    Revised define

--- functions/misc/define.js
+++ functions/misc/define.js
@@ -1,12 +1,62 @@
 function define(name, value) {
     // http://kevin.vanzonneveld.net
     // +   original by: Paulo Ricardo F. Santos
+    // +    revised by: Brett Zamir
     // *     example 1: define('IMAGINARY_CONSTANT1', 'imaginary_value1');
-    // *     returns 1: true
+    // *     results 1: IMAGINARY_CONSTANT1 == 'imaginary_value1'
     
-    if (/boolean|number|null|string/.test(typeof value) !== true) {
-        return false;
+    var define, replace, script;
+    var toString = function (name, value) {
+        return 'const ' + name + '=' + (
+            /^(null|true|false|(\+|\-)?\d+(\.\d+)?)$/.test(value = String(value)) ? value : '"' + replace(value) + '"'
+            );
+    };
+    try {
+        eval('const e=1');
+        replace = function (value){
+            var replace = {
+                "\x08":"b",
+                "\x0A":"\\n",
+                "\x0B":"v",
+                "\x0C":"f",
+                "\x0D":"\\r",
+                '"':'"',
+                "\\":"\\"
+            };
+            return value.replace(/\x08|[\x0A-\x0D]|"|\\/g, function(value){
+                return "\\"+replace[value]
+                });
+        };
+        define = function (name, value){
+            if (document.createElementNS) {
+                script = document.createElementNS('http://www.w3.org/1999/xhtml', 'script');
+            }
+            else {
+                script = document.createElement('script');
+            }
+            script.type = 'text/javascript';
+            script.appendChild(document.createTextNode(toString(name, value)));
+            document.documentElement.appendChild(script);
+            document.documentElement.removeChild(script);
+        }
     }
-    
-    return (window[name] = value) !== undefined;
+    catch (e){
+        replace = function (value) {
+            var replace = {
+                "\x0A":"\\n",
+                "\x0D":"\\r"
+            };
+            return value.replace(/"/g, '""').replace(/\n|\r/g, function(value){
+                return replace[value]
+                });
+        };
+        define = this.execScript ?
+        function (name, value){
+            execScript(toString(name, value), 'VBScript');
+        }:
+        function (name, value){
+            eval(toString(name, value).substring(6));
+        };
+    };
+    define(name, value);
 }
