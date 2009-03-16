523e8db355bb050c8bda2cdab93c5300907641b2
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-03-16 11:28:00 +0000

    Added gmdate

---
+++ functions/datetime/gmdate.js
@@ -0,0 +1,12 @@
+function gmdate (format, timestamp) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir
+    // -    depends on: date
+    // *     example 1: gmdate('H:m:s \\m \\i\\s \\m\\o\\n\\t\\h', 1062402400); // Return will depend on your timezone
+    // *     returns 1: '07:09:40 m is month'
+    var dt = ((typeof(timestamp) == 'undefined') ? new Date() : // Not provided
+        (typeof(timestamp) == 'number') ? new Date(timestamp*1000) : // UNIX timestamp
+        new Date(timestamp));
+    timestamp = Date.parse(dt.toUTCString().slice(0, -4))/1000;
+    return date(format, timestamp);
+}
