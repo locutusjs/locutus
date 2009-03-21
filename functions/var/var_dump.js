2b4a45f1b1801f9f18c1daf009c8fccccfd4b6b2
Brett Zamir <brettz9@yahoo.com>
2009-03-21 09:15:26 +0000

    Added var_dump

---
+++ functions/var/var_dump.js
@@ -0,0 +1,94 @@
+function var_dump() {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir
+    // -    depends on: echo
+    // *     example 1: var_dump(1);
+    // *     returns 1: 'int(1)'
+
+    var output = "", pad_char = " ", pad_val = 4, p='', lgth=0, i=0;
+
+    var getScalarVal = function (val) {
+        var ret = '';
+        if (val === null) {
+            ret = 'NULL';
+        }
+        else if (typeof val === 'boolean') {
+            ret = 'bool('+val+')';
+        }
+        else if (typeof val === 'string') {
+            ret = 'string('+val.length+') "'+val+'"';
+        }
+        else if (typeof val === 'number') {
+            if (parseFloat(val) == parseInt(val)) {
+                ret = 'int('+val+')';
+            }
+            else {
+                ret = 'float('+val+')';
+            }
+        }
+        else if (val === undefined) {
+            ret = 'UNDEFINED'; // Not PHP behavior, but neither is undefined as value
+        }
+        else if (typeof val === 'function') {
+            ret = 'FUNCTION'; // Not PHP behavior, but neither is function as value
+        }
+        return ret;
+    };
+
+    var formatArray = function (obj, cur_depth, pad_val, pad_char) {
+        if (cur_depth > 0) {
+            cur_depth++;
+        }
+
+        var base_pad = repeat_char(pad_val*(cur_depth-1), pad_char);
+        var thick_pad = repeat_char(pad_val*(cur_depth+1), pad_char);
+        var str = "";
+        var val='';
+
+        if (typeof obj === 'object' && obj !== null) {
+            lgth = 0;
+            for (p in obj) {
+                lgth++;
+            }
+            str += "array("+lgth+") {\n";
+            for (var key in obj) {
+                if (typeof obj[key] === 'object' && obj[key] !== null) {
+                    str += thick_pad + "["+key+"] =>\n"+thick_pad+formatArray(obj[key], cur_depth+1, pad_val, pad_char);
+                } else {
+                    val = getScalarVal(obj[key]);
+                    str += thick_pad + "["+key+"] =>\n"+thick_pad + val + "\n";
+                }
+            }
+            str += base_pad + "}\n";
+        } else {
+            str = getScalarVal(obj);
+        }
+        return str;
+    };
+
+    var repeat_char = function (len, pad_char) {
+        var str = "";
+        for(var i=0; i < len; i++) {
+            str += pad_char;
+        }
+        return str;
+    };
+
+    output = formatArray(arguments[0], 0, pad_val, pad_char);
+    for (i=1; i < arguments.length; i++) {
+        output += '\n'+formatArray(arguments[i], 0, pad_val, pad_char);
+    }
+
+    if (document.body) {
+        echo(output);
+    }
+    else {
+        try {
+            XULDocument; // We're in XUL, so appending as plain text won't work
+            echo('<pre xmlns="http://www.w3.org/1999/xhtml" style="white-space:pre;">'+output+'</pre>');
+        }
+        catch(e) {
+            echo(output); // Outputting as plain text may work in some plain XML
+        }
+    }
+}
