4bee3f96f233cacab0805d41c07726da79fef30d
Brett Zamir <brettz9@yahoo.com>
2009-03-27 14:41:53 +0000

    Added foreach to experimental

---
+++ _experimental/language/foreach.js
@@ -0,0 +1,19 @@
+function foreach (arr, handler) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir
+    // *     example 1: foreach (['a', 'b'], function (val) {alert(val);});
+    // *     returns 1: undefined
+    // *     example 2: foreach (['a', 'b'], function (key, val) {alert(key+'::'+val);});
+    // *     returns 2: undefined
+   var k;
+   if (handler.length === 1) {
+       for (k in arr) {
+           handler(arr[k]); // only pass the value
+       }
+   }
+   else {
+       for (k in arr) {
+           handler(k, arr[k]);
+       }
+   }
+}
