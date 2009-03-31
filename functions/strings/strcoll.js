a432b278d208321b759acd6d103ab8f806b96f9e
Brett Zamir <brettz9@yahoo.com>
2009-03-31 06:20:02 +0000

    Add strcoll()

---
+++ functions/strings/strcoll.js
@@ -0,0 +1,9 @@
+function strcoll (str1, str2) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir
+    // *     example 1: strcoll('a', 'b');
+    // *     returns 1: -1
+	
+	// We might modify this to work with setlocale() locales instead of JS's own (and same with sort(), etc.)
+    return str1.localeCompare(str2);
+}
