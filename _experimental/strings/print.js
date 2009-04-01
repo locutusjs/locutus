69813f590e317a947fc615b6eb005501e2768e0d
Brett Zamir <brettz9@yahoo.com>
2009-04-01 10:59:56 +0000

    Moved convert_uuencode() to ported (original author did not state there were any problems with it)

--- _experimental/strings/print.js
+++ _experimental/strings/print.js
@@ -1,6 +1,7 @@
 function print (arg) {
     // http://kevin.vanzonneveld.net
     // +   original by: Brett Zamir (http://brettz9.blogspot.com)
+    // %          note 1: Implemented correctly, but causes a problem for the phpjs.org compiler
     // -    depends on: echo
     // *     example 1: print('Hello World');
     // *     returns 1: 1
