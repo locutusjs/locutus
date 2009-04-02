ba82e49a8db10ea651462692872abdf2872715a4
Brett Zamir <brettz9@yahoo.com>
2009-04-02 17:01:09 +0000

    Remove instanceof in favor of constructor test since class not exposed globally

--- functions/var/get_resource_type.js
+++ functions/var/get_resource_type.js
@@ -1,5 +1,5 @@
 function get_resource_type (handle) {
-    if (!handle || typeof handle !== 'object' ||  !(handle instanceof PHPJS_Resource)) {
+    if (!handle || typeof handle !== 'object' || handle.constructor !== 'PHPJS_Resource') {
         return false;
     }
     return handle.get_resource_type();
--- functions/var/is_resource.js
+++ functions/var/is_resource.js
@@ -1,5 +1,5 @@
 function is_resource (handle) {
-    if (!handle || typeof handle !== 'object' ||  !(handle instanceof PHPJS_Resource)) {
+    if (!handle || typeof handle !== 'object' || handle.constructor !== 'PHPJS_Resource') {
         return false;
     }
     return true;
