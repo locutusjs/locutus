dc761a9c3b0c38fbb2e8ca0b2041aaa6884881ae
Brett Zamir <brettz9@yahoo.com>
2009-04-03 04:54:22 +0000

    Further disclaimer on regexp as string

--- functions/pcre/preg_grep.js
+++ functions/pcre/preg_grep.js
@@ -3,6 +3,8 @@ function preg_grep (pattern, input, flags) {
     // +   original by: Brett Zamir (http://brettz9.blogspot.com)
 	// %          note 1: If pass pattern as string, must escape backslashes, even for single quotes
     // %          note 2: The regular expression itself must be expressed JavaScript style
+    // %          note 3: It is not recommended to submit the pattern as a string, as we may implement
+    // %          note 3:   parsing of PHP-style expressions (flags, etc.) in the future
     // *     example 1: var arr = [1, 4, 4.5, 3, 'a', 4.4];
     // *     example 1: preg_grep("/^(\\d+)?\\.\\d+$/", arr);
     // *     returns 1: {2: 4.5, 5: 4.4}
