4f039832cf1c18c0e1d4cce7770a506d8f169e54
Brett Zamir <brettz9@yahoo.com>
2009-04-03 04:34:21 +0000

    Disclaimer re: regexp style

--- functions/pcre/preg_grep.js
+++ functions/pcre/preg_grep.js
@@ -2,6 +2,7 @@ function preg_grep (pattern, input, flags) {
     // http://kevin.vanzonneveld.net
     // +   original by: Brett Zamir (http://brettz9.blogspot.com)
 	// %          note 1: If pass pattern as string, must escape backslashes, even for single quotes
+    // %          note 2: The regular expression itself must be expressed JavaScript style
     // *     example 1: var arr = [1, 4, 4.5, 3, 'a', 4.4];
     // *     example 1: preg_grep("/^(\\d+)?\\.\\d+$/", arr);
     // *     returns 1: {2: 4.5, 5: 4.4}
