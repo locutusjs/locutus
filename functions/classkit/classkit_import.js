ea9990778bd6b9099facaaf786a3cc849f35be41
Brett Zamir <brettz9@yahoo.com>
2009-04-05 04:33:08 +0000

    Add runkit import

--- functions/classkit/classkit_import.js
+++ functions/classkit/classkit_import.js
@@ -3,6 +3,8 @@ function classkit_import (file) {
     // +   original by: Brett Zamir (http://brettz9.blogspot.com)
     // -    depends on: file_get_contents
     // %        note 1: does not return an associative array as in PHP
+    // %        note 2: Implement instead with include?
+    // %        note 3: CLASSKIT_AGGREGATE_OVERRIDE is mentioned as a flag at http://www.php.net/manual/en/runkit.constants.php but not in classkit docs
     // *     example 1: classkit_import('http://example.com/somefile.js');
     // *     returns 1: undefined
     
---
+++ functions/runkit/runkit_import.js
@@ -0,0 +1,18 @@
+function runkit_import (file, flags) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir (http://brettz9.blogspot.com)
+    // -    depends on: file_get_contents
+    // %        note 1: does not return an associative array as in PHP and will evaluate all variables, not only those in a function or class
+    // %        note 2: Implement instead with include?
+    // *     example 1: runkit_import('http://example.com/somefile.js');
+    // *     returns 1: undefined
+
+    if (flags) {
+        // RUNKIT_IMPORT_FUNCTIONS, RUNKIT_IMPORT_CLASS_METHODS, RUNKIT_IMPORT_CLASS_CONSTS,
+        // RUNKIT_IMPORT_CLASS_PROPS, RUNKIT_IMPORT_CLASSES, RUNKIT_IMPORT_OVERRIDE
+        // CLASSKIT_AGGREGATE_OVERRIDE ?
+        throw 'Flags not supported for runkit_import';
+    }
+
+	eval(file_get_contents(file));
+}
