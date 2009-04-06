70563499f1999f0163f5c0fee6e10914abd74378
Brett Zamir <brettz9@yahoo.com>
2009-04-06 03:50:29 +0000

    Added incomplete function fnmatch to experimental

---
+++ _experimental/filesystem/fnmatch.js
@@ -0,0 +1,25 @@
+function fnmatch (pattern, string, flags) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir (http://brettz9.blogspot.com)
+    // *     example 1: fnmatch('*gr[ae]y', 'gray');
+    // *     returns 1: true
+
+// Unfinished
+
+    // all integers
+    switch (flags) { // allow bitwise or'ed?
+        case 'FNM_NOESCAPE': // Disable backslash escaping.
+            break;
+        case 'FNM_PATHNAME': // Slash in string only matches slash in the given pattern.
+            break;
+        case 'FNM_PERIOD': // Leading period in string must be exactly matched by period in the given pattern.
+            break;
+        case 'FNM_CASEFOLD': // Caseless match. Part of the GNU extension.
+            break;
+        default:
+            break;
+    }
+
+    return (new RegExp('^'+strtr(preg_quote(pattern, '#'), {'\*' : '.*', '\?' : '.', '\[' : '[', '\]' : ']'})+'$', 'i')).test(string);
+    
+}
