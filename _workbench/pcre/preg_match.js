87e422bf8c23d2ea0e2ec71df0f6533e14b5157f
Brett Zamir <brettz9@yahoo.com>
2009-04-13 03:09:39 +0000

    Worked out more of named subpatterns and flag commenting

--- _workbench/pcre/preg_match.js
+++ _workbench/pcre/preg_match.js
@@ -6,7 +6,7 @@ function preg_match(pattern, subject, matches, flags, offset) {
     // *     example 1: preg_match(/(\w+)\W([\W\w]+)/, 'this is some text', matches);
     // *     matches 1: matches[1] == 'this'
 
-    var i, array = [], regexpFlags='';
+    var i, flag='', array = [], regexpFlags='', subPatternNames=[];
 
     if (typeof pattern === 'string') {
         /* // Potentially useful configuration
@@ -21,35 +21,37 @@ function preg_match(pattern, subject, matches, flags, offset) {
             }
             var patternPart = pattern.slice(1, lastDelimPos);
             var flagPart = pattern.slice(lastDelimPos+1);
+            // Fix: Need to study http://php.net/manual/en/regexp.reference.php more thoroughly
             for (i=0; i < flagPart.length; i++) {
-                switch(flagPart[i]) {
+                flag = flagPart[i];
+                switch(flag) {
+                    case 'e': // used in preg_replace only but ignored elsewhere; "does normal substitution of backreferences in the replacement string, evaluates it as PHP code, and uses the result for replacing the search string". "Single quotes, double quotes, backslashes and NULL chars will be escaped by backslashes in substituted backreferences."
+                        break;
                     case 'g': // We don't use this in preg_match, but it's presumably not an error
                     case 'm':
                     case 'i':
-                        regexpFlags += flagPart[i];
+                        regexpFlags += flag;
                         break;
-                    case 's':
-                    case 'x':
-                    case 'e':
-                    case 'A':
-                    case 'D':
-                    case 'S':
-                    case 'U':
-                    case 'X':
-                    case 'J':
-                    case 'u':
-                        throw 'The passed flag(s) are presently unsupported in '+arguments.callee.name;
-                        break;
+                    case 's': // "dot metacharacter in the pattern matches all characters, including newlines. Without it, newlines are excluded... A negative class such as [^a] always matches a newline character"
+                    case 'x': // "whitespace data characters in the pattern are totally ignored except when escaped or inside a character class, and characters between an unescaped # outside a character class and the next newline character, inclusive, are also ignored"; "Whitespace characters may never appear within special character sequences in a pattern"
+                    case 'A': // pattern is "constrained to match only at the start of the string which is being searched"
+                    case 'D': // "a dollar metacharacter in the pattern matches only at the end of the subject string" (ignored if 'm' set)
+                    case 'U': // "makes not greedy by default, but become greedy if followed by "?""
+                    case 'X': // "additional functionality of PCRE that is incompatible with Perl. Any backslash in a pattern that is followed by a letter that has no special meaning causes an error, thus reserving these combinations for future expansion"; not in use in PHP presently
+                    case 'J': // "changes the local PCRE_DUPNAMES option. Allow duplicate names for subpatterns"
+                    case 'u': // "turns on additional functionality of PCRE that is incompatible with Perl. Pattern strings are treated as UTF-8."
+                        throw 'The passed flag "'+flag+'" is presently unsupported in '+arguments.callee.name;
+                    case 'S': // spends "more time analyzing pattern in order to speed up the time taken for matching" (for subsequent matches)
+                        throw 'The passed flag "'+flag+'" to '+arguments.callee.name+' cannot be implemented in JavaScript'; // Could possibly optimize inefficient expressions, however
                     case 'y':
                         throw 'Flag "y" is a non-cross-browser, non-PHP flag, not supported in '+arguments.callee.name;
-                        break;
                     default:
-                        throw 'Unrecognized flag passed to '+arguments.callee.name;
-                        break;
+                        throw 'Unrecognized flag "'+flag+'" passed to '+arguments.callee.name;
                 }
             }
-            patternPart = patternPart.replace(/\(\?<(.*)\)/g, function (namedSubpattern, name) {
-                return namedSubpattern; // Not implemented
+            patternPart = patternPart.replace(/\(\?<(.*?)>(.*?)\)/g, function (namedSubpattern, name, pattern) {
+                subPatternNames.push(name);
+                return '('+pattern+')';
             });
             pattern = new RegExp(patternPart, regexpFlags);
         // }
@@ -76,6 +78,9 @@ function preg_match(pattern, subject, matches, flags, offset) {
     // loop through the first indice of the array and store the values in the $matches array
     for (i = 0; i < array[0].length; i++) {
         matches[i] = array[0][i];
+        if (i > 0 && subPatternNames[i-1] !== undefined) {
+            matches[subPatternNames] = array[0][i]; // UNTESTED
+        }
     }
 
     return 1;
