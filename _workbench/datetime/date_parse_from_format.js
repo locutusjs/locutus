cf3ce587ab97ab04aff5582a3b20e3165ed553f3
Brett Zamir <brettz9@yahoo.com>
2009-04-16 12:17:18 +0000

    Fix/build a little on date_parse_from_format

--- _workbench/datetime/date_parse_from_format.js
+++ _workbench/datetime/date_parse_from_format.js
@@ -7,21 +7,47 @@ function date_parse_from_format (format, dateArg) {
 	// *     example 1: date_parse_from_format('j.n.Y H:iP', '6.1.2009 13:00+01:00');
 	// *     returns 1: {year : 2006, month: 12, day: 12, hour: 10, minute: 0, second: 0, fraction: 0.5, warning_count: 0, warnings: [], error_count: 0, errors: [], is_localtime: false, zone_type: 1, zone: -60, is_dst:false}
 
-    var ts=0, newtime=0, retObj={};
+    var newtime=0, retObj={};
+
+    var _dst = function (t) {
+        // Calculate Daylight Saving Time (derived from gettimeofday() code)
+        var dst=0;
+        var jan1 = new Date(t.getFullYear(), 0, 1, 0, 0, 0, 0);  // jan 1st
+        var june1 = new Date(t.getFullYear(), 6, 1, 0, 0, 0, 0); // june 1st
+        var temp = jan1.toUTCString();
+        var jan2 = new Date(temp.slice(0, temp.lastIndexOf(' ')-1));
+        temp = june1.toUTCString();
+        var june2 = new Date(temp.slice(0, temp.lastIndexOf(' ')-1));
+        var std_time_offset = (jan1 - jan2) / (1000 * 60 * 60);
+        var daylight_time_offset = (june1 - june2) / (1000 * 60 * 60);
+
+        if (std_time_offset === daylight_time_offset) {
+            dst = false; // daylight savings time is NOT observed
+        }
+        else {
+            // positive is southern, negative is northern hemisphere
+            var hemisphere = std_time_offset - daylight_time_offset;
+            if (hemisphere >= 0) {
+                std_time_offset = daylight_time_offset;
+            }
+            dst = true; // daylight savings time is observed
+        }
+        return dst;
+    };
 
     try {
-        ts = strtotime(dateArg);
-        newtime = strtotime(date(format, ts));
+        newtime = 0; // fix: needs to use format, dateArg to return a time
         retObj = date_parse(newtime);
     }
     catch (e) {
         return false; // Presumably returns false, as with date_parse()
     }
 
-    // Fix: need to add the following as well (base on dateArg for zone info, and use newtime for is_dst?)
+    retObj.is_dst = _dst(newtime);
+
+    // Fix: need to add the following as well
     //zone_type: TIMELIB_ZONETYPE_OFFSET 1, TIMELIB_ZONETYPE_ABBR   2, TIMELIB_ZONETYPE_ID     3
     //zone: -60
-    //is_dst: false
     
     return retObj;
 }
