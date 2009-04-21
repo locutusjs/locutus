6f8284392696d684f9a8c4e68c66f3c062d64c31
Brett Zamir <brettz9@yahoo.com>
2009-04-21 05:58:12 +0000

    Added to comment

--- _experimental/language/HEREDOC.js
+++ _experimental/language/HEREDOC.js
@@ -3,7 +3,7 @@ function HEREDOC (xmllist) {
     // +   original by: Brett Zamir (http://brettz9.blogspot.com)
 	// %          note 1: Requires E4X extension to browser (as in Mozilla)
 	// %          note 2: To include multi-line whitespace, must encapsulate in an XMLList (see example), the literal form being easiest and reminiscent of HEREDOC angled brackets
-	// %          note 3: As per E4X rules, {} can be used to encapsulate variables within the HEREDOC
+	// %          note 3: As per E4X rules, {} can be used to encapsulate variables or JavaScript expressions within the HEREDOC
 	// %          note 4: Could be renamed HERE, etc., as the HEREDOC naming is not fixed in PHP either
     // *     example 1: var myVar = 9;
     // *     example 1: HEREDOC (<>Here is a string that could go across multiple lines and include variables thus: {myVar} or even add JavaScript thus: {Math.random()}</>);
