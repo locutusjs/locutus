d4f98915062efd4e7af58711f8cb2a52f0dddace
Brett Zamir <brettz9@yahoo.com>
2009-04-21 06:34:20 +0000

    Note to HEREDOC re: NOWDOC

--- _experimental/language/HEREDOC.js
+++ _experimental/language/HEREDOC.js
@@ -5,6 +5,7 @@ function HEREDOC (xmllist) {
 	// %          note 2: To include multi-line whitespace, must encapsulate in an XMLList (see example), the literal form being easiest and reminiscent of HEREDOC angled brackets
 	// %          note 3: As per E4X rules, {} can be used to encapsulate variables or JavaScript expressions within the HEREDOC
 	// %          note 4: Could be renamed HERE, etc., as the HEREDOC naming is not fixed in PHP either
+	// %          note 5: Unfortunately this approach will not work to simulate the PHP 5.3+ NOWDOC; you must escape any literal curly brackets manually with &#x7B and &#x7D;
     // *     example 1: var myVar = 9;
     // *     example 1: HEREDOC (<>Here is a string that could go across multiple lines and include variables thus: {myVar} or even add JavaScript thus: {Math.random()}</>);
     // *     returns 1: 'Here is a string that could go across multiple lines and include variables thus: 9 or even add JavaScript thus: 0.9957547511250214'
