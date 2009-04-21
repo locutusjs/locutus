9e99d79282b572c7e715d46fffb8f311de8aa179
Brett Zamir <brettz9@yahoo.com>
2009-04-21 06:15:43 +0000

    added preg_filter to unported and note to round()

--- functions/math/round.js
+++ functions/math/round.js
@@ -1,4 +1,4 @@
-function round ( val, precision ) {
+function round ( val, precision, mode ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Philip Peterson
     // +    revised by: Onno Marsman
@@ -6,6 +6,8 @@ function round ( val, precision ) {
     // *     returns 1: 1242000
     // *     example 2: round(3.6);
     // *     returns 2: 4
+
+    // Need to support mode flags: PHP_ROUND_HALF_UP, PHP_ROUND_HALF_DOWN, PHP_ROUND_HALF_EVEN, or PHP_ROUND_HALF_ODD
  
     return parseFloat(parseFloat(val).toFixed(precision));
 }
