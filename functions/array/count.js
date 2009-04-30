c0ce879119ffdca15c859e38ab66a6d18f5784fa
Brett Zamir <brettz9@yahoo.com>
2009-04-30 07:40:56 +0000

    Fixed count (thanks Soren Hansen!)

--- functions/array/count.js
+++ functions/array/count.js
@@ -2,6 +2,7 @@ function count( mixed_var, mode ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +      input by: Waldo Malqui Silva
+    // +      bugfixed by: Soren Hansen
     // *     example 1: count([[0,0],[0,-4]], 'COUNT_RECURSIVE');
     // *     returns 1: 6
     // *     example 2: count({'one' : [1,2,3,4,5]}, 'COUNT_RECURSIVE');
@@ -9,8 +10,18 @@ function count( mixed_var, mode ) {
 
     var key, cnt = 0;
 
-    if( mode == 'COUNT_RECURSIVE' ) mode = 1;
-    if( mode != 1 ) mode = 0;
+    if (mixed_var === null){
+        return 0;
+    } else if (mixed_var.constructor !== Array && mixed_var.constructor !== Object){
+        return 1;
+    }
+
+    if( mode === 'COUNT_RECURSIVE' ) {
+        mode = 1;
+    }
+    if( mode != 1 ) {
+        mode = 0;
+    }
 
     for (key in mixed_var){
         cnt++;
