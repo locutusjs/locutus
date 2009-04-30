c5d0d2f1c42341aa1469d95fa1a31c7380257f3a
Brett Zamir <brettz9@yahoo.com>
2009-04-30 03:47:02 +0000

    parse_str fix, thanks stag019

--- functions/strings/parse_str.js
+++ functions/strings/parse_str.js
@@ -4,8 +4,10 @@ function parse_str(str, array){
     // +   improved by: Michael White (http://getsprink.com)
     // +    tweaked by: Jack
     // +   bugfixed by: Onno Marsman
-    // %        note 1: Currently does not put variables in local scope.
-    // %        note 1: So use of second argument is required.
+    // +   reimplemented by: stag019
+    // +   bugfixed by: Brett Zamir (http://brettz9.blogspot.com)
+	// -    depends on: urldecode
+    // %        note 1: When no argument is specified, will put variables in global scope.
     // *     example 1: var arr = {};
     // *     example 1: parse_str('first=foo&second=bar', arr);
     // *     results 1: arr == { first: 'foo', second: 'bar' }
@@ -13,15 +15,94 @@ function parse_str(str, array){
     // *     example 2: parse_str('str_a=Jack+and+Jill+didn%27t+see+the+well.', arr);
     // *     results 2: arr == { str_a: "Jack and Jill didn't see the well." }
 
-    var glue1 = '=';
-    var glue2 = '&';
+	var glue1 = '=', glue2 = '&', array2 = String(str).split(glue2),
+	i, j, chr, tmp, key, value, bracket, keys, evalStr,
+	fixStr = function(str)
+	{
+		return urldecode(str).replace(/([\\"'])/g, '\\$1').replace(/\0/g, '\\0').replace(/\n/g, '\\n').replace(/\r/g, '\\r');
+	};
 
-    var array2 = (str+'').split(glue2);
-    var array2l = 0, tmp = '', x = 0;
+	if(!array)
+	{
+		array = window;
+	}
 
-    array2l = array2.length;
-    for (x = 0; x<array2l; x++) {
-        tmp = array2[x].split(glue1);
-        array[unescape(tmp[0])] = unescape(tmp[1]).replace(/[+]/g, ' ');
-    }
+	for(i = 0; i < array2.length; i++)
+	{
+		tmp = array2[i].split(glue1);
+		if(tmp.length < 2)
+		{
+			tmp = [tmp, ''];
+		}
+		key   = fixStr(tmp[0]);
+		value = fixStr(tmp[1]);
+		while(key.charAt(0) == ' ')
+		{
+			key = key.substr(1);
+		}
+		if(key && key.charAt(0) != '[')
+		{
+			keys    = [];
+			bracket = 0;
+			for(j = 0; j < key.length; j++)
+			{
+				if(key.charAt(j) == '[' && !bracket)
+				{
+					bracket = j + 1;
+				}
+				else if(key.charAt(j) == ']')
+				{
+					if(bracket)
+					{
+						if(!keys.length)
+						{
+							keys.push(key.substr(0, bracket - 1));
+						}
+						keys.push(key.substr(bracket, j - bracket));
+						bracket = 0;
+						if(key.charAt(j + 1) !== '[')
+						{
+							break;
+						}
+					}
+				}
+			}
+			if(!keys.length)
+			{
+				keys = [key];
+			}
+			for(j = 0; j < keys[0].length; j++)
+			{
+				chr = keys[0].charAt(j);
+				if(chr == ' ' || chr == '.' || chr == '[')
+				{
+					keys[0] = keys[0].substr(0, j) + '_' + keys[0].substr(j + 1);
+				}
+				if(chr == '[')
+				{
+					break;
+				}
+			}
+			evalStr = 'array';
+			for(j = 0; j < keys.length; j++)
+			{
+				key = keys[j];
+				if((key !== '' && key !== ' ') || j === 0)
+				{
+					key = "'" + key + "'";
+				}
+				else
+				{
+					key = eval(evalStr + '.push([]);') - 1;
+				}
+				evalStr += '[' + key + ']';
+				if(j != keys.length - 1 && eval('typeof ' + evalStr) == 'undefined')
+				{
+					eval(evalStr + ' = [];');
+				}
+			}
+			evalStr += " = '" + value + "';\n";
+			eval(evalStr);
+		}
+	}
 }
