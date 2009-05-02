05e5b321b1c67052e148c3bc5033e99ca6be3dc6
Brett Zamir <brettz9@yahoo.com>
2009-05-02 13:10:18 +0000

    Prepend var declarations

--- functions/var/import_request_variables.js
+++ functions/var/import_request_variables.js
@@ -7,15 +7,15 @@ function import_request_variables (types, prefix) {
     // *        example 1: import_request_variables('gc', 'pr_');
     // *        results 1: pr_snack == 'yummy'
 
-    var i = 0, current = '', url = '', vars = '';
+    var i = 0, current = '', url = '', vars = '', arrayBracketPos = -1, arrName='';
     prefix = prefix || '';
 
     if (/g/i.test(types)) { // GET
         for(i = 0, url = window.location.href, vars = url.substring(url.lastIndexOf("?") + 1, url.length).split("&"); i < vars.length;i++){
             current = vars[i].split("=");
-            var arrayBracketPos = current[0].indexOf('[');
+            arrayBracketPos = current[0].indexOf('[');
             if (arrayBracketPos !== -1) {
-                var arrName = current[0].substring(0, arrayBracketPos);
+                arrName = current[0].substring(0, arrayBracketPos);
                 if (!window[prefix+arrName]) {
                     window[prefix+arrName] = [];
                 }
