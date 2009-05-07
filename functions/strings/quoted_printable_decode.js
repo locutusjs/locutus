6406334cbbf041743d990f07645273c73883be37
Brett Zamir <brettz9@yahoo.com>
2009-05-07 02:52:04 +0000

    Adding quoted_printable_ functions

--- functions/strings/quoted_printable_decode.js
+++ functions/strings/quoted_printable_decode.js
@@ -2,59 +2,23 @@ function quoted_printable_decode(str) {
 	// http://kevin.vanzonneveld.net
 	// +   original by: Ole Vrijenhoek
 	// +   bugfixed by: Brett Zamir (http://brettz9.blogspot.com)
-    // *    example 1: quoted_printable_decode("Lorem ipsum dolor sit amet=23, consectetur adipisicing elit");
-    // *    returns 1: Lorem ipsum dolor sit amet#, consectetur adipisicing elit
+    // +   reimplemented by: Theriault
+	// +   improved by: Brett Zamir (http://brettz9.blogspot.com)
+    // *     example 1: quoted_printable_decode('a=3Db=3Dc');
+    // *     returns 1: 'a=b=c'
+    // *     example 2: quoted_printable_decode('abc  =20\r\n123  =20\r\n');
+    // *     returns 2: 'abc   \r\n123   \r\n'
+    // *     example 3: quoted_printable_decode('012345678901234567890123456789012345678901234567890123456789012345678901234=\r\n56789');
+    // *     returns 3: '01234567890123456789012345678901234567890123456789012345678901234567890123456789'
+    // *    example 4: quoted_printable_decode("Lorem ipsum dolor sit amet=23, consectetur adipisicing elit");
+    // *    returns 4: Lorem ipsum dolor sit amet#, consectetur adipisicing elit
 
-// sources: php sourcecode (php-5.2.9\ext\standard\quot_print.c)
-
-    // shortcuts
-    var chr = function(c) {
-        return String.fromCharCode(c);
+    // Removes softline breaks
+    var RFC2045Decode1 = /=\r\n/gm,
+    // Decodes all equal signs followed by two capital hex digits
+    RFC2045Decode2IN = /=([0-9A-F][0-9A-F])/gm,
+    RFC2045Decode2OUT = function (sMatch, sHex) {
+        return String.fromCharCode(parseInt(sHex, 16));
     };
-    var code = function(c) {
-        return c.charCodeAt(0);
-    };
-    var isHex = function(c) {
-        var patt = /[0-9]|[a-f]/i
-        return patt.test(c);
-    };
-
-    var str_in = str;
-    var str_out = "";
-    var i = 0, j = 0, k = 0;
-
-    while(str_in[i]) {
-        switch(str_in[i]) {
-            case "=":
-                if(code(str_in[i+1]) && code(str_in[i+2]) && isHex(str_in[i+1]) && isHex(str_in[i+2])) {
-                    str_out += chr((parseInt(str_in[i+1], 16) << 4) + parseInt(str_in[i+2], 16));
-                    i += 3;
-                } else {
-                    // Check for soft line break according to RFC 2045
-                    k = 1;
-                    while(code(str_in[i+k]) && ((code(str_in[i+k])==32) || (code(str_in[i+k])==9))) {
-                        // Possibly, skip spaces/tabs at the end of line
-                        k++;
-                    }
-                    if(!str_in[i+k]) {
-                        // End of line reached
-                        i += k;
-                    } else if((code(str_in[i+k])==13) && (code(str_in[i+k+1])==10)) {
-                        // CRLF
-                        i += k+2;
-                    } else if((code(str_in[i+k])==13) || (code(str_in[i+k])==10)) {
-                        // CR or LF
-                        i += k+1;
-                    } else {
-                        str_out[j++] = str_in[i++];
-                    }
-                }
-            break;
-            default:
-                str_out += str_in[i++];
-            break;
-        }
-    }
-    str_out[j] = '\0';
-    return str_out;
+    return str.replace(RFC2045Decode1, '').replace(RFC2045Decode2IN, RFC2045Decode2OUT);
 }
