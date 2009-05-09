5f810126b696689790a0f046e5a49188d98974c7
Brett Zamir <brettz9@yahoo.com>
2009-05-09 10:56:06 +0000

    Implemented assert and assert_options

---
+++ functions/info/assert_options.js
@@ -0,0 +1,53 @@
+function assert_options (what, value) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir (http://brettz9.blogspot.com)
+    // *     example 1: assert_options('ASSERT_CALLBACK');
+    // *     returns 1: null
+    
+    // BEGIN REDUNDANT
+    if (!this.php_js) {
+        this.php_js = {};
+    }
+    if (!this.php_js.assert_values) {
+        this.php_js.assert_values = {};
+    }
+    if (!this.php_js.ini) {
+        this.php_js.ini = {};
+    }
+    // END REDUNDANT
+
+    var ini, dflt;
+    switch(what) {
+        case 'ASSERT_ACTIVE':
+            ini = 'assert.active';
+            dflt = 1;
+            break;
+        case 'ASSERT_WARNING':
+            ini = 'assert.warning';
+            dflt = 1;
+            throw 'There are no warnings for us to throw in JavaScript for assert_options()';
+            break;
+        case 'ASSERT_BAIL':
+            ini = 'assert.bail';
+            dflt = 0;
+            break;
+        case 'ASSERT_QUIET_EVAL':
+            ini = 'assert.quiet_eval';
+            dflt = 0;
+            break;
+        case 'ASSERT_CALLBACK':
+            ini = 'assert.callback';
+            dflt = null;
+            break;
+        default:
+            throw 'Improper type for assert_options()';
+            break;
+    }
+    // I presume this is to be the most recent value, instead of the default value
+    var originalValue = this.php_js.assert_values[ini] || (this.php_js.ini[ini] && this.php_js.ini[ini].local_value) || dflt;
+
+    if (value) {
+        this.php_js.assert_values[ini] = value; // We use 'ini' instead of 'what' as key to be more convenient for assert() to test for current value
+    }
+    return originalValue;
+}
