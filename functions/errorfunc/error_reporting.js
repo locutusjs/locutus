a856b247884ee31cefa0a6435d194d3e0246f38f
Brett Zamir <brettz9@yahoo.com>
2009-05-10 07:30:31 +0000

    Adding dependency field

--- functions/errorfunc/error_reporting.js
+++ functions/errorfunc/error_reporting.js
@@ -1,6 +1,7 @@
 function error_reporting (level) {
     // http://kevin.vanzonneveld.net
     // +   original by: Brett Zamir (http://brettz9.blogspot.com)
+	// -    depends on: ini_set
     // %        note 1: This will not set a global_value or access level for the ini item
     // %        note 2: If you wish the default value to be as in PHP, you must manually set it
     // %        note 3: This function depends on functions implementing error handling
