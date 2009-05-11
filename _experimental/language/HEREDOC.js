0dcabf15e79e1ecbc68e455c33cbab9d75e30c67
Brett Zamir <brettz9@yahoo.com>
2009-05-11 05:32:35 +0000

    Allow unescaping for reserved XML characters in HEREDOC

--- _experimental/language/HEREDOC.js
+++ _experimental/language/HEREDOC.js
@@ -3,12 +3,13 @@ function HEREDOC (xmllist) {
     // +   original by: Brett Zamir (http://brettz9.blogspot.com)
 	// %          note 1: Requires E4X extension to browser (as in Mozilla)
 	// %          note 2: Will not work with poorly-formed XML contents or non-E4X XML, but should work for non-CDATA-requiring text and for well-formed XML
-	// %          note 3: To include multi-line whitespace, must encapsulate in an XMLList (see example), the literal form being easiest and reminiscent of HEREDOC angled brackets
-	// %          note 4: As per E4X rules, {} can be used to encapsulate variables or JavaScript expressions within the HEREDOC
-	// %          note 5: Could be renamed HERE, etc., as the HEREDOC naming is not fixed in PHP either
-	// %          note 6: Unfortunately this approach will not work to simulate the PHP 5.3+ NOWDOC; you must escape any literal curly brackets manually with &#x7B and &#x7D;
+    // %          note 3: Will need to escape any < or & respectively into &lt; or &amp; (and > in case in disallowed sequence ']]>')
+	// %          note 4: To include multi-line whitespace, must encapsulate in an XMLList (see example), the literal form being easiest and reminiscent of HEREDOC angled brackets
+	// %          note 5: As per E4X rules, {} can be used to encapsulate variables or JavaScript expressions within the HEREDOC
+	// %          note 6: Could be renamed HERE, etc., as the HEREDOC naming is not fixed in PHP either
+	// %          note 7: Unfortunately, this approach will not work to simulate the PHP 5.3+ NOWDOC; you must escape any literal curly brackets manually with &#x7B and &#x7D;
     // *     example 1: var myVar = 9;
     // *     example 1: HEREDOC (<>Here is a string that could go across multiple lines and include variables thus: {myVar} or even add JavaScript thus: {Math.random()}</>);
     // *     returns 1: 'Here is a string that could go across multiple lines and include variables thus: 9 or even add JavaScript thus: 0.9957547511250214'
-	return xmllist.toXMLString();
+	return xmllist.toXMLString().replace('&gt;', '>').replace('&lt;', '<').replace('&amp;', '&');
 }
