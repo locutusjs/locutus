96bf16d3f6678990e251c8c9b0c3dc91580d6920
Brett Zamir <brettz9@yahoo.com>
2009-05-11 02:12:22 +0000

    Mozilla-only __LINE__()

---
+++ _experimental/language/__LINE__.js
@@ -0,0 +1,12 @@
+function __LINE__(e) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir (http://brettz9.blogspot.com)
+    // %        note 1: Not a function in PHP, so in experimental
+    // %        note 2: This function depends on the non-standard lineNumber property of Error instances (Mozilla)
+    // %        note 3: Also requires 'e' error object to be passed in as an argument, unlike PHP
+    // *     example 1: function a () {try {throw new Error();} catch(e) {return __LINE__(e);}}
+    // *     example 1: a();
+    // *     returns 1: '10'
+
+    return e.lineNumber;
+}
