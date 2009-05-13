ce7a085bdec25b6c21fac9d3c3b522025ec99621
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-05-13 12:38:01 +0000

    Fixed undefined if offset is not passed

--- functions/strings/strpos.js
+++ functions/strings/strpos.js
@@ -2,9 +2,10 @@ function strpos( haystack, needle, offset){
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +   improved by: Onno Marsman    
+    // +   bugfixed by: Daniel Esteban
     // *     example 1: strpos('Kevin van Zonneveld', 'e', 5);
     // *     returns 1: 14
 
-    var i = (haystack+'').indexOf( needle, offset ); 
-    return i===-1 ? false : i;
+    var i = (haystack+'').indexOf(needle, (offset ? offset : 0));
+    return i === -1 ? false : i;
 }
