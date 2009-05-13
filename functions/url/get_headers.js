eab4d850969fa383416a7c0bcfb6fb9b8d66765b
Brett Zamir <brettz9@yahoo.com>
2009-05-13 23:53:51 +0000

    another bugfix

--- functions/url/get_headers.js
+++ functions/url/get_headers.js
@@ -3,14 +3,14 @@ function get_headers(url, format) {
     // +    bugfixed by: Brett Zamir (http://brettz9.blogspot.com)
     // %        note 1: This function uses XmlHttpRequest and cannot retrieve resource from different domain.
     // %        note 1: Synchronous so may lock up browser, mainly here for study purposes.
-    // *     example 1: get_headers('http://kevin.vanzonneveld.net/pj_test_supportfile_1.htm');
-    // *     returns 1: '123'
+    // *     example 1: get_headers('http://kevin.vanzonneveld.net/pj_test_supportfile_1.htm')[0];
+    // *     returns 1: 'Date: Wed, 13 May 2009 23:53:11 GMT'
 
     var req = window.ActiveXObject ? new ActiveXObject("Microsoft.XMLHTTP") : new XMLHttpRequest();
     if (!req) {
         throw new Error('XMLHttpRequest not supported');
     }
-    var tmp, headers, pair, i;
+    var tmp, headers, pair, i, j = 0;
 
     req.open('HEAD', url, false);
     req.send(null);
@@ -29,9 +29,8 @@ function get_headers(url, format) {
             pair = tmp[i].split(':');
             headers[pair.splice(0, 1)] = pair.join(':').substring(1);
         } else {
-            headers[headers.length] = tmp[i];
+            headers[j++] = tmp[i];
         }
     }
-
     return headers;
 }
