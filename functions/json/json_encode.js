722aa7aa847e4ac68b2f02976417b21c94595a82
Brett Zamir <brettz9@yahoo.com>
2009-05-16 08:05:29 +0000

    Be consistent and use all Unicode escape sequences

--- functions/json/json_encode.js
+++ functions/json/json_encode.js
@@ -16,7 +16,7 @@ function json_encode(mixed_val) {
     var value = mixed_val;
 
     var quote = function (string) {
-        var escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
+        var escapable = /[\\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
         var meta = {    // table of character substitutions
             '\b': '\\b',
             '\t': '\\t',
