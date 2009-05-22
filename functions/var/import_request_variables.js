0142cdbd82ee173b8a9df1897f3e97e70ab72c4b
Brett Zamir <brettz9@yahoo.com>
2009-05-22 01:11:00 +0000

    Changed import_request_variables to allow custom object as target instead of window

--- functions/var/import_request_variables.js
+++ functions/var/import_request_variables.js
@@ -7,29 +7,34 @@ function import_request_variables (types, prefix) {
     // *        example 1: import_request_variables('gc', 'pr_');
     // *        results 1: pr_snack == 'yummy'
 
-    var i = 0, current = '', url = '', vars = '', arrayBracketPos = -1, arrName='', win = this.window;
+    var i = 0, current = '', url = '', vars = '', arrayBracketPos = -1, arrName='', win = this.window, targetObj = this.window;
     prefix = prefix || '';
 
+    if (this.php_js && this.php_js.ini && this.php_js.ini['phpjs.requestVarsObj'] &&
+        this.php_js.ini['phpjs.requestVarsObj'].local_value) { // Allow designated object to be used instead of window
+        targetObj = this.php_js.ini['phpjs.requestVarsObj'].local_value;
+    }
+
     if (/g/i.test(types)) { // GET
-        for(i = 0, url = win.location.href, vars = url.substring(url.lastIndexOf("?") + 1, url.length).split("&"); i < vars.length;i++){
+        for(i = 0, url = win.location.href, vars = url.substring(url.lastIndexOf("?") + 1, url.length).split("&"); i < vars.length; i++){
             current = vars[i].split("=");
             arrayBracketPos = current[0].indexOf('[');
             if (arrayBracketPos !== -1) {
                 arrName = current[0].substring(0, arrayBracketPos);
-                if (!win[prefix+arrName]) {
-                    win[prefix+arrName] = [];
+                if (!targetObj[prefix+arrName]) {
+                    targetObj[prefix+arrName] = [];
                 }
-                win[prefix+arrName].push(current[1] || null);
+                targetObj[prefix+arrName].push(current[1] || null);
             }
             else {
-                win[prefix+current[0]] = current[1] || null;
+                targetObj[prefix+current[0]] = current[1] || null;
             }
         }
     }
     if (/c/i.test(types)) { // COOKIE
         for(i = 0, vars = win.document.cookie.split("&"); i < vars.length;i++){
             current = vars[i].split("=");
-            win[prefix+current[0]] = current[1].split(";")[0] || null;
+            targetObj[prefix+current[0]] = current[1].split(";")[0] || null;
         }
     }
 }
