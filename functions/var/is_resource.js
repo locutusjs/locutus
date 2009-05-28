f882dd8d745ebf77a5384d6502e88cffd15f98ca
Brett Zamir <brettz9@yahoo.com>
2009-05-28 22:38:36 +0000

    Shortened is_resource() (Thanks Luis Salazar!)

--- functions/var/is_resource.js
+++ functions/var/is_resource.js
@@ -1,12 +1,9 @@
 function is_resource (handle) {
     // http://kevin.vanzonneveld.net
     // +   original by: Brett Zamir (http://brettz9.blogspot.com)
+    // +   improved by: Luis Salazar (http://www.freaky-media.com/)
     // *     example 1: is_resource('a');
     // *     returns 1: false
 
-    if (!handle || typeof handle !== 'object' || !handle.constructor || handle.constructor.name !== 'PHPJS_Resource') {
-        return false;
-    }
-
-    return true;
+    return !(!handle || typeof handle !== 'object' || !handle.constructor || handle.constructor.name !== 'PHPJS_Resource');
 }
