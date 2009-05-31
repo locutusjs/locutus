07c7edf2df25500558fca7e29be2938882c0bc9c
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-05-31 18:31:03 +0000

    in_array Performance optimization

--- functions/array/in_array.js
+++ functions/array/in_array.js
@@ -1,17 +1,33 @@
 function in_array(needle, haystack, argStrict) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: vlado houba
     // *     example 1: in_array('van', ['Kevin', 'van', 'Zonneveld']);
     // *     returns 1: true
+    // *     example 2: in_array('vlado', {0: 'Kevin', vlado: 'van', 1: 'Zonneveld'});
+    // *     returns 2: false
+    // *     example 3: in_array(1, ['1', '2', '3']);
+    // *     returns 3: true
+    // *     example 3: in_array(1, ['1', '2', '3'], false);
+    // *     returns 3: true
+    // *     example 4: in_array(1, ['1', '2', '3'], true);
+    // *     returns 4: false
 
-    var found = false, key, strict = !!argStrict;
+    var key = '', strict = !!argStrict;
 
-    for (key in haystack) {
-        if ((strict && haystack[key] === needle) || (!strict && haystack[key] == needle)) {
-            found = true;
-            break;
+    if (strict) {
+        for (key in haystack) {
+            if (haystack[key] === needle) {
+                return true;
+            }
+        }
+    } else {
+        for (key in haystack) {
+            if (haystack[key] == needle) {
+                return true;
+            }
         }
     }
 
-    return found;
+    return false;
 }
