3b319af707b8f2f7fb0cded19e0e54e58f4ebfab
Brett Zamir <brettz9@yahoo.com>
2009-06-01 09:12:44 +0000

    Fixing number_format()... Thanks Kheang Hok Chin and davook!

--- functions/strings/number_format.js
+++ functions/strings/number_format.js
@@ -10,7 +10,9 @@ function number_format( number, decimals, dec_point, thousands_sep ) {
     // +    revised by: Luke Smith (http://lucassmith.name)
     // +     bugfix by: Diogo Resende
     // +     bugfix by: Rival
-    // %        note 1: For 1000.55 result with precision 1 in FF/Opera is 1,000.5, but in IE is 1,000.6
+    // +     input by: Kheang Hok Chin (http://www.distantia.ca/)
+    // +     improved by: davook
+    // +     improved by: Brett Zamir (http://brett-zamir.me)
     // *     example 1: number_format(1234.56);
     // *     returns 1: '1,235'
     // *     example 2: number_format(1234.56, 2, ',', ' ');
@@ -23,16 +25,26 @@ function number_format( number, decimals, dec_point, thousands_sep ) {
     // *     returns 5: '1,000'
     // *     example 6: number_format(67.311, 2);
     // *     returns 6: '67.31'
-
+    // *     example 7: number_format(1000.55, 1);
+    // *     returns 7: '1,000.6'
+    // *     example 8: number_format(67000, 5, ',', '.');
+    // *     returns 8: '67.000,00000'
+    // *     example 9: number_format(0.9, 0);
+    // *     returns 9: '1'
     var n = number, prec = decimals;
+    var toFixedFix = function (n,prec) {
+        var k = Math.pow(10,prec);
+        return (Math.round(n*k)/k).toString();
+    };
+
     n = !isFinite(+n) ? 0 : +n;
     prec = !isFinite(+prec) ? 0 : Math.abs(prec);
-    var sep = (typeof thousands_sep == "undefined") ? ',' : thousands_sep;
-    var dec = (typeof dec_point == "undefined") ? '.' : dec_point;
+    var sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep;
+    var dec = (typeof dec_point === 'undefined') ? '.' : dec_point;
 
-    var s = (prec > 0) ? n.toFixed(prec) : Math.round(n).toFixed(prec); //fix for IE parseFloat(0.55).toFixed(0) = 0;
+    var s = (prec > 0) ? toFixedFix(n, prec) : toFixedFix(Math.round(n), prec); //fix for IE parseFloat(0.55).toFixed(0) = 0;
 
-    var abs = Math.abs(n).toFixed(prec);
+    var abs = toFixedFix(Math.abs(n), prec);
     var _, i;
 
     if (abs >= 1000) {
@@ -41,11 +53,12 @@ function number_format( number, decimals, dec_point, thousands_sep ) {
 
         _[0] = s.slice(0,i + (n < 0)) +
               _[0].slice(i).replace(/(\d{3})/g, sep+'$1');
-
         s = _.join(dec);
     } else {
         s = s.replace('.', dec);
     }
-
+    if (s.indexOf(dec) === -1 && prec > 1) {
+        s += dec+new Array(prec).join(0)+'0';
+    }
     return s;
 }
