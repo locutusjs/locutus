0e57e3cddfb559d2fa0301ebb31a56077c11b517
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-06-02 08:50:44 +0000

    Revised uniqid

--- functions/misc/uniqid.js
+++ functions/misc/uniqid.js
@@ -1,111 +1,48 @@
-function uniqid() {
-    // http://kevin.vanzonneveld.net
+function uniqid( prefix, more_entropy) {
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // -    depends on: dechex
+    // +    revised by: Kankrelune (http://www.webfaktory.info/)
     // %        note 1: Uses an internal counter (in php_js global) to avoid collision
-    // %        note 2: Not perfect yet. We need to checkout the PHP source
-    // %        note 2: to find out how they generate this exactly
-    // %        note 2: but for now we have a uniqid that generates unique numbers
-    // %        note 2: similar to PHP's uniqid
-    // *     example 1: uniqid(); // delays for 2 seconds
-    // *     returns 1: '49c6ad2705df7'
-    
-    // BEGIN REDUNDANT
-    this.php_js = this.php_js || {};
-    // END REDUNDANT
+    // *     example 1: uniqid();
+    // *     returns 1: 'a30285b160c14'
+    // *     example 2: uniqid('foo');
+    // *     returns 2: 'fooa30285b1cd361'
+    // *     example 3: uniqid('bar', true);
+    // *     returns 3: 'bara20285b23dfd1.31879087'
 
-    if (!this.php_js.uniqid) {
-        var date = new Date();
-        this.php_js.uniqid = date.getTime()/1000;
+    if(typeof prefix == 'undefined') {
+        prefix = "";
     }
 
-    this.php_js.uniqid++;
+    var retId;
+    var formatSeed = function(seed, reqWidth) {
+        seed = parseInt(seed,10).toString(16); // to hex str
+        if (reqWidth < seed.length) { // so long we split
+            return seed.slice(seed.length - reqWidth);
+        }
+        if (reqWidth > seed.length) { // so short we pad
+            return Array(1 + (reqWidth - seed.length)).join('0')+seed;
+        }
+        return seed;
+    };
 
-    return this.dechex(this.php_js.uniqid)+this.dechex('10243256');
+    // BEGIN REDUNDANT
+    if (!this.php_js) {
+        this.php_js = {};
+    }
+    // END REDUNDANT
+    if (!this.php_js.uniqidSeed) { // init seed with big random int
+        this.php_js.uniqidSeed = Math.floor(Math.random() * 0x75bcd15);
+    }
+    this.php_js.uniqidSeed++;
+
+    retId  = prefix; // start with prefix, add current milliseconds hex string
+    retId += formatSeed(parseInt(new Date().getTime()/1000,10),8);
+    retId += formatSeed(this.php_js.uniqidSeed,5); // add seed hex string
+
+    if(more_entropy) {
+        // for more entropy we add a float lower to 10
+        retId += (Math.random()*10).toFixed(8).toString();
+    }
+
+    return retId;
 }
-
-// Replacement function below still needs some work, currently if you run it 3 times it returns
-// 4a22bc4100000
-// 4a22bc4100000
-// 4a22bc4100000
-//
-// We can utilize the milliseconds in time, don't divide by 1000 so soon
-//
-//function uniqid(prefix, more_entropy, reduce_collision) {
-//    // http://kevin.vanzonneveld.net
-//    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-//    // +    revised by: Kankrelune (http://www.webfaktory.info/)
-//    // %        note 1: Introduced a 3rd parameter: reduce_collision.
-//    // %        note 1: Non-existent in PHP but uses an internal hash table
-//    // %        note 1: to eliminate the possibility of collision within
-//    // %        note 1: one process.
-//    // %        note 1: This should only be used in rare cases cause
-//    // %        note 1: keeping track of uniqids isn't free.
-//    // *     example 1: uniqid();
-//    // *     returns 1: '4a2027b600c14'
-//    // *     example 2: uniqid('foo', true, true);
-//    // *     returns 2: 'fooa20285b1cd361.31879087'
-//    // *     example 3: uniqid('foo', false, true);
-//    // *     returns 3: 'bara30285b1cd361'
-//
-//    prefix +=  '';
-//
-//    var now = new Date().getTime()/1000;
-//    var sec = parseInt(now, 10);
-//    var usec = Math.round((now-sec)*1000)/1000;
-//    var uniqid = '';
-//
-//    var formatBase = function(value, reqWidth) {
-//        value = (value >>> 0).toString(16);
-//
-//        if (reqWidth > value.length) {
-//            return Array(1 + (reqWidth - value.length) >>> 0).join('0') + value;
-//        }
-//        if (reqWidth < value.length) {
-//            return value.slice(0,reqWidth);
-//        }
-//
-//        return value;
-//    };
-//
-//    var getRandSeed = function() {
-//        return [
-//        0xa524,0x11f9b,0x11c3a,0x7f5e,0xbe31,0x585a,0xc9c9,0xbee7,0x6a0f,0x8cb2,
-//        0x713e,0x11b99,0x4b7f,0x81f9,0x5e8d,0x15f34,0x57a9,0x10658,0x300d,0xa5da,
-//        0x13150,0xdf23,0x31b5,0x16fbc,0x17260,0xda84,0x15d13,0x17dc4,0x6263,0x11909,
-//        0x147ab,0x99b0,0xacf1,0x10da2,0xeb0f,0x9616,0x3e5b,0xd8dd,0x9331,0x13cb4,
-//        0x6701,0x148e3,0x139a4,0x4e74,0x6e6c,0x1230a,0x73e9,0x4b87,0x49c2,0x1357a,
-//        0x96cb,0x5010,0x9bc6,0x4588,0x9306,0x18107,0x1599f,0x7c97,0x16229,0x441c
-//        ][Math.floor( ( Math.random( ) * this.length ) )];
-//    };
-//
-//    var makeSeed = function(seed) {
-//        return seed * ((((Math.random() * getRandSeed()) * (Math.random() * getRandSeed())) * getRandSeed()) * getRandSeed());
-//    };
-//
-//    // for more entropy we add a float lower to 10
-//    if(more_entropy) {
-//        return prefix+formatBase(sec,8)+formatBase(makeSeed(usec),5)+(Math.random()*10).toFixed(8);
-//    }
-//
-//    uniqid = prefix+formatBase(sec,8)+formatBase(makeSeed(usec),5);
-//
-//    if (reduce_collision) {
-//        // BEGIN REDUNDANT
-//        this.php_js = this.php_js || {};
-//        // END REDUNDANT
-//
-//        if (!this.php_js.uniqids) {
-//            this.php_js.uniqids = {};
-//        }
-//
-//        if (this.php_js.uniqids[uniqid]) {
-//            // Collision detected. Retry
-//            return this.uniqid(prefix, more_entropy, reduce_collision);
-//        } else {
-//            this.php_js.uniqids[uniqid] = true;
-//        }
-//    }
-//
-//    return uniqid;
-//}
