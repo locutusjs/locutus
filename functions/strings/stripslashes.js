4b59d09065b62e2f0951570df055eb6f1fe02668
Brett Zamir <brettz9@yahoo.com>
2009-06-03 23:24:07 +0000

    stripslashes changed

--- functions/strings/stripslashes.js
+++ functions/strings/stripslashes.js
@@ -1,15 +1,27 @@
 function stripslashes( str ) {
-    // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +   improved by: Ates Goral (http://magnetiq.com)
     // +      fixed by: Mick@el
     // +   improved by: marrtins
     // +   bugfixed by: Onno Marsman
     // +   improved by: rezna
+    // +   input by: Rick
+    // +   reimplemented by: Brett Zamir (http://brett-zamir.me)
     // *     example 1: stripslashes('Kevin\'s code');
     // *     returns 1: "Kevin's code"
     // *     example 2: stripslashes('Kevin\\\'s code');
     // *     returns 2: "Kevin\'s code"
-
-    return (str+'').replace(/\0/g, '0').replace(/\\([\\'"])/g, '$1');
+    str = (str+'').replace(/\\(.?)/g, function (s, n1) {
+        switch(n1) {
+            case '\\': // reduce to single backslash
+                return '\\';
+            case '0': // preserve for replace below
+                return '\\0';
+            case '': // backslash at end of input
+                return '';
+            default: // reduce to character without backslash
+                return n1;
+        }
+    });
+    return str.replace(/\\0/g, '\0');
 }
