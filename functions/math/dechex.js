fb09ede424c11ca519a2e5b822c02d105386e9e6
Brett Zamir <brettz9@yahoo.com>
2009-06-10 21:05:02 +0000

    Fix to handle negative numbers in PHP way--thanks pilus!

--- functions/math/dechex.js
+++ functions/math/dechex.js
@@ -2,10 +2,13 @@ function dechex(number) {
     // http://kevin.vanzonneveld.net
     // +   original by: Philippe Baumann
     // +   bugfixed by: Onno Marsman
+    // +   improved by: pilus
     // *     example 1: dechex(10);
     // *     returns 1: 'a'
     // *     example 2: dechex(47);
     // *     returns 2: '2f'
-    
+	if (number < 0)	{
+	   return (0xFFFFFFFF+number+1).toString(16);
+	}
     return parseInt(number, 10).toString(16);
 }
