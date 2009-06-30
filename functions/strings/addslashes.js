c2c24df4b78061a723acb04899c96097d974080a
Brett Zamir <brettz9@yahoo.com>
2009-06-30 09:43:30 +0000

    Rmv warning per Denny Wardhana (thanks!)

--- functions/strings/addslashes.js
+++ functions/strings/addslashes.js
@@ -5,8 +5,10 @@ function addslashes( str ) {
     // +   improved by: marrtins
     // +   improved by: Nate
     // +   improved by: Onno Marsman
+    // +   input by: Denny Wardhana
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
     // *     example 1: addslashes("kevin's birthday");
     // *     returns 1: 'kevin\'s birthday'
  
-    return (str+'').replace(/([\\"'])/g, "\\$1").replace(/\0/g, "\\0");
+    return (str+'').replace(/([\\"'])/g, "\\$1").replace(/\u0000/g, "\\0");
 }
