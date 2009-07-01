ebbc62cab7058cff43adb231b671a7db54ca51f4
Brett Zamir <brettz9@yahoo.com>
2009-07-01 16:40:20 +0000

    implementing array_replace() functions (PHP 5.3+; now documented)

---
+++ functions/array/array_replace.js
@@ -0,0 +1,22 @@
+function array_replace (arr) {
+    // +   original by: Brett Zamir (http://brett-zamir.me)
+    // *     example 1: array_replace(["orange", "banana", "apple", "raspberry"], {0 : "pineapple", 4 : "cherry"}, {0:"grape"});
+    // *     returns 1: {0: 'grape', 1: 'banana', 2: 'apple', 3: 'raspberry', 4: 'cherry'}
+
+    if (arguments.length < 2) {
+        throw new Error('There should be at least 2 arguments passed to array_replace()');
+    }
+
+    // Although docs state that the arguments are passed in by reference, it seems they are not altered, but rather the copy that is returned (just guessing), so we make a copy here, instead of acting on arr itself
+    var retObj = {};
+    for (var prop in arr) {
+        retObj[prop] = arr[prop];
+    }
+
+    for (var i = 1; i < arguments.length; i++) {
+        for (var p in arguments[i]) {
+            retObj[p] = arguments[i][p];
+        }
+    }
+    return retObj;
+}
---
+++ functions/array/array_replace_recursive.js
@@ -0,0 +1,27 @@
+function array_replace_recursive (arr) {
+    // +   original by: Brett Zamir (http://brett-zamir.me)
+    // *     example 1: array_replace_recursive({'citrus' : ["orange"], 'berries' : ["blackberry", "raspberry"]}, {'citrus' : ['pineapple'], 'berries' : ['blueberry']});
+    // *     returns 1: {citrus : ['pineapple'], berries : ['blueberry', 'raspberry']}
+
+    if (arguments.length < 2) {
+        throw new Error('There should be at least 2 arguments passed to array_replace_recursive()');
+    }
+
+    // Although docs state that the arguments are passed in by reference, it seems they are not altered, but rather the copy that is returned (just guessing), so we make a copy here, instead of acting on arr itself
+    var retObj = {};
+    for (var prop in arr) {
+        retObj[prop] = arr[prop];
+    }
+
+    for (var i = 1; i < arguments.length; i++) {
+        for (var p in arguments[i]) {
+            if (typeof retObj[p] === 'object' && retObj[p] !== null) {
+                retObj[p] = this.array_replace_recursive(retObj[p], arguments[i][p]);
+            }
+            else {
+                retObj[p] = arguments[i][p];
+            }
+        }
+    }
+    return retObj;
+}
