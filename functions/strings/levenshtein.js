74a0b2f30aa6d88235df7d13cde5eadda7573e63
Brett Zamir <brettz9@yahoo.com>
2009-07-04 06:48:41 +0000

    reimplementation of levenshtein by Alexander M Beedie (Thanks!)

--- functions/strings/levenshtein.js
+++ functions/strings/levenshtein.js
@@ -1,13 +1,25 @@
-function levenshtein (a, b){
-    // http://kevin.vanzonneveld.net
-    // +      original by: Carlos R. L. Rodrigues (http://www.jsfromhell.com)
-    // +      bugfixed by: Onno Marsman
-    // +       revised by: Andrea Giammarchi (http://webreflection.blogspot.com)
-    // + reimplemented by: Brett Zamir (http://brett-zamir.me)
-    // *        example 1: levenshtein('Kevin van Zonneveld', 'Kevin van Sommeveld');
-    // *        returns 1: 3
-    
-    var min=Math.min, len1=0, len2=0, I=0, i=0, d=[], c='', j=0, J=0;
+function levenshtein(s1,s2){
+      // http://kevin.vanzonneveld.net
+      // +            original by: Carlos R. L. Rodrigues (http://www.jsfromhell.com)
+      // +            bugfixed by: Onno Marsman
+      // +             revised by: Andrea Giammarchi (http://webreflection.blogspot.com)
+      // + reimplemented by: Brett Zamir (http://brett-zamir.me)
+      // + reimplemented by: Alexander M Beedie
+      // *                example 1: levenshtein('Kevin van Zonneveld', 'Kevin van Sommeveld');
+      // *                returns 1: 3
+
+    if (s1 == s2) {
+        return 0;
+    }
+
+    var s1_len = s1.length;
+    var s2_len = s2.length;
+    if (s1_len === 0) {
+        return s2_len;
+    }
+    if (s2_len === 0) {
+        return s1_len;
+    }
 
     // BEGIN STATIC
     var split = false;
@@ -17,33 +29,38 @@ function levenshtein (a, b){
         split=true; // Earlier IE may not support access by string index
     }
     // END STATIC
-    
-    if (a == b) {
-        return 0;
-    }
-    if (!a.length || !b.length) {
-        return b.length || a.length;
-    }
     if (split){
-        a = a.split('');b = b.split('');
+        s1 = s1.split('');
+        s2 = s2.split('');
     }
-    len1 = a.length + 1;
-    len2 = b.length + 1;
-    d = [[0]];
-    while (++i < len2) {
-        d[0][i] = i;
+
+    var v0 = new Array(s1_len+1);
+    var v1 = new Array(s1_len+1);
+
+    var s1_idx=0, s2_idx=0, cost=0;
+    for (s1_idx=0; s1_idx<s1_len+1; s1_idx++) {
+        v0[s1_idx] = s1_idx;
     }
-    i = 0;
-    while (++i < len1) {
-        J = j = 0;
-        c = a[I];
-        d[i] = [i];
-        while (++j < len2) {
-            d[i][j] = min(d[I][j] + 1, d[i][J] + 1, d[I][J] + (c != b[J]));
-            ++J;
+    var char_s1='', char_s2='';
+    for (s2_idx=1; s2_idx<=s2_len; s2_idx++) {
+        v1[0] = s2_idx;
+        char_s2 = s2[s2_idx - 1];
+
+        for (s1_idx=0; s1_idx<s1_len;s1_idx++) {
+            char_s1 = s1[s1_idx];
+            cost = (char_s1 == char_s2) ? 0 : 1;
+            var m_min = v0[s1_idx+1] + 1;
+            var b = v1[s1_idx] + 1;
+            var c = v0[s1_idx] + cost;
+            if (b < m_min) {
+                m_min = b; }
+            if (c < m_min) {
+                m_min = c; }
+            v1[s1_idx+1] = m_min;
         }
-        ++I;
+        var v_tmp = v0;
+        v0 = v1;
+        v1 = v_tmp;
     }
-    
-    return d[len1 - 1][len2 - 1];
+    return v0[s1_len];
 }
