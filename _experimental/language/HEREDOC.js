82d208700b0b9b2f5ceb48c87a829014d3e15ff6
Brett Zamir <brettz9@yahoo.com>
2009-07-29 03:11:12 +0000

    Comment re: alternative to function

--- _experimental/language/HEREDOC.js
+++ _experimental/language/HEREDOC.js
@@ -9,6 +9,7 @@ function HEREDOC (xmllist) {
     // %          note 5: &#x7d; or easier &#123; and &#125; . Actually, one can just escape the opening curly bracket
     // %          note 6: Could be renamed HERE, etc., as the HEREDOC naming is not fixed in PHP either (or make aliases)
     // %          note 7: Unfortunately, this approach will not work to simulate the PHP 5.3+ NOWDOC; you must escape any literal curly brackets manually as mentioned above
+    // %          note 8: Note, JavaScript does allow backslashes appended at the very end of the line to indicatee the line continues--this may be somewhat more convenient than concatenating each line as an independent string
     // *     example 1: var myVar = 9;
     // *     example 1: HEREDOC (<>Here is a string that could go across multiple lines and include variables thus: {myVar} or even add JavaScript thus: {Math.random()}</>);
     // *     returns 1: 'Here is a string that could go across multiple lines and include variables thus: 9 or even add JavaScript thus: 0.9957547511250214'
