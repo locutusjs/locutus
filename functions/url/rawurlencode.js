456d382d0ba305ebdea0d144f3b273c03689c112
Brett Zamir <brettz9@yahoo.com>
2009-08-18 08:09:25 +0000

    Redoing in necessarily more complex form

--- functions/url/rawurlencode.js
+++ functions/url/rawurlencode.js
@@ -16,6 +16,41 @@ function rawurlencode (str) {
     // *     example 3: rawurlencode('http://www.google.nl/search?q=php.js&ie=utf-8&oe=utf-8&aq=t&rls=com.ubuntu:en-US:unofficial&client=firefox-a');
     // *     returns 3: 'http%3A%2F%2Fwww.google.nl%2Fsearch%3Fq%3Dphp.js%26ie%3Dutf-8%26oe%3Dutf-8%26aq%3Dt%26rls%3Dcom.ubuntu%3Aen-US%3Aunofficial%26client%3Dfirefox-a'
 
+    var hexStr = function (dec) {
+        return '%' + dec.toString(16).toUpperCase();
+    };
+
+    var ret = '',
+            unreserved = /[\w.~-]/; // A-Za-z0-9_.~-
     str = (str+'').toString();
-    return encodeURIComponent(str);
+
+    for (var i = 0, dl = str.length; i < dl; i++) {
+        var ch = str.charAt(i);
+        if (unreserved.test(ch)) {
+            ret += ch;
+        }
+        else {
+            var code = str.charCodeAt(i);
+            // Reserved assumed to be in UTF-8, as in PHP
+            if (code < 128) { // 1 byte
+                ret += hexStr(code);
+            }
+            else if (code >= 128 && code < 2048) { // 2 bytes
+                ret += hexStr((code >> 6) | 0xC0);
+                ret += hexStr((code & 0x3F) | 0x80);
+            }
+            else if (code >= 2048 && code < 65536) { // 3 bytes
+                ret += hexStr((code >> 12) | 0xE0);
+                ret += hexStr(((code >> 6) & 0x3F) | 0x80);
+                ret += hexStr((code & 0x3F) | 0x80);
+            }
+            else if (code >= 65536) { // 4 bytes
+                ret += hexStr((code >> 18) | 0xF0);
+                ret += hexStr(((code >> 12) & 0x3F) | 0x80);
+                ret += hexStr(((code >> 6) & 0x3F) | 0x80);
+                ret += hexStr((code & 0x3F) | 0x80);
+            }
+        }
+    }
+    return ret;
 }
--- functions/url/urlencode.js
+++ functions/url/urlencode.js
@@ -10,7 +10,6 @@ function urlencode (str) {
     // +      input by: Brett Zamir (http://brett-zamir.me)
     // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +   improved by: Lars Fischer
-    // +   improved by: Orlando
     // +      input by: Ratheous
     // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
     // %          note 1: This reflects PHP 5.3/6.0+ behavior
@@ -21,6 +20,44 @@ function urlencode (str) {
     // *     example 3: urlencode('http://www.google.nl/search?q=php.js&ie=utf-8&oe=utf-8&aq=t&rls=com.ubuntu:en-US:unofficial&client=firefox-a');
     // *     returns 3: 'http%3A%2F%2Fwww.google.nl%2Fsearch%3Fq%3Dphp.js%26ie%3Dutf-8%26oe%3Dutf-8%26aq%3Dt%26rls%3Dcom.ubuntu%3Aen-US%3Aunofficial%26client%3Dfirefox-a'
 
+    var hexStr = function (dec) {
+        return '%' + dec.toString(16).toUpperCase();
+    };
+
+    var ret = '',
+            unreserved = /[\w.-]/; // A-Za-z0-9_.- // Tilde is not here for historical reasons; to preserve it, use rawurlencode instead
     str = (str+'').toString();
-    return encodeURIComponent(str).replace(/~/g, '%7E').replace(/%20/g, '+');
+
+    for (var i = 0, dl = str.length; i < dl; i++) {
+        var ch = str.charAt(i);
+        if (unreserved.test(ch)) {
+            ret += ch;
+        }
+        else {
+            var code = str.charCodeAt(i);
+            // Reserved assumed to be in UTF-8, as in PHP
+            if (code === 32) {
+                ret += '+'; // %20 in rawurlencode
+            }
+            else if (code < 128) { // 1 byte
+                ret += hexStr(code);
+            }
+            else if (code >= 128 && code < 2048) { // 2 bytes
+                ret += hexStr((code >> 6) | 0xC0);
+                ret += hexStr((code & 0x3F) | 0x80);
+            }
+            else if (code >= 2048 && code < 65536) { // 3 bytes
+                ret += hexStr((code >> 12) | 0xE0);
+                ret += hexStr(((code >> 6) & 0x3F) | 0x80);
+                ret += hexStr((code & 0x3F) | 0x80);
+            }
+            else if (code >= 65536) { // 4 bytes
+                ret += hexStr((code >> 18) | 0xF0);
+                ret += hexStr(((code >> 12) & 0x3F) | 0x80);
+                ret += hexStr(((code >> 6) & 0x3F) | 0x80);
+                ret += hexStr((code & 0x3F) | 0x80);
+            }
+        }
+    }
+    return ret;
 }
