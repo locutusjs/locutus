9be9e2694ce07aea9d79ccba3e9fb30c429632d1
Brett Zamir <brettz9@yahoo.com>
2009-08-18 07:57:25 +0000

    Global replace needed

--- functions/url/urlencode.js
+++ functions/url/urlencode.js
@@ -22,5 +22,5 @@ function urlencode (str) {
     // *     returns 3: 'http%3A%2F%2Fwww.google.nl%2Fsearch%3Fq%3Dphp.js%26ie%3Dutf-8%26oe%3Dutf-8%26aq%3Dt%26rls%3Dcom.ubuntu%3Aen-US%3Aunofficial%26client%3Dfirefox-a'
 
     str = (str+'').toString();
-    return encodeURIComponent(str).replace(/~/g, '%7E').replace(/%20/, '+');
+    return encodeURIComponent(str).replace(/~/g, '%7E').replace(/%20/g, '+');
 }
