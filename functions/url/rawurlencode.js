a701314439992baddf5b6af710d63feaf1d85f51
Brett Zamir <brettz9@yahoo.com>
2009-08-18 07:52:43 +0000

    UTF-8 encoding (PHP 5.3/6.0+)

--- functions/url/rawurlencode.js
+++ functions/url/rawurlencode.js
@@ -7,73 +7,50 @@ function rawurlencode (str) {
     // +      input by: Michael Grier
     // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
     // +      input by: Ratheous
+    // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
+    // %          note 1: This reflects PHP 5.3/6.0+ behavior
     // *     example 1: rawurlencode('Kevin van Zonneveld!');
     // *     returns 1: 'Kevin%20van%20Zonneveld%21'
     // *     example 2: rawurlencode('http://kevin.vanzonneveld.net/');
     // *     returns 2: 'http%3A%2F%2Fkevin.vanzonneveld.net%2F'
     // *     example 3: rawurlencode('http://www.google.nl/search?q=php.js&ie=utf-8&oe=utf-8&aq=t&rls=com.ubuntu:en-US:unofficial&client=firefox-a');
     // *     returns 3: 'http%3A%2F%2Fwww.google.nl%2Fsearch%3Fq%3Dphp.js%26ie%3Dutf-8%26oe%3Dutf-8%26aq%3Dt%26rls%3Dcom.ubuntu%3Aen-US%3Aunofficial%26client%3Dfirefox-a'
- 
-    var hash_map = {}, unicodeStr='', hexEscStr='';
-    var ret = str.toString();
 
-    var replacer = function (search, replace, str) {
-        var tmp_arr = [];
-        tmp_arr = str.split(search);
-        return tmp_arr.join(replace);
+    var hexStr = function (dec) {
+        return '%' + dec.toString(16).toUpperCase();
     };
 
-    // The hash_map is identical to the one in urldecode.
-    hash_map["'"]   = '%27';
-    hash_map['(']   = '%28';
-    hash_map[')']   = '%29';
-    hash_map['*']   = '%2A';
-    hash_map['~']   = '%7E';
-    hash_map['!']   = '%21';
-    hash_map['\u20AC'] = '%80';
-    hash_map['\u0081'] = '%81';
-    hash_map['\u201A'] = '%82';
-    hash_map['\u0192'] = '%83';
-    hash_map['\u201E'] = '%84';
-    hash_map['\u2026'] = '%85';
-    hash_map['\u2020'] = '%86';
-    hash_map['\u2021'] = '%87';
-    hash_map['\u02C6'] = '%88';
-    hash_map['\u2030'] = '%89';
-    hash_map['\u0160'] = '%8A';
-    hash_map['\u2039'] = '%8B';
-    hash_map['\u0152'] = '%8C';
-    hash_map['\u008D'] = '%8D';
-    hash_map['\u017D'] = '%8E';
-    hash_map['\u008F'] = '%8F';
-    hash_map['\u0090'] = '%90';
-    hash_map['\u2018'] = '%91';
-    hash_map['\u2019'] = '%92';
-    hash_map['\u201C'] = '%93';
-    hash_map['\u201D'] = '%94';
-    hash_map['\u2022'] = '%95';
-    hash_map['\u2013'] = '%96';
-    hash_map['\u2014'] = '%97';
-    hash_map['\u02DC'] = '%98';
-    hash_map['\u2122'] = '%99';
-    hash_map['\u0161'] = '%9A';
-    hash_map['\u203A'] = '%9B';
-    hash_map['\u0153'] = '%9C';
-    hash_map['\u009D'] = '%9D';
-    hash_map['\u017E'] = '%9E';
-    hash_map['\u0178'] = '%9F';
+    var ret = '',
+            unreserved = /[\w.~-]/; // A-Za-z0-9_.~-
+    str = (str+'').toString();
 
-
-    // Begin with encodeURIComponent, which most resembles PHP's encoding functions
-    ret = encodeURIComponent(ret);
-
-    for (unicodeStr in hash_map) {
-        hexEscStr = hash_map[unicodeStr];
-        ret = replacer(unicodeStr, hexEscStr, ret); // Custom replace. No regexing
+    for (var i = 0, dl = str.length; i < dl; i++) {
+        var ch = str.charAt(i);
+        if (unreserved.test(ch)) {
+            ret += ch;
+        }
+        else {
+            var code = str.charCodeAt(i);
+            // Reserved assumed to be in UTF-8, as in PHP
+            if (code < 128) { // 1 byte
+                ret += hexStr(code);
+            }
+            else if (code >= 128 && code < 2048) { // 2 bytes
+                ret += hexStr((code >> 6) | 0xC0);
+                ret += hexStr((code & 0x3F) | 0x80);
+            }
+            else if (code >= 2048 && code < 65536) { // 3 bytes
+                ret += hexStr((code >> 12) | 0xE0);
+                ret += hexStr(((code >> 6) & 0x3F) | 0x80);
+                ret += hexStr((code & 0x3F) | 0x80);
+            }
+            else if (code >= 65536) { // 4 bytes
+                ret += hexStr((code >> 18) | 0xF0);
+                ret += hexStr(((code >> 12) & 0x3F) | 0x80);
+                ret += hexStr(((code >> 6) & 0x3F) | 0x80);
+                ret += hexStr((code & 0x3F) | 0x80);
+            }
+        }
     }
-
-    // Uppercase for full PHP compatibility
-    return ret.replace(/(\%([a-z0-9]{2}))/g, function (full, m1, m2) {
-        return "%"+m2.toUpperCase();
-    });
+    return ret;
 }
