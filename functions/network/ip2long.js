51ac74a14bb0237b202a4a19dc455f45e92da1d5
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-09-04 18:44:23 +0200

    Revised ip2long

--- functions/network/ip2long.js
+++ functions/network/ip2long.js
@@ -2,18 +2,12 @@ function ip2long ( ip_address ) {
     // http://kevin.vanzonneveld.net
     // +   original by: Waldo Malqui Silva
     // +   improved by: Victor
+    // +    revised by: fearphage (http://http/my.opera.com/fearphage/)
     // *     example 1: ip2long( '192.0.34.166' );
     // *     returns 1: 3221234342
-    
-    var output = false;
-    var parts = [];
-    if (ip_address.match(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)) {
-        parts  = ip_address.split('.');
-        output = ( parts[0] * 16777216 +
-        ( parts[1] * 65536 ) +
-        ( parts[2] * 256 ) +
-        ( parts[3] * 1 ) );
-    }
-     
-    return output;
+
+    var parts = ip_address.match(/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/);
+    return parts
+        ? parts[1] * 16777216 + parts[2] * 65536 + parts[3] * 256 + parts[4] * 1
+        : false;
 }
