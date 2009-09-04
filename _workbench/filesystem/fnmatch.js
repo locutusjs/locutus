c5f36a6b87b04c39ac2c96750e0de9f77e4be2d5
Brett Zamir <brettz9@yahoo.com>
2009-09-04 18:27:55 +0800

    Change tabs to spaces

--- _workbench/filesystem/fnmatch.js
+++ _workbench/filesystem/fnmatch.js
@@ -12,12 +12,12 @@ function fnmatch (pattern, string, flags) {
 
 
 /*
-#define	FNM_NOESCAPE	0x01	// Disable backslash escaping.
-#define	FNM_PATHNAME	0x02	// Slash must be matched by slash.
-#define	FNM_PERIOD	0x04	// Period must be matched by period.
-#define	FNM_LEADING_DIR	0x08	// Ignore /<tail> after Imatch.
-#define	FNM_CASEFOLD	0x10	// Case insensitive search.
-#define FNM_PREFIX_DIRS	0x20	// Directory prefixes of pattern match too.
+#define    FNM_NOESCAPE    0x01    // Disable backslash escaping.
+#define    FNM_PATHNAME    0x02    // Slash must be matched by slash.
+#define    FNM_PERIOD    0x04    // Period must be matched by period.
+#define    FNM_LEADING_DIR    0x08    // Ignore /<tail> after Imatch.
+#define    FNM_CASEFOLD    0x10    // Case insensitive search.
+#define FNM_PREFIX_DIRS    0x20    // Directory prefixes of pattern match too.
 */
     // all integers
     switch (flags) { // allow bitwise or'ed?
