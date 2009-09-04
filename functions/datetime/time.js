459b16b6d454d4e30386729ee2640d0013449578
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-09-04 19:07:52 +0200

    Use the base integer to determine the second

--- functions/datetime/time.js
+++ functions/datetime/time.js
@@ -3,8 +3,9 @@ function time () {
     // +   original by: GeekFG (http://geekfg.blogspot.com)
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +   improved by: metjay
+    // +   improved by: HKM
     // *     example 1: timeStamp = time();
     // *     results 1: timeStamp > 1000000000 && timeStamp < 2000000000
     
-    return Math.round(new Date().getTime()/1000);
+    return Math.floor(new Date().getTime()/1000);
 }
