c89c779e7826ff74e54be3f481eb092fd6e93741
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-10-08 13:51:00 +0200

    Added empty so dir exists

---
+++ gittydown.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+git pull origin master
+git submodule status
+git submodule update --init
+
+if [ "${1}" = "-modules" ]; then
+    for mod in `git submodule status | awk '{ print $2 }'`; do
+        if [ -z "${2}" ] || [ "${2}" = "${mod}" ]; then
+            changed=0
+            echo -n "${mod} "
+            cd ${mod}
+            git pull origin master && changed=1
+            git checkout master
+            cd -
+            if [ "${changed}" -ne 1 ]; then
+                echo "not changed."
+            else
+                echo "changed. updating references"
+                git add ${mod}
+                git commit -m "Updated submodule ${mod}"
+            fi
+        fi
+    done
+    
+    exit 0
+fi
