718895323939ff5e0f674a537e9a4546fce71c6e
Kevin van Zonneveld <kevin@vanzonneveld.net>
2009-10-25 13:58:10 +0100

    Faster implementation by Theriault

--- functions/strings/str_split.js
+++ functions/strings/str_split.js
@@ -1,24 +1,14 @@
-function str_split ( f_string, f_split_length){
+function str_split (string, split_length) {
     // http://kevin.vanzonneveld.net
     // +     original by: Martijn Wieringa
     // +     improved by: Brett Zamir (http://brett-zamir.me)
     // +     bugfixed by: Onno Marsman
+    // +      revised by: Theriault
     // *         example 1: str_split('Hello Friend', 3);
     // *         returns 1: ['Hel', 'lo ', 'Fri', 'end']
 
-    f_string += '';
-
-    if (f_split_length == undefined) {
-        f_split_length = 1;
+    if (string === undefined || !string.toString || split_length < 1) {
+        return false;
     }
-    if (f_split_length > 0) {
-        var result = [];
-        while (f_string.length > f_split_length) {
-            result[result.length] = f_string.substring(0, f_split_length);
-            f_string = f_string.substring(f_split_length);
-        }
-        result[result.length] = f_string;
-        return result;
-    }
-    return false;
+    return string.toString().match(new RegExp('.{1,' + (split_length || '1') + '}', 'g'));
 }
