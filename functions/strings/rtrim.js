7e0c05cb7c5049dfa89c39fc8460df48aca76948
Brett Zamir <brettz9@yahoo.com>
2009-11-07 19:29:46 +0800

    Fixed special chars not getting escaped

--- functions/strings/rtrim.js
+++ functions/strings/rtrim.js
@@ -4,10 +4,12 @@ function rtrim ( str, charlist ) {
     // +      input by: Erkekjetter
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +   bugfixed by: Onno Marsman
+    // +   input by: rem
+    // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
     // *     example 1: rtrim('    Kevin van Zonneveld    ');
     // *     returns 1: '    Kevin van Zonneveld'
 
-    charlist = !charlist ? ' \\s\u00A0' : (charlist+'').replace(/([\[\]\(\)\.\?\/\*\{\}\+\$\^\:])/g, '$1');
+    charlist = !charlist ? ' \\s\u00A0' : (charlist+'').replace(/([\[\]\(\)\.\?\/\*\{\}\+\$\^\:])/g, '\\$1');
     var re = new RegExp('[' + charlist + ']+$', 'g');
     return (str+'').replace(re, '');
 }
