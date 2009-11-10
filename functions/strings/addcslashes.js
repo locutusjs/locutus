4e63f99e7eef5660fa5364b15d820a9a1e3f27c9
Brett Zamir <brettz9@yahoo.com>
2009-11-10 09:45:39 +0800

    Treat as UTF-8

--- functions/strings/addcslashes.js
+++ functions/strings/addcslashes.js
@@ -7,7 +7,7 @@ function addcslashes (str, charlist) {
     // *     returns 1: "\\f\\o\\o\\[ \\]"
     // *     example 2: addcslashes("zoo['.']", 'z..A'); // Only escape z, period, and A here since not a lower-to-higher range
     // *     returns 2: "\\zoo['\\.']"
-    // *     example 3: addcslashes("@a\u0000\u0010\u00A9", "\0..\37!@\177..\377") == '\\@a\\0\\20\\251'); // Escape as octals those specified and less than 32 (0x20) or greater than 126 (0x7E), but not otherwise
+    // *     example 3: addcslashes("@a\u0000\u0010\u00A9", "\0..\37!@\177..\377") == '\\@a\\000\\020\\302\\251'); // Escape as octals those specified and less than 32 (0x20) or greater than 126 (0x7E), but not otherwise
     // *     returns 3: true
     // *     example 4: addcslashes("\u0020\u007E", "\40..\175") == '\\ ~'); // Those between 32 (0x20 or 040) and 126 (0x7E or 0176) decimal value will be backslashed if specified (not octalized)
     // *     returns 4: true
@@ -17,7 +17,14 @@ function addcslashes (str, charlist) {
     // *     returns 7: "\r\u0007\n"
 
     var target = '', chrs = [], i = 0, j = 0, c = '', next = '', rangeBegin = '', rangeEnd = '', chr = '', begin = 0, end = 0,
-                octalLength = 0, postOctalPos = 0, cca = 0;
+                octalLength = 0, postOctalPos = 0, cca = 0, escHexGrp = [], encoded = '', percentHex = /%([\dA-Fa-f]+)/g;
+    var _pad = function (n, c){
+        if ( (n = n + "").length < c ) {
+            return new Array(++c - n.length).join("0") + n;
+        } else {
+            return n;
+        }
+    };
 
     for (i=0; i < charlist.length; i++) {
         c = charlist[i];
@@ -98,8 +105,19 @@ function addcslashes (str, charlist) {
                     case '\v': target += 'v'; break;
                     case '\b': target += 'b'; break;
                     case '\f': target += 'f'; break;
-                    //default: target += this.sprintf(c, '%03o', c);break;
-                    default: target += cca.toString(8);break;
+                    default:                        
+                        //target += _pad(cca.toString(8), 3);break; // Sufficient for UTF-16
+
+                        encoded = encodeURIComponent(c);
+
+                        // 3-length-padded UTF-8 octets
+                        if ((escHexGrp = percentHex.exec(encoded)) !== null) {
+                            target += _pad(parseInt(escHexGrp[1], 16).toString(8), 3); // already added a slash above
+                        }
+                        while ((escHexGrp = percentHex.exec(encoded)) !== null) {
+                            target += '\\'+_pad(parseInt(escHexGrp[1], 16).toString(8), 3);
+                        }
+                        break;
                 }
             }
             else { // Perform regular backslashed escaping
