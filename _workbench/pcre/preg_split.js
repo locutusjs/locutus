32bcb8120c533834d4cce90a3dddca2c33897983
Brett Zamir <brettz9@yahoo.com>
2009-11-14 10:29:12 +0800

    More change from Marco but still infinite recursion in my tests

--- _workbench/pcre/preg_split.js
+++ _workbench/pcre/preg_split.js
@@ -1,19 +1,27 @@
 function preg_split (pattern, subject, limit, flags) {
     // http://kevin.vanzonneveld.net
-    // +   original by: Marco Marchiò
-    // *     example 1: preg_split(/[\s,]+/, "hypertext language, programming");
-    // *     returns 1: 'hypertext,language,programming'
+    // + original by: Marco Marchi?
+    // * example 1: preg_split(/[\s,]+/, "hypertext language, programming");
+    // * returns 1: [hypertext, language, programming]
+    // * example 2: preg_split('//', 'string', -1, 'PREG_SPLIT_NO_EMPTY');
+    // * returns 2: 
+    // * example 3: var str = 'hypertext language programming';
+    // * example 3: preg_split('/ /', str, -1, 'PREG_SPLIT_OFFSET_CAPTURE');
+    // * returns 3: 
 
-    limit = limit || 0; flags = flags || ''; //Limit and flags are optional
+// First example still causing infinite loop!
+
+    limit = limit || 0; flags = flags || ''; // Limit and flags are optional
     var result, ret=[], index=0, i = 0,
         noEmpty = flags.indexOf("PREG_SPLIT_NO_EMPTY") !== -1,
         delim = flags.indexOf("PREG_SPLIT_DELIM_CAPTURE") !== -1,
-        offset = flags.indexOf("PREG_SPLIT_OFFSET_CAPTURE") !== -1;
-    //non global regexp cause a infinite loop when executing the while,
-    //so if it's not global copy the regexp and add the "g" modifier
-    pattern = pattern.global ? pattern : new RegExp(/^\/(.*)\/\w*$/.exec(pattern.toString())[1],
-                                                                                                /^\/.*\/(\w*)$/.exec(pattern.toString())[1]+"g");
-
+        offset = flags.indexOf("PREG_SPLIT_OFFSET_CAPTURE") !== -1,
+        regexpBody=/^\/(.*)\/\w*$/.exec(pattern.toString())[1],
+        regexpFlags=/^\/.*\/(\w*)$/.exec(pattern.toString())[1];
+        // Non-global regexp causes an infinite loop when executing the while,
+        // so if it's not global, copy the regexp and add the "g" modifier.
+        pattern=pattern.global || typeof pattern!="string" ? pattern :
+            new RegExp(regexpBody, regexpFlags+(regexpFlags.indexOf("g")!==-1 ? "":regexpFlags+"g"));
     var _filter = function(str,strindex) {
         // If the match is empty and the PREG_SPLIT_NO_EMPTY flag is set don't add it
         if(noEmpty && !str.length) {return;}
@@ -22,6 +30,14 @@ function preg_split (pattern, subject, limit, flags) {
         if(offset) {str = [str,strindex];}
         ret.push(str);
     };
+    //Special case for empty regexp
+    if(!regexpBody){
+        result=subject.split("");
+        for(i=0;i<result.length;i++) {
+            _filter(result[i],i);
+        }
+        return ret;
+    }
     // Exec the pattern and get the result
     while(result = pattern.exec(subject)) {
         // Stop if the limit is 1
