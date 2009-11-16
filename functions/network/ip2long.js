2ecd22e0d13bb5deac17f5d52605c3a631a2d08f
Theriault <dustintheriault@gmail.com>
2009-11-16 00:08:49 -0700

    Fixed for FireFox and added comments.

--- functions/network/ip2long.js
+++ functions/network/ip2long.js
@@ -12,19 +12,24 @@ function ip2long(IP) {
     // *     returns 3: false
 
     var i = 0;
-    // Allows decimal, octal, and hexadecimal.
+    // PHP allows decimal, octal, and hexadecimal IP components.
+    // PHP allows between 1 (e.g. 127) to 4 (e.g 127.0.0.1) components.
     IP = IP.match(/^([1-9]\d*|0[0-7]*|0x[\da-f]+)(?:\.([1-9]\d*|0[0-7]*|0x[\da-f]+))?(?:\.([1-9]\d*|0[0-7]*|0x[\da-f]+))?(?:\.([1-9]\d*|0[0-7]*|0x[\da-f]+))?$/i); // Verify IP format.
     if (!IP) {
         return false; // Invalid format.
     }
+    // Reuse IP variable for component counter.
     IP[0] = 0;
     for (i = 1; i < 5; i += 1) {
-        IP[0] += !!IP[i].length;
+        IP[0] += !!((IP[i] || '').length);
         IP[i] = parseInt(IP[i]) || 0;
     }
-    IP.push(256, 256, 256, 256); // Use IP for overflow values. Each component must be less than its relative overflow.
-    IP[4 + IP[0]] *= Math.pow(256, 4 - IP[0]); // Recalculate overflow of last component.
-    if (IP[1] >= IP[5] || IP[2] >= IP[6] || IP[3] >= IP[7] || IP[4] >= IP[8]) { // Verify that nothing overflows.
+    // Continue to use IP for overflow values.
+    // PHP does not allow any component to overflow.
+    IP.push(256, 256, 256, 256);
+    // Recalculate overflow of last component supplied to make up for missing components.
+    IP[4 + IP[0]] *= Math.pow(256, 4 - IP[0]);
+    if (IP[1] >= IP[5] || IP[2] >= IP[6] || IP[3] >= IP[7] || IP[4] >= IP[8]) {
         return false;
     }
     return IP[1] * (IP[0] === 1 || 16777216) + IP[2] * (IP[0] <= 2 || 65536) + IP[3] * (IP[0] <= 3 || 256) + IP[4] * 1;
