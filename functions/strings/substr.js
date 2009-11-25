42a3195a16b6c02f214f9a2f81ae3f620a2d5870
Theriault <dustintheriault@gmail.com>
2009-11-24 18:25:28 -0700

    Second example should return false instead of an empty string.

--- functions/strings/substr.js
+++ functions/strings/substr.js
@@ -1,35 +1,25 @@
-function substr (f_string, f_start, f_length) {
-    // http://kevin.vanzonneveld.net
+function substr (str, start, len) {
+    // Returns part of a string  
+    // 
+    // version: 909.322
+    // discuss at: http://phpjs.org/functions/substr
     // +     original by: Martijn Wieringa
     // +     bugfixed by: T.Wild
     // +      tweaked by: Onno Marsman
+    // +      revised by: Theriault
     // *       example 1: substr('abcdef', 0, -1);
     // *       returns 1: 'abcde'
     // *       example 2: substr(2, 0, -6);
-    // *       returns 2: ''
-
-// (?) Use unicode.semantics and/or unicode.runtime_encoding (e.g., with string wrapped in "binary" or "Binary" class) to
-// allow access of binary (see file_get_contents()) by: charCodeAt(x) & 0xFF (see https://developer.mozilla.org/En/Using_XMLHttpRequest )
-
-// Fix: Handle 4-byte characters
-
-    f_string += '';
-
-    if (f_start < 0) {
-        f_start += f_string.length;
+    // *       returns 2: false
+    str += '';
+    var end = str.length;
+    if (start < 0) {
+        start += end;
     }
-
-    if (f_length == undefined) {
-        f_length = f_string.length;
-    } else if (f_length < 0){
-        f_length += f_string.length;
-    } else {
-        f_length += f_start;
-    }
-
-    if (f_length < f_start) {
-        f_length = f_start;
-    }
-
-    return f_string.substring(f_start, f_length);
+    end = typeof len === 'undefined' ? end : (len < 0 ? len + end : len + start);
+    // PHP returns false if start does not fall within the string.
+    // PHP returns false if the calculated end comes before the calculated start.
+    // PHP returns an empty string if start and end are the same.
+    // Otherwise, PHP returns the portion of the string from start to end.
+    return start >= str.length || start < 0 || start > end ? !1 : str.slice(start, end);
 }
