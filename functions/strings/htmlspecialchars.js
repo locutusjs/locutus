881de8748cf986d025ecfad5f448fbbb8ba7710e
Brett Zamir <brettz9@yahoo.com>
2009-11-25 19:41:55 +0800

    Reimplemented htmlspecialchars functions w/o dependencies, fixing prob, and adding flag/arg support

--- functions/strings/htmlspecialchars.js
+++ functions/strings/htmlspecialchars.js
@@ -1,4 +1,4 @@
-function htmlspecialchars (string, quote_style) {
+function htmlspecialchars (string, quote_style, charset, double_encode) {
     // http://kevin.vanzonneveld.net
     // +   original by: Mirek Slugen
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
@@ -7,22 +7,56 @@ function htmlspecialchars (string, quote_style) {
     // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +    bugfixed by: Brett Zamir (http://brett-zamir.me)
     // +      input by: Ratheous
-    // -    depends on: get_html_translation_table
+    // +      input by: Mailfaker (http://www.weedem.fr/)
+    // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
+    // %        note 1: charset argument not supported
     // *     example 1: htmlspecialchars("<a href='test'>Test</a>", 'ENT_QUOTES');
     // *     returns 1: '&lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;'
+    // *     example 2: htmlspecialchars("ab\"c'd", ['ENT_NOQUOTES', 'ENT_QUOTES']);
+    // *     returns 2: 'ab"c&#039;d'
+    // *     example 3: htmlspecialchars("my "&entity;" is still here", null, null, false);
+    // *     returns 3: 'my &quot;&entity;&quot; is still here'
 
-    var hash_map = {}, symbol = '', tmp_str = '', entity = '';
-    tmp_str = string.toString();
-    
-    if (false === (hash_map = this.get_html_translation_table('HTML_SPECIALCHARS', quote_style))) {
-        return false;
+    var optTemp = 0, i = 0, noquotes= false;
+    if (typeof quote_style === 'undefined' || quote_style === null) {
+        quote_style = 2;
     }
-    
-    hash_map["'"] = '&#039;';
-    for (symbol in hash_map) {
-        entity = hash_map[symbol];
-        tmp_str = tmp_str.split(symbol).join(entity);
+    string = string.toString();
+    if (double_encode !== false) { // Put this first to avoid double-encoding
+        string = string.replace(/&/g, '&amp;');
     }
-    
-    return tmp_str;
+    string = string.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
+    var OPTS = {
+        'ENT_NOQUOTES': 0,
+        'ENT_HTML_QUOTE_SINGLE' : 1,
+        'ENT_HTML_QUOTE_DOUBLE' : 2,
+        'ENT_COMPAT': 2,
+        'ENT_QUOTES': 3,
+        'ENT_IGNORE' : 4,
+    };
+    if (quote_style === 0) {
+        noquotes = true;
+    }
+    if (typeof quote_style !== 'number') { // Allow for a single string or an array of string flags
+        quote_style = [].concat(quote_style);
+        for (i=0; i < quote_style.length; i++) {
+            // Resolve string input to bitwise e.g. 'PATHINFO_EXTENSION' becomes 4
+            if (OPTS[quote_style[i]] === 0) {
+                noquotes = true;
+            }
+            else if (OPTS[quote_style[i]]) {
+                optTemp = optTemp | OPTS[quote_style[i]];
+            }
+        }
+        quote_style = optTemp;
+    }
+    if (quote_style & OPTS.ENT_HTML_QUOTE_SINGLE) {
+        string = string.replace(/'/g, '&#039;');
+    }
+    if (!noquotes) {
+        string = string.replace(/"/g, '&quot;');
+    }
+
+    return string;
 }
--- functions/strings/htmlspecialchars_decode.js
+++ functions/strings/htmlspecialchars_decode.js
@@ -11,24 +11,50 @@ function htmlspecialchars_decode (string, quote_style) {
     // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
     // +      input by: Ratheous
-    // -    depends on: get_html_translation_table
+    // +      input by: Mailfaker (http://www.weedem.fr/)
+    // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
     // *     example 1: htmlspecialchars_decode("<p>this -&gt; &quot;</p>", 'ENT_NOQUOTES');
     // *     returns 1: '<p>this -> &quot;</p>'
     // *     example 2: htmlspecialchars_decode("&amp;quot;");
     // *     returns 2: '&quot;'
 
-    var hash_map = {}, symbol = '', tmp_str = '', entity = '';
-    tmp_str = string.toString();
-    
-    if (false === (hash_map = this.get_html_translation_table('HTML_SPECIALCHARS', quote_style))) {
-        return false;
+    var optTemp = 0, i = 0, noquotes= false;
+    if (typeof quote_style === 'undefined') {
+        quote_style = 2;
     }
+    string = string.toString().replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+    var OPTS = {
+        'ENT_NOQUOTES': 0,
+        'ENT_HTML_QUOTE_SINGLE' : 1,
+        'ENT_HTML_QUOTE_DOUBLE' : 2,
+        'ENT_COMPAT': 2,
+        'ENT_QUOTES': 3,
+        'ENT_IGNORE' : 4,
+    };
+    if (quote_style === 0) {
+        noquotes = true;
+    }
+    if (typeof quote_style !== 'number') { // Allow for a single string or an array of string flags
+        quote_style = [].concat(quote_style);
+        for (i=0; i < quote_style.length; i++) {
+            // Resolve string input to bitwise e.g. 'PATHINFO_EXTENSION' becomes 4
+            if (OPTS[quote_style[i]] === 0) {
+                noquotes = true;
+            }
+            else if (OPTS[quote_style[i]]) {
+                optTemp = optTemp | OPTS[quote_style[i]];
+            }
+        }
+        quote_style = optTemp;
+    }
+    if (quote_style & OPTS.ENT_HTML_QUOTE_SINGLE) {
+        string = string.replace(/&#0*39;/g, "'");
+    }
+    if (!noquotes) {
+        string = string.replace(/&quot;/g, '"');
+    }
+    // Put this in last place to avoid escape being double-decoded
+    string = string.replace(/&amp;/g, '&');
 
-    for (symbol in hash_map) {
-        entity  = hash_map[symbol];
-        tmp_str = tmp_str.split(entity).join(symbol);
-    }
-    tmp_str = tmp_str.split('&#039;').join("'");
-    
-    return tmp_str;
+    return string;
 }
