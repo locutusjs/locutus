79e37134898c1df0faf18e6904abf3033eed353b
Brett Zamir <brettz9@yahoo.com>
2009-12-10 12:36:41 +0800

    Fixed strtr for IE and filter for-in

--- functions/strings/strtr.js
+++ functions/strings/strtr.js
@@ -7,7 +7,10 @@ function strtr (str, from, to) {
     // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +      input by: Taras Bogach
     // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
+    // +      input by: jpfle
+    // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
     // -   depends on: krsort
+    // -   depends on: ini_set
     // *     example 1: $trans = {'hello' : 'hi', 'hi' : 'hello'};
     // *     example 1: strtr('hi all, I said hello', $trans)
     // *     returns 1: 'hello all, I said hi'
@@ -22,45 +25,61 @@ function strtr (str, from, to) {
     // *     example 6: strtr('aa', {'a':1,'aa':2});
     // *     returns 6: '2'
 
-    var fr = '', i = 0, j = 0, lenStr = 0, lenFrom = 0;
+    var fr = '', i = 0, j = 0, lenStr = 0, lenFrom = 0, tmpStrictForIn = false, fromTypeStr = '', toTypeStr = '', istr = '';
     var tmpFrom = [];
-    var tmpTo   = [];
+    var tmpTo = [];
     var ret = '';
     var match = false;
 
     // Received replace_pairs?
     // Convert to normal from->to chars
     if (typeof from === 'object') {
-        this.krsort(from);
+        tmpStrictForIn = this.ini_set('phpjs.strictForIn', false); // Not thread-safe; temporarily set to true
+        from = this.krsort(from);
+        this.ini_set('phpjs.strictForIn', tmpStrictForIn);
+
         for (fr in from) {
-            tmpFrom.push(fr);
-            tmpTo.push(from[fr]);
+            if (from.hasOwnProperty(fr)) {
+                tmpFrom.push(fr);
+                tmpTo.push(from[fr]);
+            }
         }
 
         from = tmpFrom;
-        to   = tmpTo;
+        to = tmpTo;
     }
     
     // Walk through subject and replace chars when needed
     lenStr  = str.length;
     lenFrom = from.length;
+    fromTypeStr = typeof from === 'string';
+    toTypeStr = typeof to === 'string';
+
     for (i = 0; i < lenStr; i++) {
         match = false;
-        for (j = 0; j < lenFrom; j++) {
-            if (str.substr(i, from[j].length) == from[j]) {
-                match = true;
-
-                // Fast forward
-                i = (i + from[j].length)-1;
-                
-                break;
+        if (fromTypeStr) {
+            istr = str.charAt(i);
+            for (j = 0; j < lenFrom; j++) {
+                if (istr == from.charAt(j)) {
+                    match = true;
+                    break;
+                }
             }
         }
-        
-        if (false !== match) {
-            ret += to[j];
+        else {
+            for (j = 0; j < lenFrom; j++) {
+                if (str.substr(i, from[j].length) == from[j]) {
+                    match = true;
+                    // Fast forward
+                    i = (i + from[j].length)-1;
+                    break;
+                }
+            }
+        }
+        if (match) {
+            ret += toTypeStr ? to.charAt(j) : to[j];
         } else {
-            ret += str[i];
+            ret += str.charAt(i);
         }
     }
 
