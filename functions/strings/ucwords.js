946aab6f47204f107d84d2896ea713f2b2e8ef71
Brett Zamir <brettz9@yahoo.com>
2010-01-29 09:43:05 +0800

    JSLint fixes (Thanks, Robin!)

--- functions/strings/ucwords.js
+++ functions/strings/ucwords.js
@@ -1,12 +1,15 @@
-function ucwords (str) {
+function ucwords(str) {
     // http://kevin.vanzonneveld.net
     // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
     // +   improved by: Waldo Malqui Silva
     // +   bugfixed by: Onno Marsman
+    // +   improved by: Robin
     // *     example 1: ucwords('kevin van zonneveld');
     // *     returns 1: 'Kevin Van Zonneveld'
     // *     example 2: ucwords('HELLO WORLD');
     // *     returns 2: 'HELLO WORLD'
 
-    return (str+'').replace(/^(.)|\s(.)/g, function ( $1 ) { return $1.toUpperCase( ); } );
+    return (str + '').replace(/^(.)|\s(.)/g, function ($1) {
+        return $1.toUpperCase();
+    });
 }
