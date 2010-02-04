2a909afb4c917ea53cb6275dbedb763ef62808e0
Brett Zamir <brettz9@yahoo.com>
2010-02-04 09:16:09 +0800

    Remove depdendency note and changed to regular note until we add depdendency (Thanks nitin gupta!)

--- functions/array/array_unique.js
+++ functions/array/array_unique.js
@@ -8,8 +8,8 @@ function array_unique (inputArr) {
     // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +   improved by: Michael Grier
     // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
-   // %          note 1: the second argument, sort_flags is not implemented
-    // -    depends on: asort
+    // %          note 1: The second argument, sort_flags is not implemented;
+    // %          note 1: also should be sorted (asort?) first according to docs
     // *     example 1: array_unique(['Kevin','Kevin','van','Zonneveld','Kevin']);
     // *     returns 1: {0: 'Kevin', 2: 'van', 3: 'Zonneveld'}
     // *     example 2: array_unique({'a': 'green', 0: 'red', 'b': 'green', 1: 'blue', 2: 'red'});
