0341548f49543d24a4f33f3261f5d844c3e12bbb
Kevin van Zonneveld <kevin@vanzonneveld.net>
2010-04-23 11:49:22 +0200

    Added a testcase to avoid regression

--- functions/strings/str_rot13.js
+++ functions/strings/str_rot13.js
@@ -8,8 +8,10 @@ function str_rot13 (str) {
     // *     returns 1: 'Xriva ina Mbaariryq'
     // *     example 2: str_rot13('Xriva ina Mbaariryq');
     // *     returns 2: 'Kevin van Zonneveld'
+    // *     example 3: str_rot13(33);
+    // *     returns 3: '33'
     
-    return str.replace(/[a-z]/gi, function(s){ 
+    return (str+'').replace(/[a-z]/gi, function(s){
         return String.fromCharCode(s.charCodeAt(0)+(s.toLowerCase()<'n'?13:-13));
     });
 }
