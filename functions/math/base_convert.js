419ceb08a367a7f2fb564ff0dc4e2cbf2bbffe96
Kevin van Zonneveld <kevin@vanzonneveld.net>
2010-04-23 11:16:55 +0200

    Improvements by Rafał Kukawski: frombase and tobase should be integers. When passing some other types, like float or a numeric string, PHP converts them to integer. The bitwise OR operator does more or less the same, converts the values to number and truncates the floating part of the number

--- functions/math/base_convert.js
+++ functions/math/base_convert.js
@@ -1,8 +1,9 @@
 function base_convert (number, frombase, tobase) {
     // http://kevin.vanzonneveld.net
     // +   original by: Philippe Baumann
+    // +   improved by: Rafał Kukawski
     // *     example 1: base_convert('A37334', 16, 2);
     // *     returns 1: '101000110111001100110100'
 
-    return parseInt(number+'', frombase+0).toString(tobase+0);
+    return parseInt(number + '', frombase | 0).toString(tobase | 0);
 }
