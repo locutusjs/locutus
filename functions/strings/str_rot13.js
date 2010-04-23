f4fcf895c16ecce83550efc9c190c82ed118c06c
Kevin van Zonneveld <kevin@vanzonneveld.net>
2010-04-23 11:29:24 +0200

    Improvements by Rafał Kukawski: tighter str_rot13 implementation

--- functions/strings/str_rot13.js
+++ functions/strings/str_rot13.js
@@ -3,12 +3,13 @@ function str_rot13 (str) {
     // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
     // +   improved by: Ates Goral (http://magnetiq.com)
     // +   bugfixed by: Onno Marsman
+    // +   improved by: Rafał Kukawski (http://blog.kukawski.pl)
     // *     example 1: str_rot13('Kevin van Zonneveld');
     // *     returns 1: 'Xriva ina Mbaariryq'
     // *     example 2: str_rot13('Xriva ina Mbaariryq');
     // *     returns 2: 'Kevin van Zonneveld'
-
-    return (str+'').replace(/[A-Za-z]/g, function (c) {
-        return String.fromCharCode((((c = c.charCodeAt(0)) & 223) - 52) % 26 + (c & 32) + 65);
+    
+    return str.replace(/[a-z]/gi, function(s){ 
+        return String.fromCharCode(s.charCodeAt(0)+(s.toLowerCase()<'n'?13:-13));
     });
 }
