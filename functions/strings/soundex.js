e4fab750fe5606450b631a392b84f1a08183e219
Kevin van Zonneveld <kevin@vanzonneveld.net>
2010-08-10 10:53:28 +0200

    Declare j. Jslinted. Preserve original authors. Removed global caching for compatibility reasons. Don't modify original str. phpjs code style changes.

--- functions/strings/soundex.js
+++ functions/strings/soundex.js
@@ -1,33 +1,38 @@
 function soundex (str) {
-	// +   original by: Arnout Kazemier (http://www.3rd-Eden.com)
+    // http://kevin.vanzonneveld.net
+    // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+    // +    tweaked by: Jack
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
+    // +      input by: Brett Zamir (http://brett-zamir.me)
+    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   original by: Arnout Kazemier (http://www.3rd-Eden.com)
     // *     example 1: soundex('Kevin');
     // *     returns 1: 'K150'
     // *     example 2: soundex('Ellery');
     // *     returns 2: 'E460'
     // *     example 3: soundex('Euler');
     // *     returns 3: 'E460'
-	str = str.toUpperCase();
-	
-	var sdx = [str[0],0,0,0],
-		m = soundex.map,
-		k = soundex.keys,
-		i = 1, s = 0, key, code,
-		l = str.length;
-	
-	for (; i < l; i++){
-		j = k.length;
-		while( s != 3 && j-- ){
-			key = k[j];
-			if (key.indexOf(str[i]) !== -1) {
-				code = m[key];
-				if ( code != sdx[s] ){
-					sdx[++s] = code;
-				}
-			}
-		}
-	}
-	
-	return sdx.join('')
-};
-soundex.map = {	BFPV: 1, CGJKQSXZ: 2, DT: 3, L: 4, MN: 5, R: 6 };
-soundex.keys = ['BFPV', 'CGJKQSXZ', 'DT', 'L', 'MN', 'R'];
+
+    var upStr = (str+'').toUpperCase();
+    var sdx = [upStr[0],0,0,0],
+        m = {BFPV: 1, CGJKQSXZ: 2, DT: 3, L: 4, MN: 5, R: 6 },
+        k = ['BFPV', 'CGJKQSXZ', 'DT', 'L', 'MN', 'R'],
+        i = 1, j = 0, s = 0, key, code,
+        l = upStr.length;
+
+    for (; i < l; i++){
+        j = k.length;
+        while (s != 3 && j--){
+            key = k[j];
+            if (key.indexOf(upStr[i]) !== -1) {
+                code = m[key];
+                if (code != sdx[s]){
+                    sdx[++s] = code;
+                }
+            }
+        }
+    }
+
+    return sdx.join('');
+}
