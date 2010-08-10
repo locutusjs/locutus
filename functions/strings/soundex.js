9434536ff5b5a77a7579fd4515c7ce1e6b6dc10f
Arnout Kazemier <info@3rd-Eden.com>
2010-08-10 16:29:40 +0800

    Updated the soundex function to a complete rewrite, which is about 80% faster that the origional build. For a performance benchmark see: http://jsperf.com/soundex/2

--- functions/strings/soundex.js
+++ functions/strings/soundex.js
@@ -1,39 +1,33 @@
 function soundex (str) {
-    // http://kevin.vanzonneveld.net
-    // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
-    // +    tweaked by: Jack
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   bugfixed by: Onno Marsman
-    // +      input by: Brett Zamir (http://brett-zamir.me)
-    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	// +   original by: Arnout Kazemier (http://www.3rd-Eden.com)
     // *     example 1: soundex('Kevin');
     // *     returns 1: 'K150'
     // *     example 2: soundex('Ellery');
     // *     returns 2: 'E460'
     // *     example 3: soundex('Euler');
     // *     returns 3: 'E460'
-
-    var s = '';
-    var i, j, l, p = isNaN(p) ? 4 : p > 10 ? 10 : p < 4 ? 4 : p;
-    var m = {
-        BFPV: 1,
-        CGJKQSXZ: 2,
-        DT: 3,
-        L: 4,
-        MN: 5, 
-        R: 6
-    };
-    var r = (s = (str+'').toUpperCase().replace(/[^A-Z]/g, '').split('')).splice(0, 1);
-    var sl = 0;
-
-    sl = s.length;
-    for (i = -1, l = sl; ++i < l;) {
-        for (j in m) {
-            if (j.indexOf(s[i]) + 1 && r[r.length-1] != m[j] && r.push(m[j])) {
-                break;
-            }
-        }
-    }
-
-    return r.length > p && (r.length = p), r.join('') + (new Array(p - r.length + 1)).join('0');
-}
+	str = str.toUpperCase();
+	
+	var sdx = [str[0],0,0,0],
+		m = soundex.map,
+		k = soundex.keys,
+		i = 1, s = 0, key, code,
+		l = str.length;
+	
+	for (; i < l; i++){
+		j = k.length;
+		while( s != 3 && j-- ){
+			key = k[j];
+			if (key.indexOf(str[i]) !== -1) {
+				code = m[key];
+				if ( code != sdx[s] ){
+					sdx[++s] = code;
+				}
+			}
+		}
+	}
+	
+	return sdx.join('')
+};
+soundex.map = {	BFPV: 1, CGJKQSXZ: 2, DT: 3, L: 4, MN: 5, R: 6 };
+soundex.keys = ['BFPV', 'CGJKQSXZ', 'DT', 'L', 'MN', 'R'];
