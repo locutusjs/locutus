1af86bfd4202907d9bba7ee83b06e9ea3ff7da47
Kevin van Zonneveld <kevin@vanzonneveld.net>
2010-09-08 17:56:16 +0200

    Only uppercase small letters. Support for words separated by more than 1 spaces

--- functions/strings/ucwords.js
+++ functions/strings/ucwords.js
@@ -1,15 +1,17 @@
-function ucwords(str) {
+function ucwords (str) {
     // http://kevin.vanzonneveld.net
     // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
     // +   improved by: Waldo Malqui Silva
     // +   bugfixed by: Onno Marsman
     // +   improved by: Robin
-    // *     example 1: ucwords('kevin van zonneveld');
-    // *     returns 1: 'Kevin Van Zonneveld'
+    // +      input by: James (http://www.james-bell.co.uk/)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // *     example 1: ucwords('kevin van  zonneveld');
+    // *     returns 1: 'Kevin Van  Zonneveld'
     // *     example 2: ucwords('HELLO WORLD');
     // *     returns 2: 'HELLO WORLD'
 
-    return (str + '').replace(/^(.)|\s(.)/g, function ($1) {
+    return (str + '').replace(/^([a-z])|\s+([a-z])/g, function ($1) {
         return $1.toUpperCase();
     });
 }
