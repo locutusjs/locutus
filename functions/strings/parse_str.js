ae81570a75df96dee9aed4e474f8e64ad9f5976f
Brett Zamir <brettz9@yahoo.com>
2010-09-14 11:47:03 +0800

    Simplify parse_str

--- functions/strings/parse_str.js
+++ functions/strings/parse_str.js
@@ -18,7 +18,7 @@ function parse_str (str, array){
     // *     example 2: parse_str('str_a=Jack+and+Jill+didn%27t+see+the+well.', arr);
     // *     results 2: arr == { str_a: "Jack and Jill didn't see the well." }
 
-    var glue1 = '=', glue2 = '&', array2 = String(str).replace(/^&(.*)$/, '$1').replace(/^(.*)&$/, '$1').split(glue2),
+    var glue1 = '=', glue2 = '&', array2 = String(str).replace(/^&?([\s\S]*?)&?$/, '$1').split(glue2),
     i, j, chr, tmp, key, value, bracket, keys, evalStr, that = this,
     fixStr = function (str) {
         return that.urldecode(str).replace(/([\\"'])/g, '\\$1').replace(/\n/g, '\\n').replace(/\r/g, '\\r');
