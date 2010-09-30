28b50f95fa846b3177859b9d8240e3e8963f7f77
rafaelk <rafal@ztcw.pl>
2010-10-01 00:26:11 +0200

    Revised the algorithm for soundex calculation

--- functions/strings/soundex.js
+++ functions/strings/soundex.js
@@ -6,8 +6,8 @@ function soundex (str) {
     // +   bugfixed by: Onno Marsman
     // +      input by: Brett Zamir (http://brett-zamir.me)
     // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   bugfixed by: Rafał Kukawski (http://blog.kukawski.pl)
     // +   original by: Arnout Kazemier (http://www.3rd-Eden.com)
+    // +    revised by: Rafał Kukawski (http://blog.kukawski.pl)
     // *     example 1: soundex('Kevin');
     // *     returns 1: 'K150'
     // *     example 2: soundex('Ellery');
@@ -16,21 +16,25 @@ function soundex (str) {
     // *     returns 3: 'E460'
 
     str = (str + '').toUpperCase();
-    var sdx = [str.charAt(0), 0, 0 , 0],
-        k = ['BFPV', 'CGJKQSXZ', 'DT', 'L', 'MN', 'R'], kl = k.length,
-        i = 1, j = 0, s = 0, c, p;
+    if (!str) {
+        return '';
+    }
+    var sdx = [0, 0, 0, 0],
+        m = {B: 1, F: 1, P: 1, V: 1, C: 2, G: 2, J: 2, K: 2, Q: 2, S: 2, X: 2, Z: 2, D: 3, T: 3, L: 4, M: 5, N: 5, R: 6},
+        i = 0, j, s = 0, c, p;
 
-    while ((c = str.charAt(i++)) && s < 3 ){
-	j = 0;
-        while (p = k[j++]){
-            if (p.indexOf(c) !== -1) {
-                if (j !== sdx[s]){
-                    sdx[++s] = j;
-                }
-                break;
+    while ((c = str.charAt(i++)) && s < 4) {
+        if (j = m[c]) {
+            if (j !== p) {
+                sdx[s++] = p = j;
             }
+        } else {
+            s += i === 1;
+            p = 0;
         }
     }
+    
+    sdx[0] = str.charAt(0);
     return sdx.join('');
 }
 
