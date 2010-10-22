6b0a317fc03a07787f3c0e3c3295b0182d77d5af
Brett Zamir <brettz9@yahoo.com>
2010-10-23 03:59:18 +0800

    Fixing this to that reference in http_build_query()

--- functions/url/http_build_query.js
+++ functions/url/http_build_query.js
@@ -6,13 +6,15 @@ function http_build_query (formdata, numeric_prefix, arg_separator) {
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +   improved by: Brett Zamir (http://brett-zamir.me)
     // +    revised by: stag019
+    // +   input by: Dreamer
+    // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
     // -    depends on: urlencode
     // *     example 1: http_build_query({foo: 'bar', php: 'hypertext processor', baz: 'boom', cow: 'milk'}, '', '&amp;');
     // *     returns 1: 'foo=bar&amp;php=hypertext+processor&amp;baz=boom&amp;cow=milk'
     // *     example 2: http_build_query({'php': 'hypertext processor', 0: 'foo', 1: 'bar', 2: 'baz', 3: 'boom', 'cow': 'milk'}, 'myvar_');
     // *     returns 2: 'php=hypertext+processor&myvar_0=foo&myvar_1=bar&myvar_2=baz&myvar_3=boom&cow=milk'
 
-    var value, key, tmp = [];
+    var value, key, tmp = [], that = this;
 
     var _http_build_query_helper = function (key, val, arg_separator) {
         var k, tmp = [];
@@ -29,7 +31,7 @@ function http_build_query (formdata, numeric_prefix, arg_separator) {
             }
             return tmp.join(arg_separator);
         } else if (typeof(val) !== "function") {
-            return this.urlencode(key) + "=" + this.urlencode(val);
+            return that.urlencode(key) + "=" + that.urlencode(val);
         } else {
             throw new Error('There was an error processing for http_build_query().');
         }
