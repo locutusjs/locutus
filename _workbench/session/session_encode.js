31aa231a1514fc864f5abad03d47d1606f96058f
Brett Zamir <brettz9@yahoo.com>
2010-12-07 11:03:29 +0800

    More clean-up (still needs work to finish reconstruction)

--- _workbench/session/session_encode.js
+++ _workbench/session/session_encode.js
@@ -2,12 +2,22 @@ function session_encode () {
     // http://kevin.vanzonneveld.net
     // +   original by: Louis Stowasser
     // -    depends on: urldecode
-    // -    depends on: getcookie
     // *     example 1: 
     // *     returns 1: 
+
+    var _getcookie = function (name) {
+        var cookies = document.cookie.split(';'),i=0,l=cookies.length,
+            current;
+        for(;i<l;i++) {
+            current = cookies[i].split('=');
+    //            current[0] = current[0].replace(/\s+/,"");
+            if(current[0] === name) {return current[1];}
+        }
+        return undefined;
+    };
 
     /**
     * Encode string in session format (serialized then url encoded)
     */
-    return this.urldecode(this.getcookie('JSSESSID'));
+    return this.urldecode(_getcookie('JSSESSID'));
 }
--- _workbench/session/session_start.js
+++ _workbench/session/session_start.js
@@ -5,6 +5,16 @@ function session_start() {
     // *     example 1: 
     // *     returns 1: 
 
+    var _getcookie = function (name) {
+        var cookies = document.cookie.split(';'),i=0,l=cookies.length,
+            current;
+        for(;i<l;i++) {
+            current = cookies[i].split('=');
+    //            current[0] = current[0].replace(/\s+/,"");
+            if(current[0] === name) {return current[1];}
+        }
+        return undefined;
+    };
 	/**
 	* Check for a PHPSESSID. If found unpack it from the cookie
 	* If not found, create it then pack everything in $_SESSION 
@@ -14,7 +24,7 @@ function session_start() {
 		$_SESSION = {};
 		this.setcookie('JSSESSID', this.serialize($_SESSION));
 	} else {
-		$_SESSION = this.unserialize(this.urldecode(this.getcookie('JSSESSID')));
+		$_SESSION = this.unserialize(this.urldecode(_getcookie('JSSESSID')));
 	}
 }
 
@@ -31,13 +41,22 @@ function session_start () {
     // -    depends on: unserialize
     // -    depends on: serialize
     // -    depends on: urlencode
-    // -    depends on: getcookie
     // *     example 1: 
     // *     returns 1: 
 
     //* Bundle all session destroying functions (they all do the same thing)
     //* Resets the global $_SESSION and sets the cookie to null
     var t = this;
+    var _getcookie = function (name) {
+        var cookies = document.cookie.split(';'),i=0,l=cookies.length,
+            current;
+        for(;i<l;i++) {
+            current = cookies[i].split('=');
+    //            current[0] = current[0].replace(/\s+/,"");
+            if(current[0] === name) {return current[1];}
+        }
+        return undefined;
+    };
     var session_set_cookie = function (name, value, expires, path, domain, secure) {
         if (expires) {
             expires = (new Date((new Date).getTime() + expires * 3600)).toGMTString();
@@ -54,12 +73,14 @@ function session_start () {
         return secure && r.push('secure'), document.cookie = r.join(";"), true;
     };
 
-    var sid = 'JSSESSID', this.php_js = this.php_js || {}, pj = this.php_js;
-    var cookie = this.getcookie(sid);
+    var sid = 'JSSESSID';
+    this.php_js = this.php_js || {};
+    var pj = this.php_js;
+    var cookie = _getcookie(sid);
     if(!cookie || cookie == "null") {
         t.$_SESSION = {};
         return session_set_cookie(sid, t.serialize(t.$_SESSION), pj.lifetime, pj.path, pj.domain, pj.secure);
     }
-    t.$_SESSION = t.unserialize(t.urldecode(t.getcookie(sid)));
+    t.$_SESSION = t.unserialize(t.urldecode(_getcookie(sid)));
     return true;
 }
