cd5bd1c2ce0113cf0ead94e81e855b6e5333b3ff
Brett Zamir <brettz9@yahoo.com>
2011-01-24 11:43:56 +0800

    Remove dead code on ord (thanks incidence!); simplify ord/chr

--- functions/strings/chr.js
+++ functions/strings/chr.js
@@ -14,7 +14,5 @@ function chr (codePt) {
         codePt -= 0x10000;
         return String.fromCharCode(0xD800 + (codePt >> 10), 0xDC00 + (codePt & 0x3FF));
     }
-    else {
-        return String.fromCharCode(codePt);
-    }
+    return String.fromCharCode(codePt);
 }
