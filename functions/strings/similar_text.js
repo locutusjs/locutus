4e944f4b9001bd478bc0e259ea454aea5bcd9db8
Brett Zamir <brettz9@yahoo.com>
2011-01-31 18:09:00 +0800

    Bugfixed with similar_text (thanks Chris McMacken!); minor coding standard changes

--- functions/strings/similar_text.js
+++ functions/strings/similar_text.js
@@ -1,12 +1,13 @@
 function similar_text (first, second) {
     // http://kevin.vanzonneveld.net
     // +   original by: Rafał Kukawski (http://blog.kukawski.pl)
+    // +   bugfixed by: Chris McMacken
     // *     example 1: similar_text('Hello World!', 'Hello phpjs!');
     // *     returns 1: 7
     // *     example 2: similar_text('Hello World!', null);
     // *     returns 2: 0
     
-    if (first === null || second === null || typeof first === "undefined" || typeof second === "undefined") {
+    if (first === null || second === null || typeof first === 'undefined' || typeof second === 'undefined') {
         return 0;
     }
     
@@ -33,14 +34,13 @@ function similar_text (first, second) {
     
     if (sum) {
         if (pos1 && pos2) {
-            sum += similar_text(first.substr(0, pos2), second.substr(0, pos2));
+            sum += this.similar_text(first.substr(0, pos2), second.substr(0, pos2));
         }
         
         if ((pos1 + max < firstLength) && (pos2 + max < secondLength)) {
-            sum += similar_text(first.substr(pos1 + max, firstLength - pos1 - max), second.substr(pos2 + max, secondLength - pos2 - max));
+            sum += this.similar_text(first.substr(pos1 + max, firstLength - pos1 - max), second.substr(pos2 + max, secondLength - pos2 - max));
         }
     }
     
     return sum;
 }
-
