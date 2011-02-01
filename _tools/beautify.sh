a227cc16d48614055995b2121fd1640cf481c6c8
Kevin van Zonneveld <kevin@vanzonneveld.net>
2011-02-01 12:20:54 +0100

    Bugfixes in beautifier

--- _tools/beautify.sh
+++ _tools/beautify.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 Root="$(dirname "$(dirname "${0}")")/functions"
 Tool="$(dirname "$(dirname "${0}")")/ext/js-beautify/bin/beautify_js -i 4 -a -n -p"
-find ${Root} -type f -mmin +5 -name '*.js' |sort | while read File; do
-    $Tool > "${File}.beautified" && mv -f "${File}.beautified" "${File}"
+
+for File in $(find ${Root} -type f -mmin +5 -name '*.js'); do
+    echo "Beautifying ${File}"
+    $Tool "${File}"  > "${File}.beautified" && mv -f "${File}.beautified" "${File}"
 done
