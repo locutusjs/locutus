8a30679207132715f847289303cba22ad9555110
Kevin van Zonneveld <kevin@vanzonneveld.net>
2011-02-01 12:22:20 +0100

    Kill -p switch

--- _tools/beautify.sh
+++ _tools/beautify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Root="$(dirname "$(dirname "${0}")")/functions"
-Tool="$(dirname "$(dirname "${0}")")/ext/js-beautify/bin/beautify_js -i 4 -a -n -p"
+Tool="$(dirname "$(dirname "${0}")")/ext/js-beautify/bin/beautify_js -i 4 -a -n"
 
 for File in $(find ${Root} -type f -mmin +5 -name '*.js'); do
     echo "Beautifying ${File}"
