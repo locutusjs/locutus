6a53406093a911c32bce0d21d7c341a867968644
Kevin van Zonneveld <kevin@vanzonneveld.net>
2011-02-01 12:24:53 +0100

    Limit for testing

--- _tools/beautify.sh
+++ _tools/beautify.sh
@@ -2,7 +2,7 @@
 Root="$(dirname "$(dirname "${0}")")/functions"
 Tool="$(dirname "$(dirname "${0}")")/ext/js-beautify/bin/beautify_js -i 4 -a -n"
 
-for File in $(find ${Root} -type f -mmin +5 -name '*.js'); do
+for File in $(find ${Root} -type f -mmin +5 -name '*.js' |sort |head -n20); do
     echo "Beautifying ${File}"
     $Tool "${File}"  > "${File}.beautified" && mv -f "${File}.beautified" "${File}"
 done
