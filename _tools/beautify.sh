db29386f1d6d3ae7011aa6043f9bcd3e6ab57bd2
Kevin van Zonneveld <kevin@vanzonneveld.net>
2011-02-01 13:21:18 +0100

    Better regex

--- _tools/beautify.sh
+++ _tools/beautify.sh
@@ -5,7 +5,7 @@ Tool="$(dirname "$(dirname "${0}")")/ext/js-beautify/bin/beautify_js -i 4 -a -n 
 for File in $(find ${Root} -type f -name '*.js' |sort |head -n20); do
     echo "Beautifying ${File}"
     $Tool "${File}" |sed \
-        -e 's#[\t ]*function[\t ]*\([a-z_0-9]\+\)[\t ]*#function \1 #' \
+        -e 's#^[\t ]*function[\t ]*\([a-z_0-9]\+\)[\t ]*#function \1 #' \
         > "${File}.beautified" \
     && mv -f "${File}.beautified" "${File}"
 done
