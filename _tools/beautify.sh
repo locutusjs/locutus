980671ed820079956b31d950a465011420f9a0fb
Kevin van Zonneveld <kevin@vanzonneveld.net>
2011-02-01 12:32:13 +0100

    Also take recent files, doesn't matter

--- _tools/beautify.sh
+++ _tools/beautify.sh
@@ -2,7 +2,7 @@
 Root="$(dirname "$(dirname "${0}")")/functions"
 Tool="$(dirname "$(dirname "${0}")")/ext/js-beautify/bin/beautify_js -i 4 -a -n -p"
 
-for File in $(find ${Root} -type f -mmin +5 -name '*.js' |sort |head -n20); do
+for File in $(find ${Root} -type f -name '*.js' |sort |head -n20); do
     echo "Beautifying ${File}"
     $Tool "${File}"  > "${File}.beautified" && mv -f "${File}.beautified" "${File}"
 done
