1854ef8660c6fd86e9f85cd7f904ab27da1fdd76
Kevin van Zonneveld <kevin@vanzonneveld.net>
2011-02-16 11:10:57 +0100

    Fixed count bug thanks to mgcrea

--- functions/array/count.js
+++ functions/array/count.js
@@ -2,16 +2,17 @@ function count (mixed_var, mode) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +      input by: Waldo Malqui Silva
-    // +      bugfixed by: Soren Hansen
+    // +   bugfixed by: Soren Hansen
     // +      input by: merabi
-    // +      improved by: Brett Zamir (http://brett-zamir.me)
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
+    // +   bugfixed by: Olivier Louvignes (http://mg-crea.com/)
     // *     example 1: count([[0,0],[0,-4]], 'COUNT_RECURSIVE');
     // *     returns 1: 6
     // *     example 2: count({'one' : [1,2,3,4,5]}, 'COUNT_RECURSIVE');
     // *     returns 2: 6
     var key, cnt = 0;
 
-    if (mixed_var === null) {
+    if (mixed_var === null || typeof mixed_var === 'undefined') {
         return 0;
     } else if (mixed_var.constructor !== Array && mixed_var.constructor !== Object) {
         return 1;
