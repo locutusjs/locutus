47def44a7a108632603d84aed490265b67fd48db
Brett Zamir <brettz9@yahoo.com>
2011-04-12 19:36:36 +0800

    Credits

--- functions/array/array_chunk.js
+++ functions/array/array_chunk.js
@@ -1,6 +1,7 @@
 function array_chunk (input, size, preserve_keys) {
     // http://kevin.vanzonneveld.net
     // +   original by: Carlos R. L. Rodrigues (http://www.jsfromhell.com)
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
     // *     example 1: array_chunk(['Kevin', 'van', 'Zonneveld'], 2);
     // *     returns 1: [['Kevin', 'van'], ['Zonneveld']]
     // *     example 2: array_chunk(['Kevin', 'van', 'Zonneveld'], 2, true);
