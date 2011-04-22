958bbe9342f0e624881ea2b6dd36da929debea0a
unknown <Brett@.(none)>
2011-04-22 15:55:31 +0800

    Simplify slightly

--- functions/datetime/gmdate.js
+++ functions/datetime/gmdate.js
@@ -6,10 +6,9 @@ function gmdate (format, timestamp) {
     // -    depends on: date
     // *     example 1: gmdate('H:m:s \\m \\i\\s \\m\\o\\n\\t\\h', 1062402400); // Return will depend on your timezone
     // *     returns 1: '07:09:40 m is month'
-    var dt = ((typeof(timestamp) == 'undefined') ? new Date() : // Not provided
-    (typeof(timestamp) == 'object') ? new Date(timestamp) : // Javascript Date()
-    new Date(timestamp * 1000) // UNIX timestamp (auto-convert to int)
-    );
+    var dt = typeof timestamp === 'undefined' ? new Date() : // Not provided
+			typeof timestamp === 'object' ? new Date(timestamp) : // Javascript Date()
+			new Date(timestamp * 1000); // UNIX timestamp (auto-convert to int)
     timestamp = Date.parse(dt.toUTCString().slice(0, -4)) / 1000;
     return this.date(format, timestamp);
 }
