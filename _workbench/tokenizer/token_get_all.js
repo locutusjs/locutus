463c6ed0ea55d884dc682abdd8918e14b7a64174
unknown <Brett@.(none)>
2011-07-25 13:14:55 +0800

    Change to UTF-8 encoding

--- _workbench/tokenizer/token_get_all.js
+++ _workbench/tokenizer/token_get_all.js
@@ -1,6 +1,6 @@
 function token_get_all(source) {
     // Split given source into PHP tokens
-    // +      original by: Marco Marchiò
+    // +      original by: Marco MarchiÃ²
     // +      improved by: Brett Zamir (http://brett-zamir.me)
     // -    depends on: token_name
     // %        note 1: Token numbers depend on the PHP version
