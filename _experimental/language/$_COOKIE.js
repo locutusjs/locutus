cd3af966b6d63bf0075a1809917f70a7a1caf7a4
unknown <Brett@.(none)>
2011-10-05 22:00:43 -0700

    Change tabs to spaces

--- _experimental/language/$_COOKIE.js
+++ _experimental/language/$_COOKIE.js
@@ -7,14 +7,14 @@ function $_COOKIE (name) {
     // *     example 1: var $myVar = $_COOKIE('test'); // Note the round brackets!
     // *     returns 1: 'hello'
     
-	var i = 0, c = '', nameEQ = name + '=',
+    var i = 0, c = '', nameEQ = name + '=',
         ca = document.cookie.split(';'),
         cal = ca.length;
-	for (i = 0; i < cal; i++) {
-		c = ca[i].replace(/^ */, '');
-		if (c.indexOf(nameEQ) === 0) {
+    for (i = 0; i < cal; i++) {
+        c = ca[i].replace(/^ */, '');
+        if (c.indexOf(nameEQ) === 0) {
             return decodeURIComponent(c.slice(nameEQ.length).replace(/\+/g, '%20'));
         }
-	}
-	return null;
+    }
+    return null;
 }
