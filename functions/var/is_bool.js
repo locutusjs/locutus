5180394705d989a2a1b1fdc9bf6ba39afea4db64
Brett Zamir <brettz9@yahoo.com>
2012-07-04 23:09:14 +0800

    Applied optimization of is_bool (Thanks CoursesWeb!)

--- functions/var/is_bool.js
+++ functions/var/is_bool.js
@@ -1,9 +1,10 @@
 function is_bool (mixed_var) {
     // http://kevin.vanzonneveld.net
     // +   original by: Onno Marsman
+    // +   improved by: CoursesWeb (http://www.coursesweb.net/)
     // *     example 1: is_bool(false);
     // *     returns 1: true
     // *     example 2: is_bool(0);
     // *     returns 2: false
-    return (typeof mixed_var === 'boolean');
+    return (obj === true || obj === false); // Faster (in FF) than type checking
 }
