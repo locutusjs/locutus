86690b1cd6850b0a32e556f856663944910c89e7
oscar-broman <oscar.broman@gmail.com>
2012-08-23 09:42:43 +0300

    make long2ip work on all systems
    also, it now supports signed integers (like the PHP function does)

--- functions/network/long2ip.js
+++ functions/network/long2ip.js
@@ -1,13 +1,10 @@
-function long2ip (proper_address) {
+function long2ip (ip) {
     // http://kevin.vanzonneveld.net
     // +   original by: Waldo Malqui Silva
     // *     example 1: long2ip( 3221234342 );
     // *     returns 1: '192.0.34.166'
-    var output = false;
-
-    if (!isNaN(proper_address) && (proper_address >= 0 || proper_address <= 4294967295)) {
-        output = Math.floor(proper_address / Math.pow(256, 3)) + '.' + Math.floor((proper_address % Math.pow(256, 3)) / Math.pow(256, 2)) + '.' + Math.floor(((proper_address % Math.pow(256, 3)) % Math.pow(256, 2)) / Math.pow(256, 1)) + '.' + Math.floor((((proper_address % Math.pow(256, 3)) % Math.pow(256, 2)) % Math.pow(256, 1)) / Math.pow(256, 0));
-    }
-
-    return output;
+    if (!isFinite(ip))
+    	return false;
+	
+	return [ip >>> 24, ip >>> 16 & 0xFF, ip >>> 8 & 0xFF, ip & 0xFF].join('.');
 }
