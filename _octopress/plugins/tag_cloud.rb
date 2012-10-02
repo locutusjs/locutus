dce2586e404666ca7bafdd1560665ed71b3c305d
Kevin van Zonneveld <kevin@vanzonneveld.net>
2012-10-02 03:26:23 +0200

    switch to original tagcloud generator

--- _octopress/plugins/tag_cloud.rb
+++ _octopress/plugins/tag_cloud.rb
@@ -1,5 +1,6 @@
-# Tag Cloud for Octopress, modified by pf_miles, for use with utf-8 encoded blogs(all regexp added 'u' option).
-# modified by alswl, tag_cloud -> category_cloud
+# encoding: utf-8
+
+# Tag Cloud for Octopress
 # =======================
 # 
 # Description:
@@ -40,16 +41,18 @@
 # Distributed under the [MIT License][MIT].
 # 
 # [MIT]: http://www.opensource.org/licenses/mit-license.php
+#
+# https://github.com/tokkonopapa/octopress-tagcloud
 # 
 module Jekyll
 
-  class CategoryCloud < Liquid::Tag
+  class TagCloud < Liquid::Tag
 
     def initialize(tag_name, markup, tokens)
       @opts = {}
-      if markup.strip =~ /\s*counter:(\w+)/iu
+      if markup.strip =~ /\s*counter:(\w+)/i
         @opts['counter'] = ($1 == 'true')
-        markup = markup.strip.sub(/counter:\w+/iu,'')
+        markup = markup.strip.sub(/counter:\w+/i,'')
       end
       super
     end
@@ -68,7 +71,7 @@ module Jekyll
 
       html = ''
       lists.each do | category, counter |
-        url = category_dir + category.gsub(/_|\P{Word}/u, '-').gsub(/-{2,}/u, '-').downcase
+        url = category_dir + category.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase
         style = "font-size: #{100 + (60 * Float(counter)/max)}%"
         html << "<a href='#{url}' style='#{style}'>#{category}"
         if @opts['counter']
@@ -84,9 +87,9 @@ module Jekyll
 
     def initialize(tag_name, markup, tokens)
       @opts = {}
-      if markup.strip =~ /\s*counter:(\w+)/iu
+      if markup.strip =~ /\s*counter:(\w+)/i
         @opts['counter'] = ($1 == 'true')
-        markup = markup.strip.sub(/counter:\w+/iu,'')
+        markup = markup.strip.sub(/counter:\w+/i,'')
       end
       super
     end
@@ -97,7 +100,7 @@ module Jekyll
       category_dir = config['root'] + config['category_dir'] + '/'
       categories = context.registers[:site].categories
       categories.keys.sort_by{ |str| str.downcase }.each do |category|
-        url = category_dir + category.gsub(/_|\P{Word}/u, '-').gsub(/-{2,}/u, '-').downcase
+        url = category_dir + category.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase
         html << "<li><a href='#{url}'>#{category}"
         if @opts['counter']
           html << " (#{categories[category].count})"
@@ -110,5 +113,5 @@ module Jekyll
 
 end
 
-Liquid::Template.register_tag('category_cloud', Jekyll::CategoryCloud)
+Liquid::Template.register_tag('tag_cloud', Jekyll::TagCloud)
 Liquid::Template.register_tag('category_list', Jekyll::CategoryList)
