9f948c99ae1b468cbbd284663f94ade5308fd277
Kevin van Zonneveld <kevin@vanzonneveld.net>
2013-04-25 00:10:00 +0200

    Cleanup

--- examples/php.example.js
+++
@@ -1,7 +0,0 @@
-// Include the php.js file.
-
-// Call a PHPJS function
-p = strpos('Kevin van Zonneveld', 'e');
-
-// Call another PHPJS function
-d = date('F j, Y, g:i a');
--- examples/php.namespaced.example.js
+++
@@ -1,7 +0,0 @@
-// Include the php.namespaced.js file.
-
-// Call a PHPJS function
-p = $P.strpos('Kevin van Zonneveld', 'e');
-
-// Call another PHPJS function
-d = $P.date('F j, Y, g:i a');
