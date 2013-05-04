d22da5d9c03c3e01236cbe14a53d8740684ee227
mlataza <mlataza@gmail.com>
2013-05-04 22:20:27 +0800

    Update json_encode.js

--- functions/json/json_encode.js
+++ functions/json/json_encode.js
@@ -1,152 +1,410 @@
-function json_encode (mixed_val) {
-  // http://kevin.vanzonneveld.net
-  // +      original by: Public Domain (http://www.json.org/json2.js)
-  // + reimplemented by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // +      improved by: Michael White
-  // +      input by: felix
-  // +      bugfixed by: Brett Zamir (http://brett-zamir.me)
-  // *        example 1: json_encode(['e', {pluribus: 'unum'}]);
-  // *        returns 1: '[\n    "e",\n    {\n    "pluribus": "unum"\n}\n]'
-/*
-    http://www.JSON.org/json2.js
-    2008-11-19
-    Public Domain.
-    NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
-    See http://www.JSON.org/js.html
-  */
-  var retVal, json = this.window.JSON;
-  try {
-    if (typeof json === 'object' && typeof json.stringify === 'function') {
-      retVal = json.stringify(mixed_val); // Errors will not be caught here if our own equivalent to resource
-      //  (an instance of PHPJS_Resource) is used
-      if (retVal === undefined) {
-        throw new SyntaxError('json_encode');
-      }
-      return retVal;
-    }
+var json = ( function ( ) {
 
-    var value = mixed_val;
-
-    var quote = function (string) {
-      var escapable = /[\\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
-      var meta = { // table of character substitutions
-        '\b': '\\b',
-        '\t': '\\t',
-        '\n': '\\n',
-        '\f': '\\f',
-        '\r': '\\r',
-        '"': '\\"',
-        '\\': '\\\\'
-      };
-
-      escapable.lastIndex = 0;
-      return escapable.test(string) ? '"' + string.replace(escapable, function (a) {
-        var c = meta[a];
-        return typeof c === 'string' ? c : '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
-      }) + '"' : '"' + string + '"';
-    };
-
-    var str = function (key, holder) {
-      var gap = '';
-      var indent = '    ';
-      var i = 0; // The loop counter.
-      var k = ''; // The member key.
-      var v = ''; // The member value.
-      var length = 0;
-      var mind = gap;
-      var partial = [];
-      var value = holder[key];
-
-      // If the value has a toJSON method, call it to obtain a replacement value.
-      if (value && typeof value === 'object' && typeof value.toJSON === 'function') {
-        value = value.toJSON(key);
-      }
-
-      // What happens next depends on the value's type.
-      switch (typeof value) {
-      case 'string':
-        return quote(value);
-
-      case 'number':
-        // JSON numbers must be finite. Encode non-finite numbers as null.
-        return isFinite(value) ? String(value) : 'null';
-
-      case 'boolean':
-      case 'null':
-        // If the value is a boolean or null, convert it to a string. Note:
-        // typeof null does not produce 'null'. The case is included here in
-        // the remote chance that this gets fixed someday.
-        return String(value);
-
-      case 'object':
-        // If the type is 'object', we might be dealing with an object or an array or
-        // null.
-        // Due to a specification blunder in ECMAScript, typeof null is 'object',
-        // so watch out for that case.
-        if (!value) {
-          return 'null';
-        }
-        if ((this.PHPJS_Resource && value instanceof this.PHPJS_Resource) || (window.PHPJS_Resource && value instanceof window.PHPJS_Resource)) {
-          throw new SyntaxError('json_encode');
-        }
-
-        // Make an array to hold the partial results of stringifying this object value.
-        gap += indent;
-        partial = [];
-
-        // Is the value an array?
-        if (Object.prototype.toString.apply(value) === '[object Array]') {
-          // The value is an array. Stringify every element. Use null as a placeholder
-          // for non-JSON values.
-          length = value.length;
-          for (i = 0; i < length; i += 1) {
-            partial[i] = str(i, value) || 'null';
-          }
-
-          // Join all of the elements together, separated with commas, and wrap them in
-          // brackets.
-          v = partial.length === 0 ? '[]' : gap ? '[\n' + gap + partial.join(',\n' + gap) + '\n' + mind + ']' : '[' + partial.join(',') + ']';
-          gap = mind;
-          return v;
-        }
-
-        // Iterate through all of the keys in the object.
-        for (k in value) {
-          if (Object.hasOwnProperty.call(value, k)) {
-            v = str(k, value);
-            if (v) {
-              partial.push(quote(k) + (gap ? ': ' : ':') + v);
-            }
-          }
-        }
-
-        // Join all of the member texts together, separated with commas,
-        // and wrap them in braces.
-        v = partial.length === 0 ? '{}' : gap ? '{\n' + gap + partial.join(',\n' + gap) + '\n' + mind + '}' : '{' + partial.join(',') + '}';
-        gap = mind;
-        return v;
-      case 'undefined':
-        // Fall-through
-      case 'function':
-        // Fall-through
-      default:
-        throw new SyntaxError('json_encode');
-      }
-    };
-
-    // Make a fake root object containing our value under the key of ''.
-    // Return the result of stringifying the value.
-    return str('', {
-      '': value
-    });
-
-  } catch (err) { // Todo: ensure error handling above throws a SyntaxError in all cases where it could
-    // (i.e., when the JSON global is not available and there is an error)
-    if (!(err instanceof SyntaxError)) {
-      throw new Error('Unexpected error type in json_encode()');
-    }
-    this.php_js = this.php_js || {};
-    this.php_js.last_error_json = 4; // usable by json_last_error()
-    return null;
-  }
+if ( ! String.prototype.trim ) {
+  String.prototype.trim = function ( ) {
+		return this.replace ( /^\s+|\s+$/g , '' ) ;
+  } ;
 }
+
+function toString ( object ) {
+	return Object.prototype.toString.call ( object ) ;
+}
+
+function syn_error ( msg , i ) {
+	if ( SyntaxError ) {
+		throw new SyntaxError ( msg + ' at char ' + i ) ;
+	}
+	else {
+		throw 'SyntaxError: ' + msg + ' at char ' + i ;
+	}
+}
+
+function decode ( code ) {
+	var code = code.trim ( ) ;
+	
+	//core variables
+	var output = null ;
+	var buffer = null ;
+	var parent = null ;
+	var path = [ ] ;
+	var i = 0 ;
+	var chr ;
+	var key = null ;
+	
+	//temporary syntax buffer
+	var sbuffer = '' ;
+	
+	//string parser
+	var str_esc = false ;
+	var unicode = false ;
+	var ubuff ;
+	var hexa_char = /^[a-f0-9]$/i ;
+	
+	//number parser
+	var number = /^-?[0-9]+(\.[0-9]+)?([eE][+-]?[0-9]+)?$/ ;	
+	
+	//object parser
+	var write_key = false ;
+	
+	//tokens
+	var expected ;
+	var val = expected = /[\{\[tfn"0-9\-]/ ;
+	
+	function is_object ( ) {
+		var temp = output ;
+		for ( var i = 0 ; i < path.length ; i ++ ) {
+			temp = temp [ path [ i ] ] 
+		}
+		return toString ( temp ) == '[object Object]' ;
+	}
+	
+	function is_array ( ) {
+		var temp = output ;
+		for ( var i = 0 ; i < path.length ; i ++ ) {
+			temp = temp [ path [ i ] ] 
+		}
+		return toString ( temp ) == '[object Array]' ;
+	}
+		
+	function flush ( ) {
+		function input ( loc , val ) {
+			if ( loc.length == 1 ) {
+				this [ loc [ 0 ] ] = val ;
+				return ;
+			}
+			if ( ! this [ loc [ 0 ] ] ) {
+				if ( toString ( loc [ 1 ] ) === '[object String]' ) {
+					this [ loc [ 0 ] ] = { } ;
+				}
+				else {
+					this [ loc [ 0 ] ] = [ ] ;
+				}
+			}
+			input.call ( this [ loc [ 0 ] ] , loc.slice ( 1 ) , val ) ;
+		}
+		function get ( loc ) {
+			if ( loc.length == 0 ) {
+				return ;
+			}
+			if ( loc.length == 1 ) {
+				return this [ loc [ 0 ] ] ;
+			}
+			return get.call ( this [ loc [ 0 ] ] , loc.slice ( 1 ) ) ;
+		}
+		if ( path.length == 0 && key == null ) {
+			if ( output == null ) {
+				output = buffer ;
+			}
+		}
+		else {
+			path.push ( key ) ;
+			input.call ( output , path , buffer ) ;
+			path.pop ( ) ;
+		}
+	}
+	
+	for ( ; i < code.length ; i ++ ) {
+		chr = code [ i ] ;
+		if ( parent == 'string' ) {
+			if ( str_esc ) {	
+				switch ( chr ) {
+				case 'b' :
+					sbuffer += '\b' ;
+					break ;
+				case 'f' :
+					sbuffer += '\f' ;
+					break ;
+				case 'n' :
+					sbuffer += '\n' ;
+					break ;
+				case 'r' :
+					sbuffer += '\r' ;
+					break ;
+				case 't' :
+					sbuffer += '\t' ;
+					break ;
+				case 'u' :
+					unicode = 0 ;
+					ubuff = '' ;
+					break ;
+				default :
+					sbuffer += chr ;
+				}
+				str_esc = false ;
+			}
+			else {
+				if ( unicode !== false ) {
+					if ( hexa_char.test ( chr ) ) {
+						unicode ++ ;
+						ubuff += chr ;
+					}
+					else {
+						syn_error ( 'Invalid hexadecimal character' , i ) ;
+					}
+					if ( unicode == 4 ) {
+						unicode = false ;
+						sbuffer += String.fromCharCode ( parseInt ( ubuff , 16 ) ) ;
+					}
+				}
+				else {
+					if ( chr == '\\' ) {
+						str_esc = true ;
+						
+					}
+					else if ( chr == '"' ) {
+						buffer = sbuffer ;
+						parent = null ;
+						if ( write_key ) {
+							key = buffer ;
+							expected = /[:]/ ;
+						}
+						else {
+							flush ( ) ;
+							if ( is_object ( ) ) {
+								expected = /[,\}]/ ;
+							}
+							else if ( is_array ( ) ) {
+								expected = /[,\]]/ ;
+							}
+							else {
+								expected = /[]/ ;
+							}
+						}
+					}
+					else {
+						sbuffer += chr ;
+					}
+				}
+			}
+		}
+		else if ( parent == 'number' ) {
+			if ( number.test ( sbuffer + chr + '0' ) ) {
+				sbuffer += chr ;
+			}
+			else {
+				if ( ! number.test ( sbuffer ) ) {
+					syn_error ( 'Unexpected token ' + chr , i ) ;
+				}
+				buffer = parseFloat ( sbuffer ) ;
+				flush ( ) ;
+				i -- ;
+				parent = null ;
+				//expect value separator
+				if ( is_object ( ) ) {
+					expected = /[,\}]/ ;
+				}
+				else if ( is_array ( ) ) {
+					expected = /[,\]]/ ;
+				}
+				else {
+					expected = /[]/ ;
+				}
+				continue ;
+			}
+		}
+		else if ( parent == 'special' ) {
+			sbuffer += chr ;
+			if ( sbuffer.length == 4 ) {
+				if ( sbuffer == 'true' || sbuffer == 'null' ) {
+					buffer = eval ( sbuffer ) ;
+					flush ( ) ;
+					parent = null ;
+					//expect value separator
+					if ( is_object ( ) ) {
+						expected = /[,\}]/ ;
+					}
+					else if ( is_array ( ) ) {
+						expected = /[,\]]/ ;
+					}
+					else {
+						expected = /[]/ ;
+					}
+				}
+				else if ( sbuffer == 'fals' && code [ i + 1 ] == 'e' ) {
+					buffer = false ;
+					flush ( ) ;
+					parent = null ;
+					i ++ ;
+					//expect value separator
+					if ( is_object ( ) ) {
+						expected = /[,\}]/ ;
+					}
+					else if ( is_array ( ) ) {
+						expected = /[,\]]/ ;
+					}
+					else {
+						expected = /[]/ ;
+					}
+				}
+				else {
+					syn_error ( 'Unidentified identifier ' + sbuffer , i ) ;
+				}
+			}
+		}
+		else {
+			sbuffer = '' ;
+			//skip whitespaces
+			if ( chr == ' ' ) {
+				continue ;
+			}
+			//check token if expected
+			if ( ! expected.test ( chr ) ) {
+				syn_error ( 'Unexpected token ' + chr , i ) ;
+			}
+			switch ( chr ) {
+			case '{' :
+				buffer = { } ;
+				flush ( ) ;
+				if ( key != null ) {
+					path.push ( key ) ;
+				}
+				key = null ;
+				write_key = true ;
+				expected = /["\}]/ ;
+				break ;
+			case '}' :
+				path.pop ( ) ;
+				write_key = false ;
+				key = null ;
+				if ( is_object ( ) ) {
+					expected = /[\},]/ ;
+				}
+				else if ( is_array ( ) ) {
+					expected = /[\],]/ ;
+				}
+				else {
+					expected = /[]/ ;
+				}
+				break ;
+			case '"' :
+				parent = 'string' ;
+				expected = val ;
+				break ;
+			case ':' :
+				if ( is_object ( ) ) {
+					if ( write_key && key != null ) {
+						write_key = false ;
+					}
+				}
+				expected = val ;
+				break ;
+			case ',' :
+				if ( is_object ( ) ) {
+					key = null ;
+					write_key = true ;
+					expected = /["]/ ;
+				}
+				else if ( is_array ( ) ) {
+					key ++ ;
+					expected = val ;
+				}
+				break ;
+			case '[' :
+				buffer = [ ] ;
+				flush ( ) ;
+				if ( key != null ) {
+					path.push ( key ) ;
+				}
+				key = 0 ;
+				write_key = false ;
+				expected = /[\{\[tfn"0-9\-\]]/ ;
+				break ;
+			case ']' :
+				path.pop ( ) ;
+				key = null ;
+				if ( is_object ( ) ) {
+					expected = /[\},]/ ;
+				}
+				else if ( is_array ( ) ) {
+					expected = /[\],]/ ;
+				}
+				else {
+					expected = /[]/ ;
+				}
+				break ;
+			default :
+				if ( /^[0-9]|-$/.test ( chr ) ) {
+					//number
+					parent = 'number' ;
+					i -- ;
+				}
+				else if ( /^[tfn]$/.test ( chr ) ) {
+					//special ( true | false | null )
+					parent = 'special' ;
+					i -- ;
+				}
+			}
+		}
+	}
+	
+	//check for unclosed { or [ or "
+	if ( ( parent != null && parent != 'number' ) || key != null ) {
+		syn_error ( 'Unexpected token ILLEGAL' , i - 1 ) ;
+	}	
+	else if ( parent == 'number' ) {
+		if ( number.test ( sbuffer ) ) {
+			return parseFloat ( sbuffer ) ;
+		}
+		else {
+			syn_error ( 'Unexpected token ILLEGAL' , i - 1 ) ;
+		}
+	}
+	
+	return output ;
+}
+
+function escape_str ( str ) {
+	return str.replace ( /"/g , '\\"' )
+		.replace ( /\\/g , '\\\\' )
+		.replace ( /\//g , '/' ) 
+		.replace ( /\b/g , '\\b' ) 
+		.replace ( /\f/g , '\\f' ) 
+		.replace ( /\n/g , '\\n' )
+		.replace ( /\r/g , '\\r' ) 
+		.replace ( /\t/g , '\\t' ) ;
+}
+
+function encode ( object ) {
+	//encode 
+	if ( toString ( object ) == '[object Array]' ) {
+		var output = '[' ;
+		for ( var i = 0 ; i < object.length ; i ++ ) {
+			if ( i > 0 ) {
+				output += ',' ;
+			}
+			output += encode ( object [ i ] ) ;
+		}
+		return output + ']' ;
+	}
+	else if ( toString ( object ) == '[object Object]' ) {
+		var output = '{' ,
+			i = 0 ;
+		for ( var prop in object ) {
+			if ( i > 0 ) {
+				output += ',' ;
+			}
+			output += '"' + escape_str ( prop ) + '":' + encode ( object [ prop ] ) ;
+			i ++ ;
+		}
+		return output + '}' ;
+	}
+	else if ( toString ( object ) == '[object Number]' ) {
+		return String ( object ) ;
+	}
+	else if ( toString ( object ) == '[object String]' ) {
+		return '"' + escape_str ( object ) + '"' ;
+	}
+	else {
+		if ( object ) {
+			return String ( object ) ;
+		}
+		return 'null' ;
+	}
+}
+
+return {
+	decode : decode ,
+	encode : encode
+} ;
+
+} ) ( ) ;
