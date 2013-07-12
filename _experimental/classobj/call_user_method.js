8ff1a3fb89616234c97e9b33c387ed45b882c83a
Brett Zamir <brettz9@yahoo.com>
2013-07-12 08:02:37 +0800

    Avoid eval in call_user_method funcs, allow PHP-alternative argument structure, actually call method on supplied object

--- _experimental/classobj/call_user_method.js
+++ _experimental/classobj/call_user_method.js
@@ -1,17 +1,26 @@
 function call_user_method(method, obj) {
     // http://kevin.vanzonneveld.net
     // +   original by: Brett Zamir (http://brett-zamir.me)
+    // +   input by: dnukem
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
     // -    depends on: Exception
     // %        note 1: Deprecated in PHP
-    // *     example 1: call_user_method('alert', 'this.window', 'Hello!');
+    // *     example 1: call_user_method('alert', this.window, 'Hello!');
     // *     returns 1: 'Hello!'
+    // *     example 2: call_user_method([this.window, 'alert'], 'Hello!');
+    // *     returns 2: 'Hello!'
 
-    var func;
-    func = eval(obj+"['"+method+"']");
+    var func, object = obj, methodName = method, paramBegin = 2;
+    if (method && typeof method === 'object' && method.length) {
+        paramBegin = 1;
+        object = method[0];
+        methodName = method[1];
+    }
+    func = object[methodName];
 
     if (typeof func !== 'function') {
         throw new this.Exception(func + ' is not a valid method');
     }
 
-    return func.apply(null, Array.prototype.slice.call(arguments, 2));
+    return func.apply(object, Array.prototype.slice.call(arguments, paramBegin));
 }
--- _experimental/classobj/call_user_method_array.js
+++ _experimental/classobj/call_user_method_array.js
@@ -1,17 +1,26 @@
 function call_user_method_array(method, obj, params) {
     // http://kevin.vanzonneveld.net
     // +   original by: Brett Zamir (http://brett-zamir.me)
+    // +   input by: dnukem
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
     // -    depends on: Exception
     // %        note 1: Deprecated in PHP
-    // *     example 1: call_user_method_array('alert', 'this.window', ['Hello!']);
+    // *     example 1: call_user_method_array('alert', this.window, ['Hello!']);
+    // *     returns 1: 'Hello!'
+    // *     example 1: call_user_method_array([this.window, 'alert'], ['Hello!']);
     // *     returns 1: 'Hello!'
 
-    var func;
-    func = eval(obj+"['"+method+"']");
+    var func, object = obj, methodName = method;
+    if (method && typeof method === 'object' && method.length) {
+        params = obj;
+        object = method[0];
+        methodName = method[1];
+    }
+    func = object[methodName];
 
     if (typeof func !== 'function') {
         throw new this.Exception(func + ' is not a valid method');
     }
 
-    return func.apply(null, params);
+    return func.apply(object, params);
 }
