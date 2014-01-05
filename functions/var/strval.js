cd22619fcfa5011977b91511fb4fc48d63977908
Kevin van Zonneveld <kevin@vanzonneveld.net>
2014-01-06 00:03:30 +0100

    Fixed strval#1

--- functions/var/strval.js
+++ functions/var/strval.js
@@ -3,10 +3,10 @@ function strval (str) {
   // +   original by: Brett Zamir (http://brett-zamir.me)
   // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
   // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
-  // %        note 1: Comment out the entire switch if you want JS-like behavior instead of PHP behavior
   // -    depends on: gettype
   // *     example 1: strval({red: 1, green: 2, blue: 3, white: 4});
-  // *     returns 1: 'Array'
+  // *     returns 1: 'Object'
+
   var type = '';
 
   if (str === null) {
@@ -14,6 +14,9 @@ function strval (str) {
   }
 
   type = this.gettype(str);
+
+  // Comment out the entire switch if you want JS-like
+  // behavior instead of PHP behavior
   switch (type) {
   case 'boolean':
     if (str === true) {
