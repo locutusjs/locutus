f9612daf260e37b93ea9377d01618af2ac617153
Brett Zamir <brettz9@yahoo.com>
2014-01-15 10:11:31 +0800

    Fix escapeshellarg example to properly escape backslash

--- functions/exec/escapeshellarg.js
+++ functions/exec/escapeshellarg.js
@@ -3,7 +3,7 @@ function escapeshellarg(arg) {
   // +   original by: Felix Geisendoerfer (http://www.debuggable.com/felix)
   // +   improved by: Brett Zamir (http://brett-zamir.me)
   // *     example 1: escapeshellarg("kevin's birthday");
-  // *     returns 1: "'kevin\'s birthday'"
+  // *     returns 1: "'kevin\\'s birthday'"
   var ret = '';
 
   ret = arg.replace(/[^\\]'/g, function(m, i, s) {
