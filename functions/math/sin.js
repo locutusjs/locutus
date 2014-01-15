5f4b6b7cb1e556a056c3cb7b66fdcec96aea9fa5
Brett Zamir <brettz9@yahoo.com>
2014-01-15 08:33:54 +0800

    Fix sin() example as per Firefox result (and Travis)

--- functions/math/sin.js
+++ functions/math/sin.js
@@ -2,6 +2,6 @@ function sin(arg) {
   // From: http://phpjs.org/functions
   // +   original by: Onno Marsman
   // *     example 1: sin(8723321.4);
-  // *     returns 1: -0.9834330348825909
+  // *     returns 1: -0.9834330348825929
   return Math.sin(arg);
 }
