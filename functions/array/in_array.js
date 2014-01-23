73591a67d8a33d511de316f88ee7311c48ab977b
joni2back <joni2back@gmail.com>
2014-01-23 13:20:04 -0200

    double check clarification

--- functions/array/in_array.js
+++ functions/array/in_array.js
@@ -18,7 +18,10 @@ function in_array(needle, haystack, argStrict) {
 
   var key = '',
     strict = !! argStrict;
-
+  
+  //we prevent the double check (strict && arr[key] === ndl) || (!strict && arr[key] == ndl)
+  //in just one for, in order to improve the performance 
+  //deciding wich type of comparation will do before walk array
   if (strict) {
     for (key in haystack) {
       if (haystack[key] === needle) {
