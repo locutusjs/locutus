652f37df3b443b84b2ca2c159b90e607b372f7ed
Bruce Lampson <brucelampson@gmail.com>
2014-03-08 12:49:01 -0600

    Just to be on the safe side :)
    Nice library, not a JS expert but maybe we can prevent possible iteration over inherited or unexpected properties

--- functions/array/array_search.js
+++ functions/array/array_search.js
@@ -30,16 +30,20 @@ function array_search(needle, haystack, argStrict) {
       needle = new RegExp(needle.source, flags);
     }
     for (key in haystack) {
-      if (needle.test(haystack[key])) {
-        return key;
+      if(haystack.hasOwnProperty(key)){
+          if (needle.test(haystack[key])) {
+              return key;
+          }
       }
     }
     return false;
   }
 
   for (key in haystack) {
-    if ((strict && haystack[key] === needle) || (!strict && haystack[key] == needle)) {
-      return key;
+    if(haystack.hasOwnProperty(key)){
+        if ((strict && haystack[key] === needle) || (!strict && haystack[key] == needle)) {
+            return key;
+        }
     }
   }
 
