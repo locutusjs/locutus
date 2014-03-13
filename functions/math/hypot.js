f20f13d6ba941a250f5ffaca1aac46d8e96bad6c
Kevin van Zonneveld <kevin@transloadit.com>
2014-03-13 12:31:49 +0100

    php returns `NULL` on invalid input
    ```bash
    $ php -r '$x = hypot([], "a"); var_dump($x);'
    PHP Warning:  hypot() expects parameter 1 to be double, array given in Command line code on line 1
    
    Warning: hypot() expects parameter 1 to be double, array given in Command line code on line 1
    NULL
    ```

--- functions/math/hypot.js
+++ functions/math/hypot.js
@@ -4,7 +4,7 @@ function hypot(x, y) {
   //   example 1: hypot(3, 4);
   //   returns 1: 5
   //   example 2: hypot([], 'a');
-  //   returns 2: 0
+  //   returns 2: null
 
   return Math.sqrt(x * x + y * y) || 0;
 }
