af32d77638e205d5ca9903d3c7443cdac991c022
Robert Eisele <robert@xarg.org>
2014-03-13 12:00:43 +0100

    expm1 can be expressed much simpler

--- functions/math/expm1.js
+++ functions/math/expm1.js
@@ -1,23 +1,10 @@
 function expm1(x) {
   //  discuss at: http://phpjs.org/functions/expm1/
   // original by: Brett Zamir (http://brett-zamir.me)
+  // improved by: Robert Eisele (http://www.xarg.org/)
   //        note: Precision 'n' can be adjusted as desired
   //   example 1: expm1(1e-15);
   //   returns 1: 1.0000000000000007e-15
 
-  var ret = 0,
-    // degree of precision
-    n = 50;
-  var factorial = function factorial(n) {
-    if ((n === 0) || (n === 1)) {
-      return 1;
-    } else {
-      var result = (n * factorial(n - 1));
-      return result;
-    }
-  };
-  for (var i = 1; i < n; i++) {
-    ret += Math.pow(x, i) / factorial(i);
-  }
-  return ret;
+  return (x < 1e-5 && -1e-5 < x) ? x + 0.5 * x * x : Math.exp(x) - 1;
 }
