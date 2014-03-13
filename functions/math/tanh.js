f4e1a56d36bbd8ae3b816d32649e4684d8723531
Robert Eisele <robert@xarg.org>
2014-03-13 12:02:07 +0100

    The equation of tanh() can be simplified a lot

--- functions/math/tanh.js
+++ functions/math/tanh.js
@@ -1,8 +1,9 @@
 function tanh(arg) {
   //  discuss at: http://phpjs.org/functions/tanh/
   // original by: Onno Marsman
+  // imprived by: Robert Eisele (http://www.xarg.org/)
   //   example 1: tanh(5.4251848798444815);
   //   returns 1: 0.9999612058841574
 
-  return (Math.exp(arg) - Math.exp(-arg)) / (Math.exp(arg) + Math.exp(-arg));
+  return 1 - 2 / (Math.exp(2 * arg) + 1);
 }
