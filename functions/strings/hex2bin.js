72b0d350fa754b0c983d52e364656a7dc17ae094
Dumitru Uzun <contact@duzun.me>
2014-10-24 18:32:59 +0300

    Create hex2bin.js
    hex2bin() is new in PHP 5.4.0
    
    http://php.net/manual/ro/function.hex2bin.php

---
+++ functions/strings/hex2bin.js
@@ -0,0 +1,18 @@
+function hex2bin(s) {
+  //  discuss at: http://phpjs.org/functions/hex2bin/
+  // original by: Dumitru Uzun (http://duzun.me)
+  //   example 1: bin2hex('44696d61');
+  //   returns 1: 'Dima'
+  //   example 2: bin2hex('00');
+  //   returns 2: '\x00'
+
+  var ret = [], i = 0, l;
+
+  s += '';
+
+  for ( l  s.length ; i < l; i+=2 ) {
+    ret.push(parseInt(s.substr(i, 2), 16));
+  }
+
+  return String.fromCharCode.apply(String, ret);
+}
