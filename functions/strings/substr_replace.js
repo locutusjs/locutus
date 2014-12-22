a3eae803d880e15e50d00dcb411c7cda1454f657
tobieebede <tobieebede@users.noreply.github.com>
2014-12-22 21:24:09 +0100

    Update substr_replace.js
    Syntaxe correction

--- functions/strings/substr_replace.js
+++ functions/strings/substr_replace.js
@@ -1,4 +1,4 @@
-function substr_replace(str, repl, start, length){
+function substr_replace(str, replace, start, length){
   //  discuss at: http://phpjs.org/functions/substr_replace/
   // original by: Brett Zamir (http://brett-zamir.me)
   //   example 1: substr_replace('ABCDEFGH:/MNRPQR/', 'bob', 0);
@@ -29,7 +29,7 @@ function substr_replace(str, repl, start, length){
   //   example 13: substr_replace(['A: XXXXX', 'B: XXXXX', 'C: XXXXX'], ['AAA', 'BBB', 'CCC'], [3, 4, 5], [1, 2, 3])
   //   returns 13: ['A: AAAXXXX','B: XBBBXX','C: XXCCC']
 
-  function substr_replace_(str, replace, start, length) {
+  function substr_replaceOperation(str, replace, start, length) {
 
     if (start < 0) { // start position in str
       start = start + str.length;
@@ -41,129 +41,85 @@ function substr_replace(str, repl, start, length){
 
     return str.slice(0, start) + replace.substr(0, length) + replace.slice(length) + str.slice(start + length);
   }
-  // Get length if not given
-  if (typeof(length)=="undefined") {
+  var returns = Array(); // Return values
+  var tasks = Array(); // Tasks
+  if (typeof(length) === "undefined") { // Get length if not given
     length = str;
-    if (typeof(length)=="object") {
-      for (i=0;i!=length;i++) {
-        length[i]=length[i].length;
-      }
+    if (typeof(length) === "object") {
+      for (i = 0; i !== length; i++)
+        length[i] = length[i].length;
     }else{
       length = length.length;
     }
   }
-  // Convert start to integer
-  if (typeof(start)=="object") {
-    for(i=0;i!=start.length;i++){
-      start[i]=parseInt(start[i]);
+  if (typeof(start) === "object") { // Convert start to integer
+    for(i = 0; i !== start.length; i++){
+      start[i] = parseInt(start[i]);
     }
   }else{
     start = parseInt(start);
   }
-  // Convert length to integer
-  if (typeof(length)=="object") {
-    for (i=0;i!=length.length;i++) {
-      length[i]=parseInt(length[i]);
+  if (typeof(length) === "object") { // Convert length to integer
+    for (i = 0; i != length.length; i++) {
+      length[i] = parseInt(length[i]);
     }
   }else{
     length = parseInt(length);
   }
-  var returns = Array();
-  var task = Array();
-  if (typeof(str)=="string") {
-      task[0] = Array(str);
-  }else if (typeof(str)=="object"&&typeof(str[0])=="string") {
-    for (i=0;i!=str.length;i++) {
-      task[i] = Array(str[i]);
+  if (typeof(str) === "string") { // Add string into taskss
+      tasks[0] = Array(str);
+  }else if (typeof(str) === "object") {
+    for (i = 0; i !== str.length; i++) {
+      tasks[i] = Array(str[i]);
     }
   }
-  for (i=0;i!=task.length; i++) {
-    // Replacement
-    if (typeof(repl)=="string") {
-      // String Replacement
-      task[i][1]=repl;
-    }else if (task.length==1) {
-      // Array non-Relative Replacement
-      task[i][1]=repl;
-    }else{
-      // Array Relative Replacement
-      task[i][1]=repl[i];
-    }
-    // Start
-    if (typeof(start)=="number") {
-      // String Start
-      task[i][2]=start;
-    }else if (task.length==1) {
-      // Array non-Relative Start
-      task[i][2]=start;
-    }else{
-      // Array Relative Start
-      task[i][2]=start[i];
-    }
-    // Length
-    if (typeof(length)=="number") {
-      // String Length
-      task[i][3]=length;
-    }else if (task.length==1) {
-      // Array non-Relative Length
-      task[i][3]=length;
-    }else{
-      // Array Relative Length
-      task[i][3]=length[i];
-    }
+  for (i = 0; i !== tasks.length; i++) {
+    tasks[i][1] = (typeof(replace) === "string" || tasks.length === 1)
+      ? tasks[i][1]=replace
+      : tasks[i][1]=replace[i];
+    tasks[i][2] = (typeof(start) === "number" || tasks.length === 1)
+      ? tasks[i][2]=start
+      : tasks[i][2]=start[i];
+    tasks[i][3] = (typeof(length) === "number" || tasks.length === 1)
+      ? tasks[i][3]=length
+      : tasks[i][3]=length[i];
   }
-  for (i=0;i!=task.length;i++) {
-    var opStr = task[i][0];
-    var opRep = task[i][1];
-    var opSta = task[i][2];
-    var opLen = task[i][3];
+  for (i=0;i!=tasks.length;i++) {
+    var opStr = tasks[i][0];
+    var opRep = tasks[i][1];
+    var opSta = tasks[i][2];
+    var opLen = tasks[i][3];
     var op = Array();
     var count = 1;
-    if (typeof(opRep)=="object"&&opRep.length>count) {
-      count = opRep.length;
-    }
-    if (typeof(opSta)=="object"&&opSta.length>count) {
-      count = opSta.length;
-    }
-    if (typeof(opLen)=="object"&&opLen.length>count) {
-      count = opLen.length;
-    }
+    count = (typeof(opRep) === "object"&&opRep.length>count)
+      ? opRep.length
+      : count;
+    count = (typeof(opSta) === "object"&&opSta.length>count)
+      ? opSta.length
+      : count;
+    count = (typeof(opLen) === "object"&&opLen.length>count)
+      ? opLen.length
+      : count;
     for (j=0;j!=count;j++) {
       op[j] = Array();
-      // Replacement
-      if (typeof(opRep)=="string") {
-        // String Replacement
-        op[j][0]=opRep;
-      }else{
-        // Array Relative Replacement
-        op[j][0]=opRep[j];
-      }
-      // Start
-      if (typeof(opSta)=="number") {
-        // String Start
-        op[j][1]=opSta;
-      }else{
-        // Array Relative Start
-        op[j][1]=opSta[j];
-      }
-      // Length
-      if (typeof(opLen)=="number") {
-        // String Length
-        op[j][2]=opLen;
-      }else{
-        // Array Relative Length
-        op[j][2]=opLen[j];
-      }
+      op[j][0] = (typeof(opRep) === "string")
+        ? op[j][0]=opRep
+        : op[j][0]=opRep[j];
+      op[j][1] = (typeof(opSta) === "number")
+        ? op[j][1]=opSta
+        : op[j][1]=opSta[j];
+      op[j][2] = (typeof(opLen) === "number")
+        ? op[j][2]=opLen
+        : op[j][2]=opLen[j];
     }
-    var string_buffering=opStr;
-    for (j=0;j!=op.length;j++) {
-      string_buffering = substr_replace_(string_buffering, op[j][0],op[j][1],op[j][2]);
+    var stringBuffering=opStr;
+    for (j=0;j !== op.length;j++) {
+      stringBuffering = substr_replaceOperation(stringBuffering, op[j][0],op[j][1],op[j][2]);
     }
-    returns[i] = string_buffering;
+    returns[i] = stringBuffering;
   }
-  if (returns.length==1) {
+  if (returns.length === 1) {
     return returns[0];
-  }else{
-    return returns;
   }
+  return returns;
 }
