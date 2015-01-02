d6277c30896b5ff8e101af432dfc83d17ce7c824
Elad Karako <eladkarako@gmail.com>
2015-01-02 22:32:51 +0200

    rewrite str_repeat for performances.
    rewrite str_repeat for performances.
    use es6 or array join, either way, no loops.

--- functions/strings/str_repeat.js
+++ functions/strings/str_repeat.js
@@ -3,20 +3,14 @@ function str_repeat(input, multiplier) {
   // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
   // improved by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
   // improved by: Ian Carter (http://euona.com/)
+  // improved by: Elad Karako (http://icompile.eladkarako.com/)
   //   example 1: str_repeat('-=', 10);
   //   returns 1: '-=-=-=-=-=-=-=-=-=-='
+  //   example 2: str_repeat('abc', 0);
+  //   returns 2: ''
 
-  var y = '';
-  while (true) {
-    if (multiplier & 1) {
-      y += input;
-    }
-    multiplier >>= 1;
-    if (multiplier) {
-      input += input;
-    } else {
-      break;
-    }
+    input = ("string" === typeof input) ? input : "";
+    multiplier = ("number" === typeof multiplier) ? Math.max(0, multiplier) : 0;
+
+    return ("function" === typeof String.prototype.repeat) ? String.prototype.repeat.call(input, multiplier) : ((new Array(multiplier + 1)).join(input)); //ES6 Harmony, or Array Join, Either Way: No Loops.
   }
-  return y;
-}
