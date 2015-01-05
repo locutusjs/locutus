a09f943a32e0007d7a7ddf1a685a42a83e6eae7c
Elad Karako <eladkarako@gmail.com>
2015-01-05 04:25:14 +0200

    minimum value fixed, use floor instead of ~~

--- functions/math/mt_rand.js
+++ functions/math/mt_rand.js
@@ -3,18 +3,26 @@ function mt_rand(min, max) {
   // original by: Onno Marsman
   // improved by: Brett Zamir (http://brett-zamir.me)
   //    input by: Kongo
+  // improved by: Elad Karako (http://icompile.eladkarako.com)
   //   example 1: mt_rand(1, 1);
   //   returns 1: 1
+  //   example 2: mt_rand(-40, -10);
+  //   returns 2: -37
 
-  var argc = arguments.length;
-  if (argc === 0) {
-    min = 0;
-    max = 2147483647;
-  } else if (argc === 1) {
-    throw new Error('Warning: mt_rand() expects exactly 2 parameters, 1 given');
-  } else {
-    min = parseInt(min, 10);
-    max = parseInt(max, 10);
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  var
+    MIN_INTEGER = Number && Number.MIN_SAFE_INTEGER || 0, //JavaScript native min lower-limit (default: 0)
+    MAX_INTEGER = Number && Number.MAX_SAFE_INTEGER || 2147483647; //JavaScript native max upper-limit.
+
+  //normalize input (if not number or float)
+  min = ("number" === typeof min) ? Math.floor(min) : MIN_INTEGER;
+  max = ("number" === typeof max) ? Math.floor(max) : MAX_INTEGER;
+
+  //buffer-overflow prevention.
+  min = min < MIN_INTEGER ? MIN_INTEGER : min;
+  min = min > MAX_INTEGER ? MAX_INTEGER : min;
+  max = max < MIN_INTEGER ? MIN_INTEGER : max;
+  max = max > MAX_INTEGER ? MAX_INTEGER : max;
+
+  max = ((max - min) + 1); //lowering upper-limit.
+  return min + Math.floor(Math.random() * max);
 }
