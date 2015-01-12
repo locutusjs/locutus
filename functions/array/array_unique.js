60863e63fe1e1b0b85f609cbdae619a760aa6a14
lyni <thomas.lampa@outlook.fr>
2015-01-12 08:57:38 +0100

    new array_unique
    faster service and recurcive

--- functions/array/array_unique.js
+++ functions/array/array_unique.js
@@ -1,44 +1,51 @@
-function array_unique(inputArr) {
-  //  discuss at: http://phpjs.org/functions/array_unique/
-  // original by: Carlos R. L. Rodrigues (http://www.jsfromhell.com)
-  //    input by: duncan
-  //    input by: Brett Zamir (http://brett-zamir.me)
-  // bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // bugfixed by: Nate
-  // bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // bugfixed by: Brett Zamir (http://brett-zamir.me)
-  // improved by: Michael Grier
-  //        note: The second argument, sort_flags is not implemented;
-  //        note: also should be sorted (asort?) first according to docs
-  //   example 1: array_unique(['Kevin','Kevin','van','Zonneveld','Kevin']);
-  //   returns 1: {0: 'Kevin', 2: 'van', 3: 'Zonneveld'}
-  //   example 2: array_unique({'a': 'green', 0: 'red', 'b': 'green', 1: 'blue', 2: 'red'});
-  //   returns 2: {a: 'green', 0: 'red', 1: 'blue'}
+// for ie8 and less
+if (!Array.isArray)
+		Array.isArray = function(ar) {
+			return Object.prototype.toString.call(ar) === '[object Array]';
+		};
 
-  var key = '',
-    tmp_arr2 = {},
-    val = '';
+var array_unique = function(ar){
+	var r = [], i = 0 , t;
+	for( ; i < ar.length ; i++) {
+		if(Array.isArray(ar[i]))
+			ar[i] = array_unique(ar[i])
+		else
+			for(t = i+1; t < ar.length ; t++)
+				if(ar[i] === ar[t])
+					t = ++i
+			r.push(ar[i])
+	} return r;
+},
 
-  var __array_search = function (needle, haystack) {
-    var fkey = '';
-    for (fkey in haystack) {
-      if (haystack.hasOwnProperty(fkey)) {
-        if ((haystack[fkey] + '') === (needle + '')) {
-          return fkey;
-        }
-      }
-    }
-    return false;
-  };
+// example
+var toto = [
+	'tutu',
+	'tutu',
+	'tete',
+	[
+		'tata',
+		'tata',
+		'tata',
+		[
+			'toto',
+			'toto',
+			'titi',
+		]
+	]
+];
 
-  for (key in inputArr) {
-    if (inputArr.hasOwnProperty(key)) {
-      val = inputArr[key];
-      if (false === __array_search(val, tmp_arr2)) {
-        tmp_arr2[key] = val;
-      }
-    }
-  }
-
-  return tmp_arr2;
-}
+toto = array_unique(toto);
+/*
+result :
+[
+	'tutu',
+	'tete',
+	[
+		'tata',
+		[
+			'toto',
+			'titi',
+		]
+	]
+]
+*/
