9a142ff3f5ea4324b080586c1ad0d0fd8f5f4209
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-04-18 22:55:23 +0200

    Hal

---
+++ src/c/math/abs.js
@@ -0,0 +1,10 @@
+module.exports = function abs (arg) {
+  //  discuss at: http://locutusjs.io/c/math/abs/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //   example 1: abs(-3)
+  //   returns 1: 3
+  //   example 2: abs(+3)
+  //   returns 2: 3
+
+  return Math.abs(arg)
+}
