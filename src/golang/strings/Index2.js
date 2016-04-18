cbb6542bdfd4404ac5d96eb1238d1fb11d6238cc
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-04-18 20:33:57 +0200

    Add python

--- src/golang/strings/Index2.js
+++ src/golang/strings/Index2.js
@@ -1,4 +1,4 @@
-module.exports = function Index (s, substr) {
+module.exports = function Index (s, sep) {
   //  discuss at: http://locutusjs.io/golang/strings/Index
   // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
   //   example 1: strings.Contains('Kevin', 'K')
---
+++ src/python/string/capwords.js
@@ -0,0 +1,18 @@
+module.exports = function capwords (str) {
+  //  discuss at: http://locutusjs.io/python/capwords/
+  // original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+  // improved by: Waldo Malqui Silva (http://waldo.malqui.info)
+  // improved by: Robin
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // bugfixed by: Onno Marsman
+  //    input by: James (http://www.james-bell.co.uk/)
+  //   example 1: ucwords('kevin van  zonneveld');
+  //   returns 1: 'Kevin Van  Zonneveld'
+  //   example 2: ucwords('HELLO WORLD');
+  //   returns 2: 'HELLO WORLD'
+
+  return (str + '')
+    .replace(/^([a-z\u00E0-\u00FC])|\s+([a-z\u00E0-\u00FC])/g, function ($1) {
+      return $1.toUpperCase()
+    })
+}
