e59ddf64b20bc2638a8e786c059e793de39e0293
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-04-25 08:44:39 +0200

    Fix test

--- test/test-util.js
+++ test/test-util.js
@@ -2,14 +2,14 @@
 var expect = require('chai').expect
 var Util = require('../src/_util/util')
 
-describe('locutusutil', function () {
-  describe('load', function () {
+describe('util', function () {
+  describe('_load', function () {
     it('should parse array_change_key_case correctly', function (done) {
       var util = new Util()
 
-      var relative = 'php/array/' + 'array_change_key_case.js'
+      var relative = 'php/array/array_change_key_case.js'
 
-      util.load(relative, {}, function (err, params) {
+      util._load(relative, {}, function (err, params) {
         expect(err).to.deep.equal(null)
         expect(params.headKeys.example[0][0]).to.deep.equal('array_change_key_case(42)')
         done()
