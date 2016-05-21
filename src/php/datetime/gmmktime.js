f6fe43ac38bcefd2f9f98c9ec885a5d51a7e87d6
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-05-21 13:59:26 +0200

    Use a fixed time for calculation

--- src/php/datetime/gmmktime.js
+++ src/php/datetime/gmmktime.js
@@ -35,7 +35,9 @@ module.exports = function gmmktime () {
   // Set hours, minutes, and seconds.
   d.setUTCHours(r[0], r[1], r[2])
 
+  var time = d.getTime()
+
   // Divide milliseconds by 1000 to return seconds and drop decimal.
   // Add 1 second if negative or it'll be off from PHP by 1 second.
-  return (d.getTime() / 1e3 >> 0) - (d.getTime() < 0)
+  return (time / 1e3 >> 0) - (time < 0)
 }
