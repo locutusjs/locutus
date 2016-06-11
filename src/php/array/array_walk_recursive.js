1e9ef4978f375445389fdeab262e28e1dd5b4213
Hugues Peccatte <hugues.peccatte@gmail.com>
2016-06-11 13:46:41 +0200

    Move the "try" inside the "for".
    Signed-off-by: Hugues Peccatte <hugues.peccatte@gmail.com>

--- src/php/array/array_walk_recursive.js
+++ src/php/array/array_walk_recursive.js
@@ -16,27 +16,27 @@ module.exports = function array_walk_recursive (array, funcname, userdata) { // 
     return false
   }
 
-  try {
-    for (var key in array) {
-      // apply "funcname" recursively only on arrays
-      if (Object.prototype.toString.call(array[key]) === '[object Array]') {
-        var funcArgs = [array[key], funcname]
-        if (arguments.length > 2) {
-          funcArgs.push(userdata)
-        }
-        if (array_walk_recursive.apply(null, funcArgs) === false) {
-          return false
-        }
-        continue
+  for (var key in array) {
+    // apply "funcname" recursively only on arrays
+    if (Object.prototype.toString.call(array[key]) === '[object Array]') {
+      var funcArgs = [array[key], funcname]
+      if (arguments.length > 2) {
+        funcArgs.push(userdata)
       }
+      if (array_walk_recursive.apply(null, funcArgs) === false) {
+        return false
+      }
+      continue
+    }
+    try {
       if (arguments.length > 2) {
         funcname(array[key], key, userdata)
       } else {
         funcname(array[key], key)
       }
+    } catch (e) {
+      return false
     }
-  } catch (e) {
-    return false
   }
 
   return true
