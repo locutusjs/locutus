ee4ed62fcdf32fa752040402d01c8be121c4fcb6
Reynier de la Rosa <sirio3mil@users.noreply.github.com>
2016-11-08 12:50:56 +0100

    Unexpected result when str is null (#313)
    * Unexpected result when str is null
    
    Some lattest browsers when str is null return and unexpected null value
    
    * Update nl2br.js
    
    * correct typo 'lattest'
    
    * Update nl2br.js
    
    add test
    
    * Update nl2br.js
    
    codding standards

--- src/php/strings/nl2br.js
+++ src/php/strings/nl2br.js
@@ -8,6 +8,7 @@ module.exports = function nl2br (str, isXhtml) {
   // improved by: Maximusya
   // bugfixed by: Onno Marsman (https://twitter.com/onnomarsman)
   // bugfixed by: Kevin van Zonneveld (http://kvz.io)
+  // bugfixed by: Reynier de la Rosa (http://scriptinside.blogspot.com.es/)
   //    input by: Brett Zamir (http://brett-zamir.me)
   //   example 1: nl2br('Kevin\nvan\nZonneveld')
   //   returns 1: 'Kevin<br />\nvan<br />\nZonneveld'
@@ -15,7 +16,14 @@ module.exports = function nl2br (str, isXhtml) {
   //   returns 2: '<br>\nOne<br>\nTwo<br>\n<br>\nThree<br>\n'
   //   example 3: nl2br("\nOne\nTwo\n\nThree\n", true)
   //   returns 3: '<br />\nOne<br />\nTwo<br />\n<br />\nThree<br />\n'
+  //   example 4: nl2br(null)
+  //   returns 4: ''
 
+  // Some latest browsers when str is null return and unexpected null value
+  if (typeof str === 'undefined' || str === null) {
+    return ''
+  }
+  
   // Adjust comment to avoid issue on locutus.io display
   var breakTag = (isXhtml || typeof isXhtml === 'undefined') ? '<br ' + '/>' : '<br>'
 
