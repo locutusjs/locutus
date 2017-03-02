0ace579401ed1c446d756bb86e0b14cbbe3e827a
Theriault <dustintheriault@gmail.com>
2017-03-02 08:54:56 -0700

    Fixed lint issues

--- src/php/strings/strcspn.js
+++ src/php/strings/strcspn.js
@@ -12,14 +12,14 @@ module.exports = function strcspn (str, mask, start, length) {
   //   returns 4: 1
 
   start = start || 0
-  length = typeof length === "undefined" ? str.length : (length || 0)
+  length = typeof length === 'undefined' ? str.length : (length || 0)
   if (start < 0) start = str.length + start
   if (length < 0) length = str.length - start + length
   if (start < 0 || start >= str.length || length <= 0 || e >= str.length) return 0
   var e = Math.min(str.length, start + length)
   for (var i = start, lgth = 0; i < e; i++) {
     if (mask.indexOf(str.charAt(i)) !== -1) {
-      break;
+      break
     }
     ++lgth
   }
