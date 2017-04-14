a47859a84b321a1f739dbc98bcd0e0d93ecba2e1
TashiuZavoziu <s.tet@meta.ua>
2017-04-14 03:57:42 +0300

    Remove useless regex part
    What was even the point of that regex part? :)

--- src/php/strings/nl2br.js
+++ src/php/strings/nl2br.js
@@ -28,5 +28,5 @@ module.exports = function nl2br (str, isXhtml) {
   var breakTag = (isXhtml || typeof isXhtml === 'undefined') ? '<br ' + '/>' : '<br>'
 
   return (str + '')
-    .replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2')
+    .replace(/(\r\n|\n\r|\r|\n)/g, breakTag + '$1')
 }
