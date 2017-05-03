4566fcc3e1de876a8a0ccaea443247c1092c6fcc
Rafał Kukawski <rafal@kukawski.pl>
2017-05-03 10:08:44 +0200

    Fix issue #327 Improved compatibility with PHP's wordwrap (#328)
    * Fix issue #327 Improved compatibility with PHP's wordwrap
    
    Improved compatilibity with PHP's wordwrap
    Made the code a little more readable
    
    * Fix lint issue

--- src/php/strings/wordwrap.js
+++ src/php/strings/wordwrap.js
@@ -7,39 +7,84 @@ module.exports = function wordwrap (str, intWidth, strBreak, cut) {
   //  revised by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
   // bugfixed by: Michael Grier
   // bugfixed by: Feras ALHAEK
-  //      note 1: It would be great if this function could be split up to have
-  //      note 1: smaller line lengths, less ternary operators, and more readable variable names
+  // improved by: Rafał Kukawski (http://kukawski.net)
   //   example 1: wordwrap('Kevin van Zonneveld', 6, '|', true)
-  //   returns 1: 'Kevin |van |Zonnev|eld'
+  //   returns 1: 'Kevin|van|Zonnev|eld'
   //   example 2: wordwrap('The quick brown fox jumped over the lazy dog.', 20, '<br />\n')
-  //   returns 2: 'The quick brown fox <br />\njumped over the lazy<br />\n dog.'
+  //   returns 2: 'The quick brown fox<br />\njumped over the lazy<br />\ndog.'
   //   example 3: wordwrap('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
-  //   returns 3: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim \nveniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea \ncommodo consequat.'
+  //   returns 3: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim\nveniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea\ncommodo consequat.'
 
-  var m = ((arguments.length >= 2) ? arguments[1] : 75)
-  var b = ((arguments.length >= 3) ? arguments[2] : '\n')
-  var c = ((arguments.length >= 4) ? arguments[3] : false)
+  intWidth = arguments.length >= 2 ? +intWidth : 75
+  strBreak = arguments.length >= 3 ? '' + strBreak : '\n'
+  cut = arguments.length >= 4 ? !!cut : false
 
-  var i, j, l, s, r
+  var i, j, line
 
   str += ''
 
-  if (m < 1) {
+  if (intWidth < 1) {
     return str
   }
 
-  for (i = -1, l = (r = str.split(/\r\n|\n|\r/)).length; ++i < l; r[i] += s) {
-    // @todo: Split this up over many more lines and more semantic variable names
-    // so it becomes readable
-    for (s = r[i], r[i] = '';
-      s.length > m;
-      r[i] += s.slice(0, j) + ((s = s.slice(j)).length ? b : '')) {
-      j = c === 2 || (j = s.slice(0, m + 1).match(/\S*(\s)?$/))[1]
-        ? m
-        : j.input.length - j[0].length || c === true && m ||
-          j.input.length + (j = s.slice(m).match(/^\S*/))[0].length
+  var reLineBreaks = /\r\n|\n|\r/
+  var reBeginningUntilFirstWhitespace = /^\S*/
+  var reLastCharsWithOptionalTrailingWhitespace = /\S*(\s)?$/
+
+  var lines = str.split(reLineBreaks)
+  var l = lines.length
+  var match
+
+  // for each line of text
+  for (i = 0; i < l; lines[i++] += line) {
+    line = lines[i]
+    lines[i] = ''
+
+    while (line.length > intWidth) {
+      // get slice of length one char above limit
+      var slice = line.slice(0, intWidth + 1)
+
+      // remove leading whitespace from rest of line to parse
+      var ltrim = 0
+      // remove trailing whitespace from new line content
+      var rtrim = 0
+
+      match = slice.match(reLastCharsWithOptionalTrailingWhitespace)
+
+      // if the slice ends with whitespace
+      if (match[1]) {
+        // then perfect moment to cut the line
+        j = intWidth
+        ltrim = 1
+      } else {
+        // otherwise cut at previous whitespace
+        j = slice.length - match[0].length
+
+        if (j) {
+          rtrim = 1
+        }
+
+        // but if there is no previous whitespace
+        // and cut is forced
+        // cut just at the defined limit
+        if (!j && cut && intWidth) {
+          j = intWidth
+        }
+
+        // if cut wasn't forced
+        // cut at next possible whitespace after the limit
+        if (!j) {
+          var charsUntilNextWhitespace = (line.slice(intWidth).match(reBeginningUntilFirstWhitespace) || [''])[0]
+
+          j = slice.length + charsUntilNextWhitespace.length
+        }
+      }
+
+      lines[i] += line.slice(0, j - rtrim)
+      line = line.slice(j + ltrim)
+      lines[i] += line.length ? strBreak : ''
     }
   }
 
-  return r.join('\n')
+  return lines.join('\n')
 }
