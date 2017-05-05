04d8738e87875130ce45bdba9319190dbfe0f079
cpojer <christoph.pojer@gmail.com>
2017-05-06 01:54:32 +0200

    Convert to Jest

--- test/util/test-util.js
+++ test/util/test-util.js
@@ -1,6 +1,5 @@
 // var fs = require('fs')
-var expect = require('chai').expect
-var Util = require('../../src/_util/util')
+var Util = require('../../src/_util/util');
 
 describe('util', function () {
   describe('_load', function () {
@@ -10,8 +9,8 @@ describe('util', function () {
       var relative = 'php/array/array_change_key_case.js'
 
       util._load(relative, {}, function (err, params) {
-        expect(err).to.deep.equal(null)
-        expect(params.headKeys.example[0][0]).to.deep.equal('array_change_key_case(42)')
+        expect(err).toEqual(null)
+        expect(params.headKeys.example[0][0]).toEqual('array_change_key_case(42)')
         done()
       })
     })
