f76f9f87b993f26a687dd9972f77ff47207a2e88
Indigo744 <Indigo744@users.noreply.github.com>
2017-05-17 13:53:14 +0200

    Fix for JS coding practices

--- src/php/url/base64_encode.js
+++ src/php/url/base64_encode.js
@@ -22,10 +22,10 @@ module.exports = function base64_encode (stringToEncode) { // eslint-disable-lin
     // first we use encodeURIComponent to get percent-encoded UTF-8,
     // then we convert the percent encodings into raw bytes which
     // can be fed into the base64 encoding algorithm.
-      return encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
-        function toSolidBytes (match, p1) {
-          return String.fromCharCode('0x' + p1)
-        })
+    return encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+      function toSolidBytes (match, p1) {
+        return String.fromCharCode('0x' + p1)
+      })
   }
 
   if (typeof window !== 'undefined') {
