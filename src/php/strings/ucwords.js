404552b91eb78e85e7c896262e213cc53283e669
Alexandr Cetvertacov <cetver@gmail.com>
2017-05-26 10:42:01 +0300

    PHP.ucwords - add multibyte support (#334)
    * Add multibyte support
    
    * add my name

--- src/php/strings/ucwords.js
+++ src/php/strings/ucwords.js
@@ -5,14 +5,19 @@ module.exports = function ucwords (str) {
   // improved by: Robin
   // improved by: Kevin van Zonneveld (http://kvz.io)
   // bugfixed by: Onno Marsman (https://twitter.com/onnomarsman)
+  // bugfixed by: Cetvertacov Alexandr (https://github.com/cetver)
   //    input by: James (http://www.james-bell.co.uk/)
   //   example 1: ucwords('kevin van  zonneveld')
   //   returns 1: 'Kevin Van  Zonneveld'
   //   example 2: ucwords('HELLO WORLD')
   //   returns 2: 'HELLO WORLD'
+  //   example 3: ucwords('у мэри был маленький ягненок и она его очень любила')
+  //   returns 3: 'У Мэри Был Маленький Ягненок И Она Его Очень Любила'
+  //   example 4: ucwords('τάχιστη αλώπηξ βαφής ψημένη γη, δρασκελίζει υπέρ νωθρού κυνός')
+  //   returns 4: 'Τάχιστη Αλώπηξ Βαφής Ψημένη Γη, Δρασκελίζει Υπέρ Νωθρού Κυνός'
 
   return (str + '')
-    .replace(/^([a-z\u00E0-\u00FC])|\s+([a-z\u00E0-\u00FC])/g, function ($1) {
+    .replace(/^(.)|\s+(.)/g, function ($1) {
       return $1.toUpperCase()
     })
 }
