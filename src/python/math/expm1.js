56d8ac15f069fc85206533cc6dac9cc59756dc56
xZDenny <dennylungu19@gmail.com>
2017-07-11 14:41:03 +0300

    Update expm1.js

--- src/python/math/expm1.js
+++ src/python/math/expm1.js
@@ -1,1 +1,10 @@
+module.exports = function expm1 (float) {
+  //  discuss at: 
+  // original by: xDenny (http://www.github.com/xZDenny)
+  //   example 1: expm1(19)
+  //   returns 1: 178482299.96318728
 
+  var value;
+  value = Math.exp(float) - 1;
+  return value
+}
