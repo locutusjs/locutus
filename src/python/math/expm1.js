2a31a746e99d5e0ce8d83d7714ba05b7282b3bb1
xZDenny <dennylungu19@gmail.com>
2017-07-11 14:52:59 +0300

    Update expm1.js

--- src/python/math/expm1.js
+++ src/python/math/expm1.js
@@ -1,8 +1,6 @@
 module.exports = function expm1 (x) {
   //  discuss at: 
   // original by: xDenny (http://www.github.com/xZDenny)
-  //   example 1: expm1(19)
-  //   returns 1: 178482299.96318728
 
   var value;
   value = Math.exp(x) - 1;
