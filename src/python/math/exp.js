23d3d0de025aba07ab8a62a4db648ac75a6ddb6a
xZDenny <dennylungu19@gmail.com>
2017-07-11 14:43:21 +0300

    Update exp.js

--- src/python/math/exp.js
+++ src/python/math/exp.js
@@ -1,8 +1,8 @@
 module.exports = function exp (x) {
   //  discuss at: 
   // original by: xDenny (http://www.github.com/xZDenny)
-  //   example 1: exp(19)
-  //   returns 1: 
+  //   example 1: exp(5)
+  //   returns 1: 148.4131591025766
 
   var value;
   value = Math.exp(x);
