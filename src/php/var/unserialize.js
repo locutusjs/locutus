b4ecb15b04534713ad513d4424f1109e67238858
Jeffery ThaGintoki <jefferytutorials@gmail.com>
2017-10-29 19:47:23 +0000

    Fix unserialize objects issue
    When an 'o' is found in the string it is considered an object, in PHP objects are not same as Arrays which is the complete opposite in JavaScript, because an array is at the same time an array.

--- src/php/var/unserialize.js
+++ src/php/var/unserialize.js
@@ -1,4 +1,6 @@
-module.exports = function unserialize (data) {
+'use strict';
+
+module.exports = function unserialize(data) {
   //  discuss at: http://locutus.io/php/unserialize/
   // original by: Arpad Ray (mailto:arpad@php.net)
   // improved by: Pedro Tainha (http://www.pedrotainha.com)
@@ -28,175 +30,204 @@ module.exports = function unserialize (data) {
   //   example 3: unserialize('a:3:{s:2:"ü";s:2:"ü";s:3:"四";s:3:"四";s:4:"𠜎";s:4:"𠜎";}')
   //   returns 3: {'ü': 'ü', '四': '四', '𠜎': '𠜎'}
 
-  var $global = (typeof window !== 'undefined' ? window : global)
+  var $global = typeof window !== 'undefined' ? window : global;
 
-  var utf8Overhead = function (str) {
-    var s = str.length
+  var utf8Overhead = function utf8Overhead(str) {
+    var s = str.length;
     for (var i = str.length - 1; i >= 0; i--) {
-      var code = str.charCodeAt(i)
+      var code = str.charCodeAt(i);
       if (code > 0x7f && code <= 0x7ff) {
-        s++
+        s++;
       } else if (code > 0x7ff && code <= 0xffff) {
-        s += 2
+        s += 2;
       }
       // trail surrogate
       if (code >= 0xDC00 && code <= 0xDFFF) {
-        i--
+        i--;
       }
     }
-    return s - 1
-  }
-  var error = function (type,
-    msg, filename, line) {
-    throw new $global[type](msg, filename, line)
-  }
-  var readUntil = function (data, offset, stopchr) {
-    var i = 2
-    var buf = []
-    var chr = data.slice(offset, offset + 1)
+    return s - 1;
+  };
+  var error = function error(type, msg, filename, line) {
+    throw new $global[type](msg, filename, line);
+  };
+  var readUntil = function readUntil(data, offset, stopchr) {
+    var i = 2;
+    var buf = [];
+    var chr = data.slice(offset, offset + 1);
 
     while (chr !== stopchr) {
-      if ((i + offset) > data.length) {
-        error('Error', 'Invalid')
+      if (i + offset > data.length) {
+        error('Error', 'Invalid');
       }
-      buf.push(chr)
-      chr = data.slice(offset + (i - 1), offset + i)
-      i += 1
+      buf.push(chr);
+      chr = data.slice(offset + (i - 1), offset + i);
+      i += 1;
     }
-    return [buf.length, buf.join('')]
-  }
-  var readChrs = function (data, offset, length) {
-    var i, chr, buf
+    return [buf.length, buf.join('')];
+  };
+  var readChrs = function readChrs(data, offset, length) {
+    var i, chr, buf;
 
-    buf = []
+    buf = [];
     for (i = 0; i < length; i++) {
-      chr = data.slice(offset + (i - 1), offset + i)
-      buf.push(chr)
-      length -= utf8Overhead(chr)
+      chr = data.slice(offset + (i - 1), offset + i);
+      buf.push(chr);
+      length -= utf8Overhead(chr);
     }
-    return [buf.length, buf.join('')]
-  }
-  function _unserialize (data, offset) {
-    var dtype
-    var dataoffset
-    var keyandchrs
-    var keys
-    var contig
-    var length
-    var array
-    var readdata
-    var readData
-    var ccount
-    var stringlength
-    var i
-    var key
-    var kprops
-    var kchrs
-    var vprops
-    var vchrs
-    var value
-    var chrs = 0
-    var typeconvert = function (x) {
-      return x
-    }
+    return [buf.length, buf.join('')];
+  };
+  function _unserialize(data, offset) {
+    var dtype;
+    var dataoffset;
+    var keyandchrs;
+    var keys;
+    var contig;
+    var length;
+    var array;
+    var readdata;
+    var readData;
+    var ccount;
+    var stringlength;
+    var i;
+    var key;
+    var kprops;
+    var kchrs;
+    var vprops;
+    var vchrs;
+    var value;
+    var chrs = 0;
+    var typeconvert = function typeconvert(x) {
+      return x;
+    };
 
     if (!offset) {
-      offset = 0
+      offset = 0;
     }
-    dtype = (data.slice(offset, offset + 1)).toLowerCase()
+    dtype = data.slice(offset, offset + 1).toLowerCase();
 
-    dataoffset = offset + 2
+    dataoffset = offset + 2;
 
     switch (dtype) {
       case 'i':
-        typeconvert = function (x) {
-          return parseInt(x, 10)
-        }
-        readData = readUntil(data, dataoffset, ';')
-        chrs = readData[0]
-        readdata = readData[1]
-        dataoffset += chrs + 1
-        break
+        typeconvert = function typeconvert(x) {
+          return parseInt(x, 10);
+        };
+        readData = readUntil(data, dataoffset, ';');
+        chrs = readData[0];
+        readdata = readData[1];
+        dataoffset += chrs + 1;
+        break;
       case 'b':
-        typeconvert = function (x) {
-          return parseInt(x, 10) !== 0
-        }
-        readData = readUntil(data, dataoffset, ';')
-        chrs = readData[0]
-        readdata = readData[1]
-        dataoffset += chrs + 1
-        break
+        typeconvert = function typeconvert(x) {
+          return parseInt(x, 10) !== 0;
+        };
+        readData = readUntil(data, dataoffset, ';');
+        chrs = readData[0];
+        readdata = readData[1];
+        dataoffset += chrs + 1;
+        break;
       case 'd':
-        typeconvert = function (x) {
-          return parseFloat(x)
-        }
-        readData = readUntil(data, dataoffset, ';')
-        chrs = readData[0]
-        readdata = readData[1]
-        dataoffset += chrs + 1
-        break
+        typeconvert = function typeconvert(x) {
+          return parseFloat(x);
+        };
+        readData = readUntil(data, dataoffset, ';');
+        chrs = readData[0];
+        readdata = readData[1];
+        dataoffset += chrs + 1;
+        break;
       case 'n':
-        readdata = null
-        break
+        readdata = null;
+        break;
       case 's':
-        ccount = readUntil(data, dataoffset, ':')
-        chrs = ccount[0]
-        stringlength = ccount[1]
-        dataoffset += chrs + 2
+        ccount = readUntil(data, dataoffset, ':');
+        chrs = ccount[0];
+        stringlength = ccount[1];
+        dataoffset += chrs + 2;
 
-        readData = readChrs(data, dataoffset + 1, parseInt(stringlength, 10))
-        chrs = readData[0]
-        readdata = readData[1]
-        dataoffset += chrs + 2
+        readData = readChrs(data, dataoffset + 1, parseInt(stringlength, 10));
+        chrs = readData[0];
+        readdata = readData[1];
+        dataoffset += chrs + 2;
         if (chrs !== parseInt(stringlength, 10) && chrs !== readdata.length) {
-          error('SyntaxError', 'String length mismatch')
+          error('SyntaxError', 'String length mismatch');
         }
-        break
+        break;
       case 'a':
-        readdata = {}
+        readdata = {};
 
-        keyandchrs = readUntil(data, dataoffset, ':')
-        chrs = keyandchrs[0]
-        keys = keyandchrs[1]
-        dataoffset += chrs + 2
+        keyandchrs = readUntil(data, dataoffset, ':');
+        chrs = keyandchrs[0];
+        keys = keyandchrs[1];
+        dataoffset += chrs + 2;
 
-        length = parseInt(keys, 10)
-        contig = true
+        length = parseInt(keys, 10);
+        contig = true;
 
         for (i = 0; i < length; i++) {
-          kprops = _unserialize(data, dataoffset)
-          kchrs = kprops[1]
-          key = kprops[2]
-          dataoffset += kchrs
+          kprops = _unserialize(data, dataoffset);
+          kchrs = kprops[1];
+          key = kprops[2];
+          dataoffset += kchrs;
 
-          vprops = _unserialize(data, dataoffset)
-          vchrs = vprops[1]
-          value = vprops[2]
-          dataoffset += vchrs
+          vprops = _unserialize(data, dataoffset);
+          vchrs = vprops[1];
+          value = vprops[2];
+          dataoffset += vchrs;
 
           if (key !== i) {
-            contig = false
+            contig = false;
           }
 
-          readdata[key] = value
+          readdata[key] = value;
         }
 
         if (contig) {
-          array = new Array(length)
+          array = new Array(length);
           for (i = 0; i < length; i++) {
-            array[i] = readdata[i]
+            array[i] = readdata[i];
           }
-          readdata = array
+          readdata = array;
         }
 
-        dataoffset += 1
-        break
+        dataoffset += 1;
+        break;
+      case 'o':
+        ccount = readUntil(data, dataoffset, ':');
+        dataoffset += ccount[0] + 2;
+
+        ccount = readUntil(data, dataoffset, '"');
+        dataoffset += ccount[0] + 2;
+
+        readdata = {};
+
+        keyandchrs = readUntil(data, dataoffset, ':');
+        chrs = keyandchrs[0];
+        keys = keyandchrs[1];
+        dataoffset += chrs + 2;
+
+        for (i = 0; i < parseInt(keys, 10); i++) {
+          kprops = _unserialize(data, dataoffset);
+          kchrs = kprops[1];
+          key = kprops[2];
+          dataoffset += kchrs;
+
+          vprops = _unserialize(data, dataoffset);
+          vchrs = vprops[1];
+          value = vprops[2];
+          dataoffset += vchrs;
+
+          readdata[key] = value;
+        }
+        dataoffset += 1;
+        break;
       default:
-        error('SyntaxError', 'Unknown / Unhandled data type(s): ' + dtype)
-        break
+        error('SyntaxError', 'Unknown / Unhandled data type(s): ' + dtype);
+        break;
     }
-    return [dtype, dataoffset - offset, typeconvert(readdata)]
+    return [dtype, dataoffset - offset, typeconvert(readdata)];
   }
 
-  return _unserialize((data + ''), 0)[2]
-}
+  return _unserialize(data + '', 0)[2];
+};
+//# sourceMappingURL=unserialize.js.map
