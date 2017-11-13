a0025b32eb2d77e4f59d37f77dc2928723db7091
kale-io <alpha@codeyourkale.ai>
2017-11-13 14:35:48 +0200

    Convert Non-strict to strict equality checking Avoid the more obscure equality algorithm used when using == / != and rely on the strict equality checking algorithm employed by === / !===

--- website/themes/icarus/scripts/thumbnail.js
+++ website/themes/icarus/scripts/thumbnail.js
@@ -16,7 +16,7 @@ hexo.extend.helper.register('thumbnail', function (post) {
             var pattern = /^[\\{0,1}\/{0,1}]([^\/^\\]+)/,
                 pattern_ = /([^\/^\\]+)/;
             if ((ret = pattern.exec(url)) != null) {
-                if(ret[0].length == url.length) {
+                if(ret[0].length === url.length) {
                     url = post.path + ret[1];
                 }
             } else if ((ret = pattern_.exec(url)) != null) {
