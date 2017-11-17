a32f21c0079a63a232590551bc17840d4abf5cd0
kale-io <alpha@codeyourkale.ai>
2017-11-17 15:57:14 +0200

    Explicit conversion between types Use explicit conversions between types (Number, String).

--- src/php/array/asort.js
+++ src/php/array/asort.js
@@ -78,7 +78,7 @@ module.exports = function asort (inputArr, sortFlags) {
         var aFloat = parseFloat(a)
         var bFloat = parseFloat(b)
         var aNumeric = aFloat + '' === a
-        var bNumeric = bFloat + '' === b
+        var bNumeric = String(bFloat) === b
         if (aNumeric && bNumeric) {
           return aFloat > bFloat ? 1 : aFloat < bFloat ? -1 : 0
         } else if (aNumeric && !bNumeric) {
