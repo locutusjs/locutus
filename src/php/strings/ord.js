f6388f859e68272c6a897eb59c7d95b162d77930
kale-io <alpha@codeyourkale.ai>
2017-11-17 17:25:08 +0200

    Explicit conversion between types Use explicit conversions between types (Number, String).

--- src/php/strings/ord.js
+++ src/php/strings/ord.js
@@ -9,10 +9,10 @@ module.exports = function ord (string) {
   //   example 2: ord('\uD800\uDC00'); // surrogate pair to create a single Unicode character
   //   returns 2: 65536
 
-  var str = string + ''
+  var str = String(string)
   var code = str.charCodeAt(0)
 
-  if (code >= 0xD800 && code <= 0xDBFF) {
+  if (code >= 55296 && code <= 56319) {
     // High surrogate (could change last hex to 0xDB7F to treat
     // high private surrogates as single characters)
     var hi = code
@@ -21,19 +21,17 @@ module.exports = function ord (string) {
       // so we return its value;
       return code
       // we could also throw an error as it is not a complete character,
-      // but someone may want to know
-    }
+      }
     var low = str.charCodeAt(1)
-    return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000
+    return ((hi - 55296) * 1024) + (low - 56320) + 65536
   }
-  if (code >= 0xDC00 && code <= 0xDFFF) {
+  if (code >= 56320 && code <= 57343) {
     // Low surrogate
     // This is just a low surrogate with no preceding high surrogate,
     // so we return its value;
     return code
     // we could also throw an error as it is not a complete character,
-    // but someone may want to know
-  }
+    }
 
   return code
 }
