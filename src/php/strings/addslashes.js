8510d123ad6f2eec4f9d1579a32712c385f3af56
kale-io <alpha@codeyourkale.ai>
2017-11-17 16:22:34 +0200

    Explicit conversion between types Use explicit conversions between types (Number, String).

--- src/php/strings/addslashes.js
+++ src/php/strings/addslashes.js
@@ -11,7 +11,7 @@ module.exports = function addslashes (str) {
   //   example 1: addslashes("kevin's birthday")
   //   returns 1: "kevin\\'s birthday"
 
-  return (str + '')
+  return (String(str))
     .replace(/[\\"']/g, '\\$&')
     .replace(/\u0000/g, '\\0')
 }
