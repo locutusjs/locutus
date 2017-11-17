66029c71e775a36fad687895c9b23669debc1821
kale-io <alpha@codeyourkale.ai>
2017-11-17 17:25:13 +0200

    Explicit conversion between types Use explicit conversions between types (Number, String).

--- src/php/strings/rtrim.js
+++ src/php/strings/rtrim.js
@@ -9,7 +9,7 @@ module.exports = function rtrim (str, charlist) {
   //   example 1: rtrim('    Kevin van Zonneveld    ')
   //   returns 1: '    Kevin van Zonneveld'
 
-  charlist = !charlist ? ' \\s\u00A0' : (charlist + '')
+  charlist = !charlist ? ' \\s\u00A0' : (String(charlist))
     .replace(/([[\]().?/*{}+$^:])/g, '\\$1')
 
   var re = new RegExp('[' + charlist + ']+$', 'g')
