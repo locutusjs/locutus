29ff54587e3a14c26f0c2338377b54ab8dbdc5ff
kale-io <alpha@codeyourkale.ai>
2017-11-17 17:25:23 +0200

    Explicit conversion between types Use explicit conversions between types (Number, String).

--- src/php/strings/strncmp.js
+++ src/php/strings/strncmp.js
@@ -12,7 +12,7 @@ module.exports = function strncmp (str1, str2, lgth) {
 
   var s1 = (str1 + '')
     .substr(0, lgth)
-  var s2 = (str2 + '')
+  var s2 = (String(str2))
     .substr(0, lgth)
 
   return ((s1 === s2) ? 0 : ((s1 > s2) ? 1 : -1))
