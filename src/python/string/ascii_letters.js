7017d1b1b71063a0afd375c2069252b6575796b4
Yury Shapkarin <yu.shapkarin@gmail.com>
2018-11-03 18:55:13 +0300

    fix lint errors

--- src/python/string/ascii_letters.js
+++ src/python/string/ascii_letters.js
@@ -1,14 +1,16 @@
-module.exports = function ascii_letters() {
+module.exports = function ascii_letters () {
   //   original by: Yury Shapkarin (http://shapkarin.me)
   //   example 1: ascii_letters()
   //   returns 1: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-  var pattern = /[a-zA-Z]+/g;
-  var length = 26;
-  var startFrom = 65;
+  var pattern = /[a-zA-Z]+/g
+  var length = 26
+  var i = 65
 
   return [...Array(length + 6 + length)]
-      .reduce(a => a + String.fromCharCode(i++), '' , i=startFrom)
-      .match(pattern)
-      .reverse().join('')
+          .reduce(function(accumulator){
+              return accumulator + String.fromCharCode(i++)
+          }, '')
+          .match(pattern)
+          .reverse().join('')
 }
