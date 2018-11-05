065d636ccf5aa100f1fe91a978b499c45a476f1d
rony <rony@amanshiabacon.com>
2018-11-05 16:00:53 +0600

    preg_match example updated, browser test added, lint corrected.

--- test/browser/app.js
+++ test/browser/app.js
@@ -7,7 +7,10 @@ var sha1 = require('../../src/php/strings/sha1')
 var isArray = require('../../src/php/var/is_array')
 var iniSet = require('../../src/php/info/ini_set')
 var varDump = require('../../src/php/var/var_dump')
+var preg_match = require('../../src/php/pcre/preg_match')
 
+varDump(preg_match('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$', 'rony@pharaohtools.com')) // Should report true
+varDump(preg_match('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$', 'ronypharaohtools.com')) // Should report flase
 document.write(sprintf('Hey %s, please check the console log', 'you'))
 varDump(iniSet('locutus.objectsAsArrays', 'on'))
 varDump(isArray({'name': 'locutus'})) // Should report true
