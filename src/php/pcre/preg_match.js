fb125f534c2a6679ab3a4b0e2290e48ac0fb0d4a
rony <rony@pharaohtools.com>
2018-11-05 19:06:40 +0600

    preg_match: lint corected

--- src/php/pcre/preg_match.js
+++ src/php/pcre/preg_match.js
@@ -4,6 +4,5 @@ module.exports = function preg_match (regex, str) { // eslint-disable-line camel
   //   returns 1: true
   //   example 2: preg_match("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", "ronypharaohtools.com")
   //   returns 2: false
-  
   return (new RegExp(regex).test(str))
 }
