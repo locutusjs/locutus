9408f661f1c320ab392a8551ebb03a8e2a35c678
edwinschi <edwinschi@gmail.com>
2018-11-06 06:14:48 -0200

    Rounding problem (#336)
    * Corret rounding problem
    
    The function is rouding some numbers wrong:
    
    Example 1: number_format(35.855,2,",",".")
    Result 1: 35,85 the corret result is 35,86
    
    Example 2: number_format(33.6750,2,",",".")
    Result 1: 33,67 the corret result is 33,68
    
    my changes fix that, i got the rounding fuction from: 
    https://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-only-if-necessary
    
    * Rounding problem
    
    Corrected some spaces and double quotes.
    Changed "includes" to indexOf.
    
    * Create number_format.js
    
    * Update number_format.js

--- src/php/strings/number_format.js
+++ src/php/strings/number_format.js
@@ -57,13 +57,20 @@ module.exports = function number_format (number, decimals, decPoint, thousandsSe
   var s = ''
 
   var toFixedFix = function (n, prec) {
-    var k = Math.pow(10, prec)
-    return '' + (Math.round(n * k) / k)
-      .toFixed(prec)
+    if (('' + n).indexOf('e') === -1) {
+      return +(Math.round(n + 'e+' + prec) + 'e-' + prec)
+    } else {
+      var arr = ('' + n).split('e')
+      var sig = ''
+      if (+arr[1] + prec > 0) {
+        sig = '+'
+      }
+      return (+(Math.round(+arr[0] + 'e' + sig + (+arr[1] + prec)) + 'e-' + prec)).toFixed(prec)
+    }
   }
 
   // @todo: for IE parseFloat(0.55).toFixed(0) = 0;
-  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.')
+  s = (prec ? toFixedFix(n, prec).toString() : '' + Math.round(n)).split('.')
   if (s[0].length > 3) {
     s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep)
   }
