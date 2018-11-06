daf2d99a77befb44423ad6ed4d63b9c42dcb65f6
Yury Shapkarin <yu.shapkarin@gmail.com>
2018-11-06 11:04:10 +0300

    Add Python's ascii_uppercase and ascii_lowercase ( and fix all ESLint errors) (#382)
    * fix ESLint errors
    
    * const and let
    
    * ascii_uppercase
    
    * ascii_lowercase
    
    * index module.exports
    
    * injectweb

--- src/python/string/ascii_letters.js
+++ src/python/string/ascii_letters.js
@@ -2,14 +2,15 @@ module.exports = function ascii_letters () { // eslint-disable-line camelcase
   //   original by: Yury Shapkarin (http://shapkarin.me)
   //   example 1: ascii_letters()
   //   returns 1: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  var pattern = /[a-zA-Z]+/g
-  var length = 26
-  var i = 65
+
+  const length = 26
+  let i = 65
 
   return [...Array(length + 6 + length)]
-          .reduce(function (accumulator) {
-            return accumulator + String.fromCharCode(i++)
-          }, '')
-          .match(pattern)
-          .reverse().join('')
+    .reduce(function (accumulator) {
+      return accumulator + String.fromCharCode(i++)
+    }, '')
+    .match(/[a-zA-Z]+/g)
+    .reverse()
+    .join('')
 }
--- src/python/string/index.js
+++ src/python/string/index.js
@@ -1,2 +1,4 @@
 module.exports['capwords'] = require('./capwords')
 module.exports['ascii_letters'] = require('./ascii_letters')
+module.exports['ascii_lowercase'] = require('./ascii_lowercase')
+module.exports['ascii_uppercase'] = require('./ascii_uppercase')
