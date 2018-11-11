9f81aaf493c936e1eede44a10d234787a429f0fd
rony2k6 <rony2k6@gmail.com>
2018-11-11 14:01:17 +0600

    preg_replace: test syntext corrected

--- src/php/pcre/preg_replace.js
+++ src/php/pcre/preg_replace.js
@@ -1,9 +1,9 @@
 module.exports = function preg_replace (pattern, replacement, string) { // eslint-disable-line camelcase
   //   original by: rony2k6 (https://github.com/rony2k6)
-  //   example 1: preg_replace("/xmas/i", "Christmas", "It was the night before Xmas...")
-  //   returns 1: It was the night before Christmas...
-  //   example 2: preg_replace("/xmas/ig", "Christmas", "xMas: It was the night before Xmas...")
-  //   returns 2: Christmas: It was the night before Christmas...
+  //   example 1: preg_replace("/xmas/i", "Christmas", "Twas the night before Xmas.")
+  //   returns 1: Twas the night before Christmas.
+  //   example 2: preg_replace("/xmas/ig", "Christmas", "xMas: Twas the night before Xmas.")
+  //   returns 2: Christmas: Twas the night before Christmas.
   //   example 3: preg_replace("\/(\\w+) (\\d+), (\\d+)\/i", "${1}1,$3", "April 15, 2003")
   //   returns 3: April1,2003
   //   example 4: preg_replace("/[^a-zA-Z0-9]+/", "", "The Development of code . http://www.")
--- test/module/module.js
+++ test/module/module.js
@@ -12,8 +12,8 @@ var math = require(location + '/ruby/Math')
 var preg_match = require(location + '/php/pcre/preg_match')
 var preg_replace = require(location + '/php/pcre/preg_replace')
 
-console.log(preg_replace("/xmas/i", "Christmas", "It was the night before Xmas...")) // Should report It was the night before Christmas...
-console.log(preg_replace("/xmas/ig", "Christmas", "xMas: It was the night before Xmas...")) // Should report Christmas: It was the night before Christmas...
+console.log(preg_replace("/xmas/i", "Christmas", "Twas the night before Xmas.")) // Should report Twas the night before Christmas.
+console.log(preg_replace("/xmas/ig", "Christmas", "xMas: Twas the night before Xmas.")) // Should report Christmas: Twas the night before Christmas.
 console.log(preg_replace("/(\\w+) (\\d+), (\\d+)/i", "$11,$3", "April 15, 2003")) // Should report April1,2003
 console.log(preg_replace("/[^a-zA-Z0-9]+/", "", "The Development of code . http://www.")) // Should report TheDevelopmentofcodehttpwww
 console.log(preg_replace("/[^A-Za-z0-9_\\s]/", "", 'D"usseldorfer H"auptstrasse')) // Should report Dusseldorfer Hauptstrasse
