c6e0529fc8ac733ef89cb0e5537ee971b53e05c9
Waldo Malqui <waldo@fayr.us>
2019-04-20 17:40:56 -0500

    FIX: Missing the first & 0xFF

--- src/php/network/long2ip.js
+++ src/php/network/long2ip.js
@@ -1,6 +1,6 @@
 module.exports = function long2ip (ip) {
   //  discuss at: http://locutus.io/php/long2ip/
-  // original by: Waldo Malqui Silva (http://waldo.malqui.info)
+  // original by: Waldo Malqui Silva (https://fayr.us/waldo/)
   //   example 1: long2ip( 3221234342 )
   //   returns 1: '192.0.34.166'
 
@@ -8,5 +8,5 @@ module.exports = function long2ip (ip) {
     return false
   }
 
-  return [ip >>> 24, ip >>> 16 & 0xFF, ip >>> 8 & 0xFF, ip & 0xFF].join('.')
+  return [ip >>> 24 & 0xFF, ip >>> 16 & 0xFF, ip >>> 8 & 0xFF, ip & 0xFF].join('.')
 }
