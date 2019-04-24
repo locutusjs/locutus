58c455e708e79306fc96a6022eaf3822f90ef729
wmalqui <wmalqui@vmc.la>
2019-04-24 18:40:47 -0500

    Improve pow function with precision size

--- src/php/math/pow.js
+++ src/php/math/pow.js
@@ -1,8 +1,9 @@
 module.exports = function pow (base, exp) {
   //  discuss at: http://locutus.io/php/pow/
   // original by: Onno Marsman (https://twitter.com/onnomarsman)
+  // improved by: Waldo Malqui Silva (https://fayr.us/waldo/)
   //   example 1: pow(8723321.4, 7)
-  //   returns 1: 3.8439091680778995e+48
+  //   returns 1: 3.8439091680779e+48
 
-  return Math.pow(base, exp)
+  return Number(Math.pow(base, exp).toPrecision(15))
 }
