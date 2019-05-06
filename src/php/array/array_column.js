e221a293143896a3a9aeec0ca8f39d608a8a8f14
Enzarro <spreadingmadness@gmail.com>
2019-05-06 12:55:23 -0400

    Update array_column.js
    Function almost completely rewritten. Added third parameter and new validations. Variable names changed to match PHP.

--- src/php/array/array_column.js
+++ src/php/array/array_column.js
@@ -1,20 +1,43 @@
-module.exports = function array_column (array, columnName) { // eslint-disable-line camelcase
+module.exports = function array_column (input, column_key, index_key = null) { // eslint-disable-line camelcase
   //  discuss at: http://locutus.io/php/array_column/
   // original by: Enzo Dañobeytía
-  //   example 1: array_column({{name: 'Alex', value: 1}, {name: 'Elvis', value: 2}, {name: 'Michel', value: 3}}, 'name')
+  //   example 1: array_column([{name: 'Alex', value: 1}, {name: 'Elvis', value: 2}, {name: 'Michael', value: 3}], 'name')
   //   returns 1: ['Alex', 'Elvis', 'Michael']
+  //   example 2: array_column({0: {name: 'Alex', value: 1}, 1: {name: 'Elvis', value: 2}, 2: {name: 'Michael', value: 3}}, 'name')
+  //   returns 2: ['Alex', 'Elvis', 'Michael']
+  //   example 3: array_column({0: {name: 'Alex', value: 1}, 1: {name: 'Elvis', value: 2}, 2: {name: 'Michael', value: 3}}, 'name')
+  //   returns 3: ['Alex', 'Elvis', 'Michael']
   
-	if (array !== null) {
-		if (Array.isArray(array)) {
-			return array.map(function(value,index) {
-				return value[columnName];
-			})
-		} else if (typeof array === 'object') {
-			var newarray = [];
-			for (var key in array) {
-				newarray.push(array[key][columnName]);
-			}
-			return newarray;
-		}
+	if (input !== null && (typeof input === 'object' || Array.isArray(input))) {
+        var newarray = [];
+
+        if (typeof input === 'object') {
+            let temparray = [];
+			for (let key of Object.keys(input)) {
+                temparray.push(input[key]);
+            }
+            input = temparray;
+        }
+        
+		if (Array.isArray(input)) {
+            for (let key of input.keys()) {
+                if (index_key && input[key][index_key]) {
+                    if (column_key) {
+                        newarray[input[key][index_key]] = input[key][column_key];
+                    } else {
+                        newarray[input[key][index_key]] = input[key];
+                    }
+                    
+                } else {
+                    if (column_key) {
+                        newarray.push(input[key][column_key]);
+                    } else {
+                        newarray.push(input[key]);
+                    }
+                }
+            }
+        }
+        
+        return newarray;
 	}
 }
