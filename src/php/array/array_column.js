d275aff6c1257a37697945f2285cf28a34de4c35
Enzarro <spreadingmadness@gmail.com>
2019-05-09 17:55:03 -0400

    Update array_column.js
    Made some format corrections according to automatic checks.

--- src/php/array/array_column.js
+++ src/php/array/array_column.js
@@ -1,45 +1,41 @@
-module.exports = function array_column(input, column_key, index_key = null) { // eslint-disable-line camelcase
-    //   discuss at: http://locutus.io/php/array_column/
-    //   original by: Enzo Dañobeytía
-    //   example 1: array_column([{name: 'Alex', value: 1}, {name: 'Elvis', value: 2}, {name: 'Michael', value: 3}], 'name')
-    //   returns 1: {0: "Alex", 1: "Elvis", 2: "Michael"}
-    //   example 2: array_column({0: {name: 'Alex', value: 1}, 1: {name: 'Elvis', value: 2}, 2: {name: 'Michael', value: 3}}, 'name')
-    //   returns 2: {0: "Alex", 1: "Elvis", 2: "Michael"}
-    //   example 3: array_column([{name: 'Alex', value: 1}, {name: 'Elvis', value: 2}, {name: 'Michael', value: 3}], 'name', 'value')
-    //   returns 3: {1: "Alex", 2: "Elvis", 3: "Michael"}
-    //   example 4: array_column([{name: 'Alex', value: 1}, {name: 'Elvis', value: 2}, {name: 'Michael', value: 3}], null, 'value')
-    //   returns 4: {1: {name: 'Alex', value: 1}, 2: {name: 'Elvis', value: 2}, 3: {name: 'Michael', value: 3}}
+module.exports = function array_column (input, ColumnKey, IndexKey = null) { // eslint-disable-line camelcase
+  //   discuss at: http://locutus.io/php/array_column/
+  //   original by: Enzo Dañobeytía
+  //   example 1: array_column([{name: 'Alex', value: 1}, {name: 'Elvis', value: 2}, {name: 'Michael', value: 3}], 'name')
+  //   returns 1: {0: "Alex", 1: "Elvis", 2: "Michael"}
+  //   example 2: array_column({0: {name: 'Alex', value: 1}, 1: {name: 'Elvis', value: 2}, 2: {name: 'Michael', value: 3}}, 'name')
+  //   returns 2: {0: "Alex", 1: "Elvis", 2: "Michael"}
+  //   example 3: array_column([{name: 'Alex', value: 1}, {name: 'Elvis', value: 2}, {name: 'Michael', value: 3}], 'name', 'value')
+  //   returns 3: {1: "Alex", 2: "Elvis", 3: "Michael"}
+  //   example 4: array_column([{name: 'Alex', value: 1}, {name: 'Elvis', value: 2}, {name: 'Michael', value: 3}], null, 'value')
+  //   returns 4: {1: {name: 'Alex', value: 1}, 2: {name: 'Elvis', value: 2}, 3: {name: 'Michael', value: 3}}
 
-    if (input !== null && (typeof input === 'object' || Array.isArray(input))) {
-        var newarray = [];
-
-        if (typeof input === 'object') {
-            let temparray = [];
-            for (let key of Object.keys(input)) {
-                temparray.push(input[key]);
-            }
-            input = temparray;
-        }
-
-        if (Array.isArray(input)) {
-            for (let key of input.keys()) {
-                if (index_key && input[key][index_key]) {
-                    if (column_key) {
-                        newarray[input[key][index_key]] = input[key][column_key];
-                    } else {
-                        newarray[input[key][index_key]] = input[key];
-                    }
-
-                } else {
-                    if (column_key) {
-                        newarray.push(input[key][column_key]);
-                    } else {
-                        newarray.push(input[key]);
-                    }
-                }
-            }
-        }
-
-        return Object.assign({}, newarray);
+  if (input !== null && (typeof input === 'object' || Array.isArray(input))) {
+    var newarray = []
+    if (typeof input === 'object') {
+      let temparray = []
+      for (let key of Object.keys(input)) {
+        temparray.push(input[key])
+      }
+      input = temparray
     }
+    if (Array.isArray(input)) {
+      for (let key of input.keys()) {
+        if (IndexKey && input[key][IndexKey]) {
+          if (ColumnKey) {
+            newarray[input[key][IndexKey]] = input[key][ColumnKey]
+          } else {
+            newarray[input[key][IndexKey]] = input[key]
+          }
+        } else {
+          if (ColumnKey) {
+            newarray.push(input[key][ColumnKey])
+          } else {
+            newarray.push(input[key])
+          }
+        }
+      }
+    }
+    return Object.assign({}, newarray)
+  }
 }
