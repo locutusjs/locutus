dadfc8a2ddd0b3a4b5351e15490d603e9e491949
Enzarro <spreadingmadness@gmail.com>
2019-05-09 17:28:27 -0400

    Update array_column.js
    Now it returns an object instead of an array, because when pushing array indexes over non existing ones, it fills the array with empty entries.

--- src/php/array/array_column.js
+++ src/php/array/array_column.js
@@ -2,13 +2,13 @@ module.exports = function array_column(input, column_key, index_key = null) { //
     //   discuss at: http://locutus.io/php/array_column/
     //   original by: Enzo Dañobeytía
     //   example 1: array_column([{name: 'Alex', value: 1}, {name: 'Elvis', value: 2}, {name: 'Michael', value: 3}], 'name')
-    //   returns 1: ['Alex', 'Elvis', 'Michael']
+    //   returns 1: {0: "Alex", 1: "Elvis", 2: "Michael"}
     //   example 2: array_column({0: {name: 'Alex', value: 1}, 1: {name: 'Elvis', value: 2}, 2: {name: 'Michael', value: 3}}, 'name')
-    //   returns 2: ['Alex', 'Elvis', 'Michael']
+    //   returns 2: {0: "Alex", 1: "Elvis", 2: "Michael"}
     //   example 3: array_column([{name: 'Alex', value: 1}, {name: 'Elvis', value: 2}, {name: 'Michael', value: 3}], 'name', 'value')
-    //   returns 3: [empty, 1: 'Alex', 2: 'Elvis', 3: 'Michael']
+    //   returns 3: {1: "Alex", 2: "Elvis", 3: "Michael"}
     //   example 4: array_column([{name: 'Alex', value: 1}, {name: 'Elvis', value: 2}, {name: 'Michael', value: 3}], null, 'value')
-    //   returns 4: [empty, 1: {name: 'Alex', value: 1}, 2: {name: 'Elvis', value: 2}, 3: {name: 'Michael', value: 3}]
+    //   returns 4: {1: {name: 'Alex', value: 1}, 2: {name: 'Elvis', value: 2}, 3: {name: 'Michael', value: 3}}
 
     if (input !== null && (typeof input === 'object' || Array.isArray(input))) {
         var newarray = [];
@@ -40,6 +40,6 @@ module.exports = function array_column(input, column_key, index_key = null) { //
             }
         }
 
-        return newarray;
+        return Object.assign({}, newarray);
     }
 }
