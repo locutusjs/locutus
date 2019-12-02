ebe9c243e778d9fc8719b8349cdaed5c83f7f891
Brett Zamir <brettz9@yahoo.com>
2019-12-02 20:44:27 +0800

    Comment out another unused variable
    Flagged by lgtm.com

--- src/php/json/json_encode.js
+++ src/php/json/json_encode.js
@@ -130,7 +130,7 @@ module.exports = function json_encode (mixedVal) { // eslint-disable-line camelc
             v = partial.length === 0 ? '[]' : gap
               ? '[\n' + gap + partial.join(',\n' + gap) + '\n' + mind + ']'
               : '[' + partial.join(',') + ']'
-            gap = mind
+            // gap = mind // not used
             return v
           }
 
