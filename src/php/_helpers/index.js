8f9ff2cc8772317466b9687d317955db6d65036d
Rafał Kukawski <rafal@kukawski.pl>
2019-12-02 08:32:45 +0100

    Follow PHP behavior in round function (#405)
    * Add php_cast_float function
    
    Helper function for quick conversion between data types (especially function params) using PHP logic
    
    * Add export for php_cast_float
    
    * Implement round following PHP logic
    
    - param casting
    - default param values
    - additional pre-rounding logic
    - helper function for rounding to integer values with various mode support

--- src/php/_helpers/index.js
+++ src/php/_helpers/index.js
@@ -1,3 +1,4 @@
 module.exports['_bc'] = require('./_bc')
 module.exports['_phpCastString'] = require('./_phpCastString')
+module.exports['_php_cast_float'] = require('./_php_cast_float')
 module.exports['_php_cast_int'] = require('./_php_cast_int')
