787e6ec6abcf4a00041578fa2c1ab18b51cb98e8
Rafał Kukawski <rafal@kukawski.pl>
2019-12-11 00:30:50 +0100

    Unserialize re-implemented
    - more strict and robust parser
    - better PHP compatibility, especially in many edge cases
    - basic reference support (r and R types)
    - support for string with escape sequences (S type)
    - support for infinity and nan values (d type)

--- src/php/var/unserialize.js
+++ src/php/var/unserialize.js
@@ -1,254 +1,315 @@
+function cache() {
+  const store = [];
+  const cache = function cache (value) {
+    store.push(value[0]);
+    return value;
+  };
+
+  cache.get = (index) => {
+    if (index >= store.length) {
+      throw RangeError(`Can't resolve reference ${index + 1}`);
+    }
+
+    return store[index];
+  };
+
+  return cache;
+}
+
+function expectType(str, cache) {
+  const types = /^(?:N(?=;)|[bidsSaOCrR](?=:)|[^:]+(?=:))/g;
+  const type = (types.exec(str) || [])[0];
+
+  if (!type) {
+    throw SyntaxError('Invalid input: ' + str);
+  }
+
+  switch (type) {
+    case 'N':
+      return cache([ null, 2 ]);
+    case 'b':
+      return cache(expectBool(str));
+    case 'i':
+      return cache(expectInt(str));
+    case 'd':
+      return cache(expectFloat(str));
+    case 's':
+      return cache(expectString(str));
+    case 'S':
+      return cache(expectEscapedString(str));
+    case 'a':
+      return expectArray(str, cache);
+    case 'O':
+      return expectObject(str, cache);
+    case 'C':
+      return expectClass(str, cache);
+    case 'r':
+    case 'R':
+      return expectReference(str, cache);
+    default:
+      throw SyntaxError(`Invalid or unsupported data type: ${type}`);
+  }
+}
+
+function expectBool(str) {
+	const reBool = /^b:([01]);/;
+  const [ match, boolMatch ] = reBool.exec(str) || [];
+  
+  if (!boolMatch) {
+  	throw SyntaxError('Invalid bool value, expected 0 or 1');
+  }
+  
+  return [ boolMatch === '1', match.length ];
+}
+
+function expectInt(str) {
+  const reInt = /^i:([+-]?\d+);/;
+  const [ match, intMatch ] = reInt.exec(str) || [];
+
+  if (!intMatch) {
+    throw SyntaxError('Expected an integer value');
+  }
+
+  return [ parseInt(intMatch, 10), match.length ];
+}
+
+function expectFloat(str) {
+  const reFloat = /^d:(NAN|-?INF|(?:\d+\.\d*|\d*\.\d+|\d+)(?:[eE][+-]\d+)?);/;
+  const [ match, floatMatch ] = reFloat.exec(str) || [];
+
+  if (!floatMatch) {
+    throw SyntaxError('Expected a float value');
+  }
+
+  let floatValue;
+
+  switch (floatMatch) {
+    case 'NAN':
+      floatValue = Number.NaN;
+      break;
+    case '-INF':
+      floatValue = Number.NEGATIVE_INFINITY;
+      break;
+    case 'INF':
+      floatValue = Number.POSITIVE_INFINITY;
+      break;
+    default:
+      floatValue = parseFloat(floatMatch);
+      break;
+  }
+
+  return [ floatValue, match.length ];
+}
+
+function expectString(str) {
+	const reStrLength = /^s:(\d+):/g
+  const [ match, strLenMatch ] = reStrLength.exec(str) || [];
+  
+  if (!strLenMatch) {
+  	throw SyntaxError('Expected string length annotation');
+  }
+  
+  const len = parseInt(strLenMatch, 10);
+
+  // todo: handle utf8 overhead chars
+  const reString = RegExp(`^"([\\s\\S]{${len}})";`);
+  const [ strLiteralMatch, strMatch ] = reString.exec(str.substr(match.length)) || [];
+
+  if (!strLiteralMatch) {
+    throw SyntaxError('Expected a string value');
+  }
+
+  return [ strMatch, match.length + strLiteralMatch.length ];
+}
+
+function expectEscapedString(str) {
+	const reStrLength = /^S:(\d+):/g
+  const [ match, strLenMatch ] = reStrLength.exec(str) || [];
+  
+  if (!strLenMatch) {
+  	throw SyntaxError('Expected string length annotation');
+  }
+  
+  const len = parseInt(strLenMatch, 10);
+
+  // todo: handle utf8 overhead chars
+  const reString = RegExp(`^"((?:\\\\[0-9a-fA-F]{2}|[\\s\\S]){${len}})";`);
+  const [ strLiteralMatch, strMatch ] = reString.exec(str.substr(match.length)) || [];
+
+  if (!strLiteralMatch) {
+    throw SyntaxError('Expected a string value');
+  }
+
+  return [ strMatch.replace(/\\([0-9a-fA-F]{2})/g, (m, e) => String.fromCharCode(parseInt(e, 16))), match.length + strLiteralMatch.length ];
+}
+function expectKeyOrIndex(str) {
+  try {
+    return expectString(str);
+  } catch (err) {}
+
+  try {
+    return expectEscapedString(str);
+  } catch (err) {}
+
+  try {
+    return expectInt(str);
+  } catch (err) {
+    throw SyntaxError('Expected key or index');
+  }
+}
+
+function expectObject(str, cache) {
+  // O:<class name length>:"class name":<prop count>:{<props and values>}
+  // O:8:"stdClass":2:{s:3:"foo";s:3:"bar";s:3:"bar";s:3:"baz";}
+  const reObjectLiteral = /^O:(\d+):"([^"]+)":(\d+):\{/;
+  const [ objectLiteralBeginMatch, classNameLengthMatch, className, propCountMatch ] = reObjectLiteral.exec(str) || [];
+
+  if (!objectLiteralBeginMatch) {
+    throw SyntaxError('Invalid input');
+  }
+
+  if (className !== 'stdClass') {
+    throw SyntaxError(`Unsupported object type: ${className}`);
+  }
+
+  let totalOffset = objectLiteralBeginMatch.length;
+
+  const propCount = parseInt(propCountMatch, 10)
+  const obj = {};
+  cache([obj]);
+
+  str = str.substr(totalOffset);
+
+  for (let i = 0; i < propCount; i++) {
+    const prop = expectKeyOrIndex(str);
+    str = str.substr(prop[1]);
+    totalOffset += prop[1];
+
+    const value = expectType(str, cache);
+    str = str.substr(value[1]);
+    totalOffset += value[1]
+
+    obj[prop[0]] = value[0];
+  }
+
+  // strict parsing, expect } after object literal
+  if (str.charAt(0) !== '}') {
+    throw SyntaxError('Expected }');
+  }
+
+  return [ obj, totalOffset + 1 ]; // skip final }
+}
+
+function expectClass(str, cache) {
+  // can't be well supported, because requires calling eval (or similar)
+  // in order to call serialized constructor name
+  // which is unsafe
+  // or assume that constructor is defined in global scope
+  // but this is too much limiting
+  throw Error('Not yet implemented');
+}
+
+function expectReference(str, cache) {
+  const reRef = /^[rR]:([1-9]\d*);/;
+  const [ match, refIndex ] = reRef.exec(str) || [];
+
+  if (!match) {
+    throw SyntaxError('Expected reference value');
+  }
+
+  return [ cache.get(parseInt(refIndex, 10) - 1), match.length ];
+}
+
+function expectArray(str, cache) {
+  const reArrayLength = /^a:(\d+):{/;
+  const [ arrayLiteralBeginMatch, arrayLengthMatch ] = reArrayLength.exec(str) || [];
+
+  if (!arrayLengthMatch) {
+    throw SyntaxError('Expected array length annotation');
+  }
+
+  str = str.substr(arrayLiteralBeginMatch.length);
+
+  const array = expectArrayItems(str, parseInt(arrayLengthMatch, 10), cache);
+
+  // strict parsing, expect closing } brace after array literal
+  if (str.charAt(array[1]) !== '}') {
+    throw SyntaxError('Expected }');
+  }
+
+  return [ array[0], arrayLiteralBeginMatch.length + array[1] + 1 ]; // jump over }
+}
+
+function expectArrayItems(str, expectedItems = 0, cache) {
+  let key, item, totalOffset = 0;
+  const items = [];
+  cache([items]);
+
+  for (let i = 0; i < expectedItems; i++) {
+    key = expectKeyOrIndex(str);
+
+    str = str.substr(key[1]);
+    totalOffset += key[1];
+
+    // references are resolved immediately, so if duplicate key overwrites previous array index
+    // the old value is anyway resolved
+    // fixme: but next time the same reference should point to the new value
+    item = expectType(str, cache);
+    str = str.substr(item[1]);
+    totalOffset += item[1];
+
+    items[key[0]] = item[0];
+  }
+
+  return [ items, totalOffset ];
+}
+
 module.exports = function unserialize (data) {
-  //  discuss at: https://locutus.io/php/unserialize/
-  // original by: Arpad Ray (mailto:arpad@php.net)
-  // improved by: Pedro Tainha (https://www.pedrotainha.com)
-  // improved by: Kevin van Zonneveld (https://kvz.io)
-  // improved by: Kevin van Zonneveld (https://kvz.io)
-  // improved by: Chris
-  // improved by: James
-  // improved by: Le Torbi
-  // improved by: Eli Skeggs
-  // bugfixed by: dptr1988
-  // bugfixed by: Kevin van Zonneveld (https://kvz.io)
-  // bugfixed by: Brett Zamir (https://brett-zamir.me)
-  // bugfixed by: philippsimon (https://github.com/philippsimon/)
-  //  revised by: d3x
-  //    input by: Brett Zamir (https://brett-zamir.me)
-  //    input by: Martin (https://www.erlenwiese.de/)
-  //    input by: kilops
-  //    input by: Jaroslaw Czarniak
-  //    input by: lovasoa (https://github.com/lovasoa/)
-  // improved by: Rafał Kukawski
-  //      note 1: We feel the main purpose of this function should be
-  //      note 1: to ease the transport of data between php & js
-  //      note 1: Aiming for PHP-compatibility, we have to translate objects to arrays
-  //   example 1: unserialize('a:3:{i:0;s:5:"Kevin";i:1;s:3:"van";i:2;s:9:"Zonneveld";}')
-  //   returns 1: ['Kevin', 'van', 'Zonneveld']
-  //   example 2: unserialize('a:2:{s:9:"firstName";s:5:"Kevin";s:7:"midName";s:3:"van";}')
-  //   returns 2: {firstName: 'Kevin', midName: 'van'}
-  //   example 3: unserialize('a:3:{s:2:"ü";s:2:"ü";s:3:"四";s:3:"四";s:4:"𠜎";s:4:"𠜎";}')
-  //   returns 3: {'ü': 'ü', '四': '四', '𠜎': '𠜎'}
-  //   example 4: unserialize(undefined)
-  //   returns 4: false
-  //   example 5: unserialize('O:8:"stdClass":1:{s:3:"foo";b:1;}')
-  //   returns 5: { foo: true }
-  //   example 6: unserialize('a:2:{i:0;N;i:1;s:0:"";')
-  //   returns 6: [null, ""]
-
-  var utf8Overhead = function (str) {
-    var s = str.length
-    for (var i = str.length - 1; i >= 0; i--) {
-      var code = str.charCodeAt(i)
-      if (code > 0x7f && code <= 0x7ff) {
-        s++
-      } else if (code > 0x7ff && code <= 0xffff) {
-        s += 2
-      }
-      // trail surrogate
-      if (code >= 0xDC00 && code <= 0xDFFF) {
-        i--
-      }
-    }
-    return s - 1
-  }
-  var readUntil = function (data, offset, stopchr) {
-    var i = 2
-    var buf = []
-    var chr = data.slice(offset, offset + 1)
-
-    while (chr !== stopchr) {
-      if ((i + offset) > data.length) {
-        throw Error('Invalid')
-      }
-      buf.push(chr)
-      chr = data.slice(offset + (i - 1), offset + i)
-      i += 1
-    }
-    return [buf.length, buf.join('')]
-  }
-  var readChrs = function (data, offset, length) {
-    var i, chr, buf
-
-    buf = []
-    for (i = 0; i < length; i++) {
-      chr = data.slice(offset + (i - 1), offset + i)
-      buf.push(chr)
-      length -= utf8Overhead(chr)
-    }
-    return [buf.length, buf.join('')]
-  }
-  function _unserialize (data, offset) {
-    var dtype
-    var dataoffset
-    var keyandchrs
-    var keys
-    var contig
-    var length
-    var array
-    var obj
-    var readdata
-    var readData
-    var ccount
-    var stringlength
-    var i
-    var key
-    var kprops
-    var kchrs
-    var vprops
-    var vchrs
-    var value
-    var chrs = 0
-    var typeconvert = function (x) {
-      return x
-    }
-
-    if (!offset) {
-      offset = 0
-    }
-    dtype = (data.slice(offset, offset + 1))
-
-    dataoffset = offset + 2
-
-    switch (dtype) {
-      case 'i':
-        typeconvert = function (x) {
-          return parseInt(x, 10)
-        }
-        readData = readUntil(data, dataoffset, ';')
-        chrs = readData[0]
-        readdata = readData[1]
-        dataoffset += chrs + 1
-        break
-      case 'b':
-        typeconvert = function (x) {
-          const value = parseInt(x, 10)
-
-          switch (value) {
-            case 0:
-              return false
-            case 1:
-              return true
-            default:
-              throw SyntaxError('Invalid boolean value')
-          }
-        }
-        readData = readUntil(data, dataoffset, ';')
-        chrs = readData[0]
-        readdata = readData[1]
-        dataoffset += chrs + 1
-        break
-      case 'd':
-        typeconvert = function (x) {
-          return parseFloat(x)
-        }
-        readData = readUntil(data, dataoffset, ';')
-        chrs = readData[0]
-        readdata = readData[1]
-        dataoffset += chrs + 1
-        break
-      case 'N':
-      case 'n':
-        readdata = null
-        break
-      case 's':
-        ccount = readUntil(data, dataoffset, ':')
-        chrs = ccount[0]
-        stringlength = ccount[1]
-        dataoffset += chrs + 2
-
-        readData = readChrs(data, dataoffset + 1, parseInt(stringlength, 10))
-        chrs = readData[0]
-        readdata = readData[1]
-        dataoffset += chrs + 2
-        if (chrs !== parseInt(stringlength, 10) && chrs !== readdata.length) {
-          throw SyntaxError('String length mismatch')
-        }
-        break
-      case 'a':
-        readdata = {}
-
-        keyandchrs = readUntil(data, dataoffset, ':')
-        chrs = keyandchrs[0]
-        keys = keyandchrs[1]
-        dataoffset += chrs + 2
-
-        length = parseInt(keys, 10)
-        contig = true
-
-        for (i = 0; i < length; i++) {
-          kprops = _unserialize(data, dataoffset)
-          kchrs = kprops[1]
-          key = kprops[2]
-          dataoffset += kchrs
-
-          vprops = _unserialize(data, dataoffset)
-          vchrs = vprops[1]
-          value = vprops[2]
-          dataoffset += vchrs
-
-          if (key !== i) {
-            contig = false
-          }
-
-          readdata[key] = value
-        }
-
-        if (contig) {
-          array = new Array(length)
-          for (i = 0; i < length; i++) {
-            array[i] = readdata[i]
-          }
-          readdata = array
-        }
-
-        dataoffset += 1
-        break
-      case 'O': {
-        // O:<class name length>:"class name":<prop count>:{<props and values>}
-        // O:8:"stdClass":2:{s:3:"foo";s:3:"bar";s:3:"bar";s:3:"baz";}
-        readData = readUntil(data, dataoffset, ':') // read class name length
-        dataoffset += readData[0] + 1
-        readData = readUntil(data, dataoffset, ':')
-
-        if (readData[1] !== '"stdClass"') {
-          throw Error('Unsupported object type: ' + readData[1])
-        }
-
-        dataoffset += readData[0] + 1 // skip ":"
-        readData = readUntil(data, dataoffset, ':')
-        keys = parseInt(readData[1], 10)
-
-        dataoffset += readData[0] + 2 // skip ":{"
-        obj = {}
-
-        for (i = 0; i < keys; i++) {
-          readData = _unserialize(data, dataoffset)
-          key = readData[2]
-          dataoffset += readData[1]
-
-          readData = _unserialize(data, dataoffset)
-          dataoffset += readData[1]
-          obj[key] = readData[2]
-        }
-
-        dataoffset += 1 // skip "}"
-        readdata = obj
-        break
-      }
-      default:
-        throw SyntaxError('Unknown / Unhandled data type(s): ' + dtype)
-    }
-    return [dtype, dataoffset - offset, typeconvert(readdata)]
-  }
+  //       discuss at: https://locutus.io/php/unserialize/
+  //      original by: Arpad Ray (mailto:arpad@php.net)
+  //      improved by: Pedro Tainha (https://www.pedrotainha.com)
+  //      improved by: Kevin van Zonneveld (https://kvz.io)
+  //      improved by: Kevin van Zonneveld (https://kvz.io)
+  //      improved by: Chris
+  //      improved by: James
+  //      improved by: Le Torbi
+  //      improved by: Eli Skeggs
+  //      bugfixed by: dptr1988
+  //      bugfixed by: Kevin van Zonneveld (https://kvz.io)
+  //      bugfixed by: Brett Zamir (https://brett-zamir.me)
+  //      bugfixed by: philippsimon (https://github.com/philippsimon/)
+  //       revised by: d3x
+  //         input by: Brett Zamir (https://brett-zamir.me)
+  //         input by: Martin (https://www.erlenwiese.de/)
+  //         input by: kilops
+  //         input by: Jaroslaw Czarniak
+  //         input by: lovasoa (https://github.com/lovasoa/)
+  //      improved by: Rafał Kukawski
+  // reimplemented by: Rafał Kukawski
+  //           note 1: We feel the main purpose of this function should be
+  //           note 1: to ease the transport of data between php & js
+  //           note 1: Aiming for PHP-compatibility, we have to translate objects to arrays
+  //        example 1: unserialize('a:3:{i:0;s:5:"Kevin";i:1;s:3:"van";i:2;s:9:"Zonneveld";}')
+  //        returns 1: ['Kevin', 'van', 'Zonneveld']
+  //        example 2: unserialize('a:2:{s:9:"firstName";s:5:"Kevin";s:7:"midName";s:3:"van";}')
+  //        returns 2: {firstName: 'Kevin', midName: 'van'}
+  //        example 3: unserialize('a:3:{s:2:"ü";s:2:"ü";s:3:"四";s:3:"四";s:4:"𠜎";s:4:"𠜎";}')
+  //        returns 3: {'ü': 'ü', '四': '四', '𠜎': '𠜎'}
+  //        example 4: unserialize(undefined)
+  //        returns 4: false
+  //        example 5: unserialize('O:8:"stdClass":1:{s:3:"foo";b:1;}')
+  //        returns 5: { foo: true }
+  //        example 6: unserialize('a:2:{i:0;N;i:1;s:0:"";')
+  //        returns 6: [null, ""]
 
   try {
     if (typeof data !== 'string') {
-      return false
+      return false;
     }
 
-    return _unserialize(data, 0)[2]
+  	return expectType(data, cache())[0];
   } catch (err) {
-    console.error(err)
-    return false
+    console.error(err);
+  	return false;
   }
 }
