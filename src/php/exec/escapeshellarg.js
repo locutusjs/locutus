4b543617f53d9a5f3087222686e143805c47e7d5
divinity76 <divinity76@gmail.com>
2020-09-23 13:45:11 +0200

    add bufix sample & credits

--- src/php/exec/escapeshellarg.js
+++ src/php/exec/escapeshellarg.js
@@ -2,8 +2,11 @@ module.exports = function escapeshellarg (arg) {
   //  discuss at: https://locutus.io/php/escapeshellarg/
   // original by: Felix Geisendoerfer (https://www.debuggable.com/felix)
   // improved by: Brett Zamir (https://brett-zamir.me)
+  // bugfixed by: divinity76 (https://github.com/divinity76)
   //   example 1: escapeshellarg("kevin's birthday")
   //   returns 1: "'kevin\\'s birthday'"
+  //   example 2: /home'; whoami;''
+  //   returns 2: '/home'\''; whoami;'\'''\'''
 
   var ret = ''
 
