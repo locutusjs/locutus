406a4bcad284f88c52eba43fd3ddbe8d06fcc8a4
divinity76 <divinity76@gmail.com>
2020-10-02 11:11:13 +0200

    refuse to escape strings with null bytes (#425)
    PHP also refuse to escape strings with null bytes, using a similar message. (not to mention, it's *literally impossible* to escape null bytes)

--- src/php/exec/escapeshellarg.js
+++ src/php/exec/escapeshellarg.js
@@ -8,6 +8,10 @@ module.exports = function escapeshellarg (arg) {
   //   example 2: escapeshellarg("/home'; whoami;''")
   //   returns 2: "'/home'\''; whoami;'\'''\'''"
 
+  if(arg.indexOf("\x00") !== -1) {
+    throw new Error('escapeshellarg(): Argument #1 ($arg) must not contain any null bytes');
+  }
+  
   var ret = ''
 
   ret = arg.replace(/\'/g, '\'\\\'\'')
