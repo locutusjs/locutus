97eed9a84a3d4125e78d9735baea36d1d84a4df2
JamieSlome <jamie@418sec.com>
2020-10-22 17:10:43 +0100

    Making requested code cleanliness and function changes.

--- src/php/network/inet_pton.js
+++ src/php/network/inet_pton.js
@@ -17,48 +17,54 @@ module.exports = function inet_pton (a) { // eslint-disable-line camelcase
   m = a.match(/^(?:\d{1,3}(?:\.|$)){4}/)
   if (m) {
     m = m[0].split('.')
-    m = f(m[0]) + f(m[1]) + f(m[2]) + f(m[3])
+    m = f(m[0], m[1], m[2], m[3])
     // Return if 4 bytes, otherwise false.
     return m.length === 4 ? m : false
   }
 
   // IPv6
-  if(a.length>39)return false;
+  if (a.length > 39) {
+    return false
+  }
 
   m = a.split('::')
 
   let res = ''
-  if(m.length>2)return false;	// :: can't be used more than once in IPv6.
+  if (m.length > 2) {
+    return false
+  }	// :: can't be used more than once in IPv6.
+
+  let reHexDigits = /^[\da-f]{1,4}$/
+
   for (j = 0; j < m.length; j++) {
     if (m[j].length === 0) {	// Skip if empty.
       continue
     }
     m[j] = m[j].split(':')
     for (i = 0; i < m[j].length; i++) {
-
+      let hextet = m[j][i]
       // check if valid hex string up to 4 chars
-      if(!(m[j][i].match(/^[\da-f]{1,4}$/))) {
+      if(!reHexDigits.test(hextet)) {
         return false
       }
 
-      m[j][i] = parseInt(m[j][i], 16)
+      hextet = parseInt(hextet, 16)
 
       // Would be NaN if it was blank, return false.
-      if (isNaN(m[j][i])) {
+      if (isNaN(hextet)) {
         // Invalid IP.
         return false
       }
-      m[j][i] = f(m[j][i] >> 8) + f(m[j][i] & 0xFF)
+      hextet = f(hextet >> 8, hextet & 0xFF)
     }
-    m[j] = m[j].join('')
-    res += m[j];
+    res += m[j].join('')
+    res += m[j]
   }
   x = res.length
   if (x === 16) {
     return res
   } else if (x < 16 && m.length > 1) {
-    return res + (new Array(16 - x + 1))
-      .join('\x00') + m[1]
+    return res + '\x00'.repeat(16 - x + 1) + m[1]
   }
 
   // Invalid IP
