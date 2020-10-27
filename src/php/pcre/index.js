a010ca5f10507f2ce03f90c9a47b6c71d3a82514
Muhammad Humayun Rashed <rony2k6@gmail.com>
2020-10-27 08:51:56 +0600

    php->pcre->preg_replace function added. (#385)
    * php preg_replace function added
    
    * preg_replace: some test added
    
    * preg_replace: test syntext corrected
    
    * SyntaxError corrected
    
    * tests modified
    
    * annotation modified to pass test and test added
    
    * example 3 replace syntax corrected

--- src/php/pcre/index.js
+++ src/php/pcre/index.js
@@ -1,3 +1,4 @@
 module.exports['preg_quote'] = require('./preg_quote')
 module.exports['sql_regcase'] = require('./sql_regcase')
 module.exports['preg_match'] = require('./preg_match')
+module.exports['preg_replace'] = require('./preg_replace')
