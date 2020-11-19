88da3f1a356362d93a408f172542b26ff84b803c
Kevin van Zonneveld <kevin@vanzonneveld.net>
2020-11-19 20:38:40 +0100

    Remove _legacy folder

--- _legacy/php/var/is_resource.js
+++
@@ -1,9 +0,0 @@
-module.exports = function is_resource (handle) { // eslint-disable-line camelcase
-  //  discuss at: https://locutus.io/php/is_resource/
-  // original by: Brett Zamir (https://brett-zamir.me)
-  // improved by: Luis Salazar (https://www.freaky-media.com/)
-  //   example 1: is_resource('a')
-  //   returns 1: false
-
-  return !(!handle || typeof handle !== 'object' || !handle.constructor)
-}
