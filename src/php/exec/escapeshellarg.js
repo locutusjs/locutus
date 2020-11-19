f522ed1d970eb88436668c7919e80ef7abaeb49c
Kevin van Zonneveld <kevin@vanzonneveld.net>
2020-11-19 11:59:59 +0100

    Update escapeshellarg.js

--- src/php/exec/escapeshellarg.js
+++ src/php/exec/escapeshellarg.js
@@ -2,7 +2,7 @@ module.exports = function escapeshellarg (arg) {
   //  discuss at: https://locutus.io/php/escapeshellarg/
   // Warning: this function emulates escapeshellarg() for php-running-on-linux
   // the function behaves differently when running on Windows, which is not covered by this code.
-  // 
+  //
   // original by: Felix Geisendoerfer (https://www.debuggable.com/felix)
   // improved by: Brett Zamir (https://brett-zamir.me)
   // bugfixed by: divinity76 (https://github.com/divinity76)
@@ -11,10 +11,10 @@ module.exports = function escapeshellarg (arg) {
   //   example 2: escapeshellarg("/home'; whoami;''")
   //   returns 2: "'/home'\''; whoami;'\'''\'''"
 
-  if(arg.indexOf("\x00") !== -1) {
-    throw new Error('escapeshellarg(): Argument #1 ($arg) must not contain any null bytes');
+  if (arg.indexOf('\x00') !== -1) {
+    throw new Error('escapeshellarg(): Argument #1 ($arg) must not contain any null bytes')
   }
-  
+
   var ret = ''
 
   ret = arg.replace(/\'/g, '\'\\\'\'')
