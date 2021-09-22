bd1c6d1552127715c72bdd5f89b89ad6bcb7a270
Clari <clarinet.puppy@gmail.com>
2021-09-22 14:59:36 -0500

    Create punctuation.js
    I'm new to js coming from py, so sorry if this isn't the format you want it in.
    `[33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96, 123, 124, 125, 126]` char codes of all the characters if you want em

---
+++ src/python/string/punctuation.js
@@ -0,0 +1,7 @@
+module.exports = function punctuation () { // eslint-disable-line camelcase
+  //   example 1: punctuation()
+  //   returns 1: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
+  return "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+}
+© 2021 GitHub, Inc.
