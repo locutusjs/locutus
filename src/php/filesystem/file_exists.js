0eceddf696ce42aebd965b465f196f412c1708a0
Kevin van Zonneveld <vanzonneveld@gmail.com>
2024-04-05 19:35:54 +0200

    Update src/php/filesystem/file_exists.js

--- src/php/filesystem/file_exists.js
+++ src/php/filesystem/file_exists.js
@@ -2,7 +2,7 @@ module.exports = function file_exists(filename) {
   //       discuss at: https://locutus.io/php/file_exists/
   //      original by: Erik Niebla
   //           note 1: so this function is Node-only
-  //        example 1: var $bool = file_exists('test/never-change.txt')
+  //        example 1: file_exists('test/never-change.txt')
   //        returns 1: true
 
   const fs = require('fs')
