3e812135184129360361e9f2a19194b47148f595
kvz <vanzonneveld@gmail.com>
2024-04-06 05:44:34 +0000

    Deploying to gh-pages from  @ a01aa617330a7e0bc5f8412ecd56f690b2c818b1 🚀

--- js/locutus.js
+++ js/locutus.js
@@ -2,38 +2,40 @@ var $ = $ || window.jQuery
 $('section.authors').each(function () {
   var uniqueAuthors = {}
   var language = $(this).data('language')
-  $(this).find('entry').each(function () {
-    var type = $(this).data('type')
-    var func = $(this).data('function')
-    var author = $(this).text()
-    var m = false
-    if ((m = author.match(/Kevin van Zonneveld/))) {
-      return
-    } else if ((m = author.match(/^\s*(.+)\s*\(([^\)]+)\)\s*$/))) {
-      author = '<a href="' + m[2] + '">' + m[1] + '</a>'
-    } else if ((m = author.match(/^\s*(https?:\/\/.+)\s*$/i))) {
-      author = '<a href="' + m[1] + '">' + m[1].split('/')[2] + '</a>'
-    }
-
-    if (!uniqueAuthors[author]) {
-      uniqueAuthors[author] = {
-        author: author
+  $(this)
+    .find('entry')
+    .each(function () {
+      var type = $(this).data('type')
+      var func = $(this).data('function')
+      var author = $(this).text()
+      var m = false
+      if ((m = author.match(/Kevin van Zonneveld/))) {
+        return
+      } else if ((m = author.match(/^\s*(.+)\s*\(([^\)]+)\)\s*$/))) {
+        author = '<a href="' + m[2] + '">' + m[1] + '</a>'
+      } else if ((m = author.match(/^\s*(https?:\/\/.+)\s*$/i))) {
+        author = '<a href="' + m[1] + '">' + m[1].split('/')[2] + '</a>'
       }
-    }
-    if (!uniqueAuthors[author][type]) {
-      uniqueAuthors[author][type] = 0
-    }
-    if (!uniqueAuthors[author].total) {
-      uniqueAuthors[author].total = 0
-    }
-    if (!uniqueAuthors[author].functions) {
-      uniqueAuthors[author].functions = []
-    }
 
-    uniqueAuthors[author][type]++
-    uniqueAuthors[author].total++
-    uniqueAuthors[author].functions.push(func)
-  })
+      if (!uniqueAuthors[author]) {
+        uniqueAuthors[author] = {
+          author: author,
+        }
+      }
+      if (!uniqueAuthors[author][type]) {
+        uniqueAuthors[author][type] = 0
+      }
+      if (!uniqueAuthors[author].total) {
+        uniqueAuthors[author].total = 0
+      }
+      if (!uniqueAuthors[author].functions) {
+        uniqueAuthors[author].functions = []
+      }
+
+      uniqueAuthors[author][type]++
+      uniqueAuthors[author].total++
+      uniqueAuthors[author].functions.push(func)
+    })
 
   var authorList = []
   for (var name in uniqueAuthors) {
@@ -68,7 +70,7 @@ $('section.authors').each(function () {
       'parts by',
       'bugfixed by',
       'revised by',
-      'input by'
+      'input by',
     ]
 
     var contributions = []
